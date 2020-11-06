390aa1a3300fda70fe46da48f5eccca726920738
Roberto Preghenella <preghenella@bo.infn.it>
2020-11-06 15:06:03 +0100

    Simulation example of MPI-triggered Pythia8

---
+++ MC/run/examples/trigger_mpi.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+### This script run the simulation of Pythia8 pp INEL events with a trigger on number of MPI.
+### The details of the configuration are defined in the INI config files (--configFile).
+
+set -x
+
+MODULES="PIPE ITS TPC"
+EVENTS=20
+NWORKERS=8
+
+o2-sim -j ${NWORKERS} -n ${EVENTS} -g pythia8 -m ${MODULES} -o sim \
+       --configFile ${O2DPG_ROOT}/MC/config/examples/ini/trigger_mpi.ini \
+       > logsim 2>&1
