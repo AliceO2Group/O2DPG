581d16c06fc2e2151c6fe638208ed97efbe5ac93
gconesab <26649152+gconesab@users.noreply.github.com>
2020-12-02 17:11:50 +0100

    Adapt to use taskwrapper (#7)

--- MC/run/PWGGAJE/run_dirgamma.sh
+++ MC/run/PWGGAJE/run_dirgamma.sh
@@ -1,39 +1,56 @@
 #!/usr/bin/env bash
 
 # Generate gamma-jet events, Pythia8 in a given pt hard bin.
-# run_dirgamma.sh n_pthatbin
+# Execute: ./run_dirgamma.sh 
+# Set at least before running PTHATBIN with 1 to 6
 
-set -x
+#set -x 
 
-SIGEVENTS=10
-NWORKERS=2
-MODULES=
-RNDSEED=0    # [default = 0] time-based random seed
+# ----------- LOAD UTILITY FUNCTIONS --------------------------
+. ${O2_ROOT}/share/scripts/jobutils.sh
 
-# generate Pythia8 gamma-jet configuration
+# ----------- START ACTUAL JOB  ----------------------------- 
+
+RNDSEED=${RNDSEED:-0}   # [default = 0] time-based random seed
+
+NSIGEVENTS=${NSIGEVENTS:-20}
+NWORKERS=${NWORKERS:-8}
+MODULES="--skipModules ZDC" #"PIPE ITS TPC EMCAL"
+CONFIG_ENERGY=${CONFIG_ENERGY:-5020.0}
+CONFIG_NUCLEUSA=${CONFIG_NUCLEUSA:-2212}
+CONFIG_NUCLEUSB=${CONFIG_NUCLEUSB:-2212}
 
 # Define the pt hat bin arrays
 pthatbin_loweredges=(5 11 21 36 57 84)
 pthatbin_higheredges=(11 21 36 57 84 -1)
 
-# Define environmental vars for pt binning
-PTHATBIN=$1 #set it here or externally? Add protection out of array?
+# Recover environmental vars for pt binning
+#PTHATBIN=${PTHATBIN:-1} 
+
+if [ -z "$PTHATBIN" ]; then
+    echo "Pt-hat bin (env. var. PTHATBIN) not set, abort."
+    exit 1
+fi
 
 PTHATMIN=${pthatbin_loweredges[$PTHATBIN]}
 PTHATMAX=${pthatbin_higheredges[$PTHATBIN]}
 
+# Generate Pythia8 gamma-jet configuration
 ${O2DPG_ROOT}/MC/config/common/pythia8/utils/mkpy8cfg.py \
-	     --output=pythia8_dirgamma.cfg \
-	     --seed=${RNDSEED} \
-	     --idA=2212 \
-	     --idB=2212 \
-	     --eCM=13000. \
-	     --process=dirgamma \
-	     --ptHatMin=${PTHATMIN} \
-	     --ptHatMax=${PTHATMAX}
+        --output=pythia8_dirgamma.cfg \
+        --seed=${RNDSEED}             \
+        --idA=${CONFIG_NUCLEUSA}      \
+        --idB=${CONFIG_NUCLEUSB}      \
+        --eCM=${CONFIG_ENERGY}        \
+        --process=dirgamma            \
+        --ptHatMin=${PTHATMIN}        \
+        --ptHatMax=${PTHATMAX}
 
-# embed signal into background
-
-o2-sim -j ${NWORKERS} -n ${SIGEVENTS} -g pythia8 -m ${MODULES} \
-       --configKeyValues "GeneratorPythia8.config=pythia8_dirgamma.cfg" \
-       > log 2>&1
+# Generate signal 
+taskwrapper sgnsim.log o2-sim -j ${NWORKERS} -n ${NSIGEVENTS}               \
+           -g pythia8 -m ${MODULES}                                         \
+           --configKeyValues "GeneratorPythia8.config=pythia8_dirgamma.cfg" \
+           -o sgn
+ 
+# We need to exit for the ALIEN JOB HANDLER!
+exit 0
--- MC/run/PWGGAJE/run_jets.sh
+++ MC/run/PWGGAJE/run_jets.sh
@@ -1,39 +1,56 @@
 #!/usr/bin/env bash
 
 # Generate jet-jet events, Pythia8 in a pre-defined pt hard bin.
-# run_jets.sh n_pthatbin
+# Execute: ./run_jets.sh
+# Set at least before running PTHATBIN with 1 to 21
 
-set -x
+#set -x
 
-SIGEVENTS=10
-NWORKERS=2
-MODULES=
-RNDSEED=0    # [default = 0] time-based random seed
+# ----------- LOAD UTILITY FUNCTIONS --------------------------
+. ${O2_ROOT}/share/scripts/jobutils.sh
 
-# generate Pythia8 jet-jet configuration
+# ----------- START ACTUAL JOB  ----------------------------- 
+
+RNDSEED=${RNDSEED:-0}   # [default = 0] time-based random seed
+
+NSIGEVENTS=${NSIGEVENTS:-20}
+NWORKERS=${NWORKERS:-8}
+MODULES="--skipModules ZDC" #"PIPE ITS TPC EMCAL"
+CONFIG_ENERGY=${CONFIG_ENERGY:-5020.0}
+CONFIG_NUCLEUSA=${CONFIG_NUCLEUSA:-2212}
+CONFIG_NUCLEUSB=${CONFIG_NUCLEUSB:-2212}
 
 # Define the pt hat bin arrays
 pthatbin_loweredges=(0 5 7 9 12 16 21 28 36 45 57 70 85 99 115 132 150 169 190 212 235)
 pthatbin_higheredges=( 5 7 9 12 16 21 28 36 45 57 70 85 99 115 132 150 169 190 212 235 -1)
 
-# Define environmental vars for pt binning
-PTHATBIN=$1 #set it here or externally? Add protection out of array?
+# Recover environmental vars for pt binning
+#PTHATBIN=${PTHATBIN:-1} 
+
+if [ -z "$PTHATBIN" ]; then
+    echo "Pt-hat bin (env. var. PTHATBIN) not set, abort."
+    exit 1
+fi
 
 PTHATMIN=${pthatbin_loweredges[$PTHATBIN]}
 PTHATMAX=${pthatbin_higheredges[$PTHATBIN]}
 
+# Generate Pythia8 jet-jet configuration
 ${O2DPG_ROOT}/MC/config/common/pythia8/utils/mkpy8cfg.py \
-	     --output=pythia8_jets.cfg \
-	     --seed=${RNDSEED} \
-	     --idA=2212 \
-	     --idB=2212 \
-	     --eCM=5020. \
-	     --process=jets \
-	     --ptHatMin=${PTHATMIN} \
-	     --ptHatMax=${PTHATMAX}
+              --output=pythia8_jets.cfg \
+              --seed=${RNDSEED}         \
+              --idA=${CONFIG_NUCLEUSA}  \
+              --idB=${CONFIG_NUCLEUSB}  \
+              --eCM=${CONFIG_ENERGY}    \
+              --process=jets            \
+              --ptHatMin=${PTHATMIN}    \
+              --ptHatMax=${PTHATMAX}
 
-# embed signal into background
+# Generate signal 
+taskwrapper sgnsim.log o2-sim -j ${NWORKERS} -n ${NSIGEVENTS}          \
+          -g pythia8 -m ${MODULES}                                     \
+          --configKeyValues "GeneratorPythia8.config=pythia8_jets.cfg" \
+          -o sgn
 
-o2-sim -j ${NWORKERS} -n ${SIGEVENTS} -g pythia8 -m ${MODULES} \
-       --configKeyValues "GeneratorPythia8.config=pythia8_jets.cfg" \
-       > log 2>&1
+# We need to exit for the ALIEN JOB HANDLER!
+exit 0
