d95e9720de039daa43fdf8c02c0d5ee63e9edaa1
Sandro Wenzel <sandro.wenzel@cern.ch>
2021-01-27 16:58:39 +0100

    Revert "load taskwrapper only when not already there"
    This reverts commit b9bd930accb604df95e24b217cd8d9da20e51ebd.
    Doesn't work as expected.

--- MC/run/PWGHF/embedding_benchmark.sh
+++ MC/run/PWGHF/embedding_benchmark.sh
@@ -11,9 +11,7 @@
 [ ! "${O2_ROOT}" ] && echo "Error: This needs O2 loaded" && exit 1
 
 # ----------- LOAD UTILITY FUNCTIONS --------------------------
-if [ $(typeset -F taskwrapper) ]; then
- . ${O2_ROOT}/share/scripts/jobutils.sh
-done
+. ${O2_ROOT}/share/scripts/jobutils.sh
 
 # ----------- START ACTUAL JOB  ----------------------------- 
 
