d0435c133e0b5c9950082c7c7bd3328c2d75bb83
Roberto Preghenella <preghenella@bo.infn.it>
2021-02-26 08:39:14 +0100

    Simple template utility macro to read HepMC data

---
+++ MC/utils/hepmc_reader.C
@@ -0,0 +1,34 @@
+void hepmc_reader(const char* fname, int version = 2);
+void user_code(const std::vector<TParticle>& particles);
+
+void
+hepmc_reader(const char* fname, int version)
+{
+  
+  /** instance and initialise genarator HepMC **/
+  auto reader = new o2::eventgen::GeneratorHepMC;
+  reader->setFileName(fname);
+  reader->setVersion(version);
+  if (!reader->Init())
+    return;
+  
+  /** loop over events **/
+  while (reader->generateEvent() && reader->importParticles()) {
+    
+    /** get particles **/
+    auto& particles = reader->getParticles();
+    
+    /** execute user code **/
+    user_code(particles);
+    
+  }
+  
+}
+
+void
+user_code(const std::vector<TParticle>& particles)
+{
+  for (auto& particle : particles)
+    particle.Print();
+}
+
