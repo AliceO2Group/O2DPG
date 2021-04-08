38c9f66868c562a68dda3fc6601c6201a0b15129
Roberto Preghenella <preghenella@bo.infn.it>
2021-04-08 09:24:30 +0200

    Temptative addition of PWGDQ/GeneratorParam

---
+++ MC/config/PWGDQ/external/generator/GeneratorParam.C
@@ -0,0 +1,40 @@
+namespace o2 {
+namespace eventgen {
+
+class O2_GeneratorParam : public GeneratorTGenerator
+{
+
+public:
+  
+  O2_GeneratorParam() : GeneratorTGenerator("Param") {
+    param = new GeneratorParam(10, new GeneratorParamMUONlib(), GeneratorParamMUONlib::kJpsiFamily, "Vogt PbPb");
+    param->SetPtRange(0, 100);
+    param->SetYRange(-1., +1.);
+    param->SetDecayer(new TPythia6Decayer());
+    param->SetForceDecay(kDiElectron);
+    setTGenerator(param);
+  };
+
+  ~O2_GeneratorParam() {
+    delete param;
+  };
+
+  Bool_t Init() override {
+    GeneratorTGenerator::Init();
+    param->Init();
+    return true;
+  }
+  
+private:
+
+  GeneratorParam *param = nullptr;
+
+};
+
+}}
+
+FairGenerator*
+Get_O2_GeneratorParam()
+{
+  return new o2::eventgen::O2_GeneratorParam;
+}
