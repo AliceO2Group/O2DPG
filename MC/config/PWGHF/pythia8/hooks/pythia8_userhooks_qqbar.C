bcd2f4c3dd5edde27f6e0afa8ccd6ae980ac422c
Roberto Preghenella <preghenella@bo.infn.it>
2021-04-10 16:01:37 +0200

    Extend HF ccbar embedding becnhmark to bbbar as well

--- MC/config/PWGHF/pythia8/hooks/pythia8_userhooks_qqbar.C
+++ MC/config/PWGHF/pythia8/hooks/pythia8_userhooks_qqbar.C
@@ -1,18 +1,18 @@
 /// \author R+Preghenella - July 2020
 
 /// This Pythia8 UserHooks can veto the processing at parton level.
-/// The partonic event is scanned searching for a c-cbar mother
+/// The partonic event is scanned searching for a q-qbar mother
 /// with at least one of the c quarks produced withing a fiducial
 /// window around midrapidity that can be specified by the user.
 
 #include "Pythia8/Pythia.h"
 
-class UserHooks_ccbar : public Pythia8::UserHooks
+class UserHooks_qqbar : public Pythia8::UserHooks
 {
   
  public:
-  UserHooks_ccbar() = default;
-  ~UserHooks_ccbar() = default;
+  UserHooks_qqbar() = default;
+  ~UserHooks_qqbar() = default;
   bool canVetoPartonLevel() override { return true; };
   bool doVetoPartonLevel(const Pythia8::Event& event) override {
     // search for c-cbar mother with at least one c at midrapidity
@@ -20,8 +20,8 @@ class UserHooks_ccbar : public Pythia8::UserHooks
       auto daughterList = event[ipa].daughterList();
       bool hasc = false, hascbar = false, atmidy = false;
       for (auto ida : daughterList) {
-	if (event[ida].id() == 4) hasc = true;
-	if (event[ida].id() == -4) hascbar = true;
+	if (event[ida].id() == mPDG) hasc = true;
+	if (event[ida].id() == -mPDG) hascbar = true;
 	if (fabs(event[ida].y()) < mRapidity) atmidy = true;
       }
       if (hasc && hascbar && atmidy)
@@ -30,10 +30,12 @@ class UserHooks_ccbar : public Pythia8::UserHooks
     return true; // did not find it, veto event
   };
 
+  void setPDG(int val) { mPDG = val; };
   void setRapidity(double val) { mRapidity = val; };
   
 private:
 
+  int mPDG = 4;
   double mRapidity = 1.5;
   
 };
@@ -41,7 +43,18 @@ private:
 Pythia8::UserHooks*
   pythia8_userhooks_ccbar(double rapidity = 1.5)
 {
-  auto hooks = new UserHooks_ccbar();
+  auto hooks = new UserHooks_qqbar();
+  hooks->setPDG(4);
   hooks->setRapidity(rapidity);
   return hooks;
 }
+
+Pythia8::UserHooks*
+  pythia8_userhooks_bbbar(double rapidity = 1.5)
+{
+  auto hooks = new UserHooks_qqbar();
+  hooks->setPDG(5);
+  hooks->setRapidity(rapidity);
+  return hooks;
+}
+
