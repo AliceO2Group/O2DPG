481341fb10a560b36ba8be058f000bd9ef6d0742
David Rohr <drohr@jwdt.org>
2021-09-01 15:56:08 +0200

    Add TPC workflow

---
+++ testing/detectors/TPC/tpc-workflow.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+source common/setenv.sh
+
+SEVERITY=warning
+ARGS_ALL="--session default --severity $SEVERITY --shm-segment-id $NUMAID --shm-segment-size $SHMSIZE"
+if [ $EPNMODE == 1 ]; then
+  ARGS_ALL+=" --infologger-severity $SEVERITY"
+  #ARGS_ALL+=" --monitoring-backend influxdb-unix:///tmp/telegraf.sock"
+else
+  ARGS_ALL+=" --monitoring-backend no-op://"
+fi
+if [ $SHMTHROW == 0 ]; then
+  ARGS_ALL+=" --shm-throw-bad-alloc 0"
+fi
+if [ $NORATELOG == 1 ]; then
+  ARGS_ALL+=" --fairmq-rate-logging 0"
+fi
+if [ $NUMAGPUIDS != 0 ]; then
+  ARGS_ALL+=" --child-driver 'numactl --membind $NUMAID --cpunodebind $NUMAID'"
+fi
+if [ $GPUTYPE != "CPU" ]; then
+  ARGS_ALL+="  --shm-mlock-segment-on-creation 1"
+fi
+ARGS_ALL_CONFIG="NameConf.mDirGRP=$FILEWORKDIR;NameConf.mDirGeom=$FILEWORKDIR;NameConf.mDirCollContext=$FILEWORKDIR;NameConf.mDirMatLUT=$FILEWORKDIR;keyval.input_dir=$FILEWORKDIR;keyval.output_dir=/dev/null"
+
+if [ $GPUTYPE == "HIP" ]; then
+  if [ $NUMAID == 0 ] || [ $NUMAGPUIDS == 0 ]; then
+    export TIMESLICEOFFSET=0
+  else
+    export TIMESLICEOFFSET=$NGPUS
+  fi
+  GPU_CONFIG_KEY+="GPU_proc.deviceNum=0;GPU_global.mutexMemReg=true;"
+  GPU_CONFIG+=" --environment \"ROCR_VISIBLE_DEVICES={timeslice${TIMESLICEOFFSET}}\""
+  export HSA_NO_SCRATCH_RECLAIM=1
+  #export HSA_TOOLS_LIB=/opt/rocm/lib/librocm-debug-agent.so.2
+else
+  GPU_CONFIG_KEY+="GPU_proc.deviceNum=-2;"
+fi
+
+if [ $GPUTYPE != "CPU" ]; then
+  GPU_CONFIG_KEY+="GPU_proc.forceMemoryPoolSize=$GPUMEMSIZE;"
+  if [ $HOSTMEMSIZE == "0" ]; then
+    HOSTMEMSIZE=$(( 1 << 30 ))
+  fi
+fi
+if [ $HOSTMEMSIZE != "0" ]; then
+  GPU_CONFIG_KEY+="GPU_proc.forceHostMemoryPoolSize=$HOSTMEMSIZE;"
+fi
+
+PROXY_INSPEC="A:TPC/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0;eos:***/INFORMATION"
+CALIB_INSPEC="A:TPC/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0;eos:***/INFORMATION"
+
+o2-dpl-raw-proxy $ARGS_ALL \
+    --dataspec "$PROXY_INSPEC" \
+    --readout-proxy "--channel-config 'name=readout-proxy,type=pull,method=connect,address=ipc://@$INRAWCHANNAME,transport=shmem,rateLogging=1'" \
+    | o2-tpc-raw-to-digits-workflow $ARGS_ALL \
+    --input-spec "$CALIB_INSPEC"  \
+    --remove-duplicates \
+    --pipeline tpc-raw-to-digits-0:6 \
+    --configKeyValues "$ARGS_ALL_CONFIG;TPCDigitDump.LastTimeBin=1000;" \
+    | o2-tpc-reco-workflow $ARGS_ALL \
+    --input-type digitizer  \
+    --output-type clusters,tracks,encoded-clusters disable-writer \
+    --disable-mc \
+    --pipeline tpc-tracker:4 \
+    $GPU_CONFIG \
+    --configKeyValues "$ARGS_ALL_CONFIG;$GPU_CONFIG_KEY;align-geom.mDetectors=none;GPU_global.deviceType=$GPUTYPE;GPU_proc.tpcIncreasedMinClustersPerRow=500000;GPU_proc.ignoreNonFatalGPUErrors=1;" \
+    | o2-eve-display $ARGS_ALL --display-tracks TPC --display-clusters TPC --disable-mc --jsons-folder /home/ed/jsons --eve-dds-collection-index 0 --configKeyValues "$ARGS_ALL_CONFIG" \
+    | o2-ctf-writer-workflow $ARGS_ALL --configKeyValues "$ARGS_ALL_CONFIG" --output-dir $CTF_DIR --ctf-dict-dir $FILEWORKDIR --output-type ctf --onlyDet TPC \
+    | o2-dpl-run $ARGS_ALL --dds
+
+#HOST=localhost
+#| o2-qc $ARGS_ALL --config json:///home/epn/odc/files/tpcQCTasks_multinode_ALL.json --local --host $HOST \
+
