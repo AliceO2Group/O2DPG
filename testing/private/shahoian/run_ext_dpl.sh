67632e3298aedf8a29f45c3e0331c1d13dc02137
shahoian <ruben.shahoyan@cern.ch>
2021-09-21 08:55:28 +0200

    wf descriptions based on master dpl-workflow.sh

---
+++ testing/private/shahoian/run_ext_dpl.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export GEN_TOPO_PARTITION=test                                       # ECS Partition
+export DDMODE=processing                                             # DataDistribution mode - possible options: processing, disk, processing-disk, discard
+
+# Use these settings to fetch the Workflow Repository using a hash / tag
+#export GEN_TOPO_HASH=1                                              # Fetch O2DataProcessing repository using a git hash
+#export GEN_TOPO_SOURCE=v0.5                                         # Git hash to fetch
+
+# Use these settings to specify a path to the workflow repository in your home dir
+export GEN_TOPO_HASH=0                                               # Specify path to O2DataProcessing repository
+export GEN_TOPO_SOURCE=/home/shahoian/alice/O2DataProcessing         # Path to O2DataProcessing repository
+
+export EXTINPUT=1
+export EPNMODE=1
+
+export GEN_TOPO_LIBRARY_FILE=testing/private/shahoian/workflows_dpl.desc # Topology description library file to load
+export WORKFLOW_DETECTORS=ALL                                        # Optional parameter for the workflow: Detectors to run reconstruction for (comma-separated list)
+export WORKFLOW_DETECTORS_QC=ALL                                     # Optional parameter for the workflow: Detectors to run QC for
+export WORKFLOW_DETECTORS_CALIB=                                     # Optional parameters for the workflow: Detectors to run calibration for
+export WORKFLOW_PARAMETERS=QC,CTF,GPU                                # Additional paramters for the workflow: QC, CTF, GPU
+export RECO_NUM_NODES_OVERRIDE=0                                     # Override the number of EPN compute nodes to use (default is specified in description library file)
+export NHBPERTF=128                                                  # Number of HBF per TF
+export ALL_EXTRA_CONFIG="HBFUtils.nHBFPerTF=$NHBPERTF"
+
+export GEN_TOPO_IGNORE_ERROR=1
+
+for wf in "$@"
+do
+ export GEN_TOPO_WORKFLOW_NAME=$wf
+ /home/epn/pdp/gen_topo.sh > $HOME/gen_topo/dpl/${GEN_TOPO_WORKFLOW_NAME}.xml
+done
