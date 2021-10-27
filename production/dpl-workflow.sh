1ec1567f8cba893d6ccaede34df7c6997b2c328d
David Rohr <drohr@jwdt.org>
2021-10-27 19:01:18 +0200

    Fix typo and simplify

--- production/dpl-workflow.sh
+++ production/dpl-workflow.sh
@@ -310,9 +310,7 @@ elif [ $RAWTFINPUT == 1 ]; then
   if [ $NTIMEFRAMES == -1 ]; then NTIMEFRAMES_CMD= ; else NTIMEFRAMES_CMD="--max-tf $NTIMEFRAMES"; fi
   add_W o2-raw-tf-reader-workflow "--delay $TFDELAY --loop $TFLOOP $NTIMEFRAMES_CMD --input-data ${TFName} ${INPUT_FILE_COPY_CMD+--copy-cmd} ${INPUT_FILE_COPY_CMD} --onlyDet $WORKFLOW_DETECTORS"
 elif [ $EXTINPUT == 1 ]; then
-  PROXY_CHANNEL="name=readout-proxy,type=pull,method=connect,address=ipc://@$INRAWCHANNAME,transport=shmem"
-  [[ $EPNMODE == 1 ]] && PROXY_CHANNEL+=",rateLogging=0"
-  [[ $EPNMODE == 1 ]] && PROXY_CHANNEL+=",rateLogging=1"
+  PROXY_CHANNEL="name=readout-proxy,type=pull,method=connect,address=ipc://@$INRAWCHANNAME,transport=shmem,rateLogging=$EPNMODE"
   PROXY_INSPEC="dd:FLP/DISTSUBTIMEFRAME/0;eos:***/INFORMATION"
   PROXY_IN_N=0
   for i in `echo "$WORKFLOW_DETECTORS" | sed "s/,/ /g"`; do
