ab33e48303c4cf06e3403bbdffdcba7b4055d81e
Piotr Konopka <piotr.jan.konopka@cern.ch>
2021-11-15 16:42:51 +0100

    [QC-456] Allow to merge QC results across grid sub-jobs
    This commit simplifies adding new QC tasks and makes it possible to split
    the QC workflow into two independent parts, thus allow to merge QC objects across sub-jobs
    before running the finalization part.
    
    The way to run the workflow on one machine stays the same.
    To run on grid, one should:
    - generate and run the o2dpg sim workflow with --include-local-qc.
    - run `o2-qc-file-merger` to merge QC objects across sub-jobs.
    - generate and run the QC finalization workflow with the merged objects. It will run
     the checks and upload objects to QCDB.

---
+++ MC/bin/o2dpg_qc_finalization_workflow.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+#
+# A script producing the QC finalization workflow.
+# If run as main, it will dump the workflow to the specified output file and tasks will not have dependencies.
+# For example:
+#   ${O2DPG_ROOT}/MC/bin/o2dpg_qc_finalization_workflow.py -o qc_workflow.json 
+
+
+# The script can be also imported.
+# In such case, one can use include_all_QC_finalization to get the QC finalization from within other workflow script.
+
+import sys
+import argparse
+from os import environ, mkdir
+from os.path import join, dirname, isdir
+
+sys.path.append(join(dirname(__file__), '.', 'o2dpg_workflow_utils'))
+
+from o2dpg_workflow_utils import createTask, dump_workflow
+
+def getDPL_global_options(bigshm=False, noIPC=None):
+   common="-b --run --fairmq-ipc-prefix ${FAIRMQ_IPC_PREFIX:-./} --driver-client-backend ws:// "
+   if noIPC != None:
+      return common + " --no-IPC "
+   if bigshm:
+      return common + " --shm-segment-size ${SHMSIZE:-50000000000} "
+   else:
+      return common
+
+qcdir = "QC"
+def include_all_QC_finalization(ntimeframes, standalone):
+
+  stages = []
+  def add_QC_finalization(taskName, qcConfigPath, needs=[]):
+    if len(needs) == 0 and standalone == False:
+      needs = [taskName + '_local' + str(tf) for tf in range(1, ntimeframes + 1)]
+    task = createTask(name=taskName + '_finalize', needs=needs, cwd=qcdir, lab=["QC"], cpu=1, mem='2000')
+    task['cmd'] = 'o2-qc --config ' + qcConfigPath + ' --remote-batch ' + taskName + '.root ' + getDPL_global_options()
+    stages.append(task)
+
+
+  # to be enabled once MFT Digits should be ran 5 times with different settings
+  #MFTDigitsQCneeds = []
+  #for flp in range(5):
+  #  MFTDigitsQCneeds.extend(['mftDigitsQC_local_'+str(flp)+'_'+str(tf) for tf in range(1, ntimeframes + 1)])
+  #
+  #add_QC_finalization('mftDigitsQC', 'json://${O2DPG_ROOT}/MC/config/QC/json/qc-mft-digit.json', MFTDigitsQCneeds)
+  add_QC_finalization('mftDigitsQC', 'json://${O2DPG_ROOT}/MC/config/QC/json/qc-mft-digit.json')
+  add_QC_finalization('mftClustersQC', 'json://${O2DPG_ROOT}/MC/config/QC/json/qc-mft-cluster.json')
+  add_QC_finalization('mftTracksQC', 'json://${O2DPG_ROOT}/MC/config/QC/json/qc-mft-track.json')
+  #add_QC_finalization('tpcTrackingQC', 'json://${O2DPG_ROOT}/MC/config/QC/json/tpc-qc-tracking-direct.json')
+  add_QC_finalization('trdDigitsQC', 'json://${O2DPG_ROOT}/MC/config/QC/json/trd-digits-task.json')
+  add_QC_finalization('vertexQC', 'json://${O2DPG_ROOT}/MC/config/QC/json/vertexing-qc-direct-mc.json')
+  add_QC_finalization('ITSTPCmatchQC', 'json://${O2DPG_ROOT}/MC/config/QC/json/ITSTPCmatchedTracks_direct_MC.json')
+  add_QC_finalization('TOFMatchQC', 'json://${O2DPG_ROOT}/MC/config/QC/json/tofMatchedTracks_ITSTPCTOF_TPCTOF_direct_MC.json')
+
+  return stages
+
+
+def main() -> int:
+  
+  parser = argparse.ArgumentParser(description='Create the ALICE QC finalization workflow')
+
+  parser.add_argument('--noIPC',help='disable shared memory in DPL')
+  parser.add_argument('-o',help='output workflow file', default='workflow.json')
+
+  args = parser.parse_args()
+  print (args)
+
+ # make sure O2DPG + O2 is loaded
+  O2DPG_ROOT=environ.get('O2DPG_ROOT')
+  O2_ROOT=environ.get('O2_ROOT')
+  QUALITYCONTROL_ROOT=environ.get('QUALITYCONTROL_ROOT')
+
+  if O2DPG_ROOT == None: 
+    print('Error: This needs O2DPG loaded')
+
+  if O2_ROOT == None: 
+    print('Error: This needs O2 loaded')
+
+  if QUALITYCONTROL_ROOT is None:
+    print('Error: This needs QUALITYCONTROL_ROOT loaded')
+
+  if not isdir(qcdir):
+    mkdir(qcdir)
+
+  workflow={}
+  workflow['stages'] = include_all_QC_finalization(ntimeframes=1, standalone=True)
+  
+  dump_workflow(workflow["stages"], args.o)
+  
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main())
