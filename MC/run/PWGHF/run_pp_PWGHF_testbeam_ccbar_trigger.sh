9a0f23ab2a613a20fea424dc102236fce57cdb8b
Marianna Mazzilli <marianna.mazzilli@cern.ch>
2021-12-21 11:27:32 +0100

    trigger config and run

---
+++ MC/run/PWGHF/run_pp_PWGHF_testbeam_ccbar_trigger.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+# A example workflow MC->RECO->AOD for a simple pp min bias 
+# production, targetting test beam conditions.
+
+# make sure O2DPG + O2 is loaded
+[ ! "${O2DPG_ROOT}" ] && echo "Error: This needs O2DPG loaded" && exit 1
+[ ! "${O2_ROOT}" ] && echo "Error: This needs O2 loaded" && exit 1
+
+# ----------- LOAD UTILITY FUNCTIONS --------------------------
+. ${O2_ROOT}/share/scripts/jobutils.sh
+
+# ----------- START ACTUAL JOB  ----------------------------- 
+
+NWORKERS=${NWORKERS:-8}
+MODULES="--skipModules ZDC"
+SIMENGINE=${SIMENGINE:-TGeant4}
+NBKGEVENTS=${NBKGEVENTS:-1}
+
+#ccbar trigger
+${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM 900 -col pp -gen pythia8 -proc "ccbar" -tf 25 \
+	                                            -ns 110 -e ${SIMENGINE}                  \
+						    -j ${NWORKERS} -interactionRate 10000 \
+						    -confKey "Diamond.width[2]=6."        \
+						    -trigger "external" -ini "/home/mmazzill/trigger/trigger_hf.ini" #"$O2DPG_ROOT/MC/config/PWGHF/ini/trigger_hf.ini"
+
+
+# run workflow
+# allow increased timeframe parallelism with --cpu-limit 32 
+${O2DPG_ROOT}/MC/bin/o2_dpg_workflow_runner.py -f workflow.json -tt aod --cpu-limit 32
+
+
+
