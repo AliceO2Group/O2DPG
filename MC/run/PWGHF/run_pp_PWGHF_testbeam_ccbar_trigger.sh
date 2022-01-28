60de8b7b7601845588562a309a93faeb31981c0b
Marianna Mazzilli <marianna.mazzilli@cern.ch>
2022-01-28 11:40:15 +0100

    fix tf

--- MC/run/PWGHF/run_pp_PWGHF_testbeam_ccbar_trigger.sh
+++ MC/run/PWGHF/run_pp_PWGHF_testbeam_ccbar_trigger.sh
@@ -9,7 +9,7 @@
 [ ! "${O2_ROOT}" ] && echo "Error: This needs O2 loaded" && exit 1
 
 # ----------- LOAD UTILITY FUNCTIONS --------------------------
-#. ${O2_ROOT}/share/scripts/jobutils.sh
+. ${O2_ROOT}/share/scripts/jobutils.sh
 
 # ----------- START ACTUAL JOB  ----------------------------- 
 
