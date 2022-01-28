5fd27788127db82f6080e7b34985320c43cb1192
Marianna Mazzilli <marianna.mazzilli@cern.ch>
2022-01-28 11:30:58 +0100

    fix process

--- MC/run/PWGHF/run_pp_PWGHF_testbeam_ccbar_trigger.sh
+++ MC/run/PWGHF/run_pp_PWGHF_testbeam_ccbar_trigger.sh
@@ -9,17 +9,17 @@
 [ ! "${O2_ROOT}" ] && echo "Error: This needs O2 loaded" && exit 1
 
 # ----------- LOAD UTILITY FUNCTIONS --------------------------
-. ${O2_ROOT}/share/scripts/jobutils.sh
+#. ${O2_ROOT}/share/scripts/jobutils.sh
 
 # ----------- START ACTUAL JOB  ----------------------------- 
 
 NWORKERS=${NWORKERS:-8}
 MODULES="--skipModules ZDC"
 SIMENGINE=${SIMENGINE:-TGeant4}
-NBKGEVENTS=${NBKGEVENTS:-1}
+#NBKGEVENTS=${NBKGEVENTS:-1}
 
 #ccbar trigger
-${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM 900 -col pp -gen pythia8 -proc "ccbar" -tf 25 \
+${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM 900 -col pp -gen pythia8 -proc "inel" -tf 1 \
 	                                            -ns 110 -e ${SIMENGINE}                  \
 						    -j ${NWORKERS} -interactionRate 10000 \
 						    -confKey "Diamond.width[2]=6."        \
