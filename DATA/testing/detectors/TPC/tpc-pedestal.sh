70fe646106b392bbff2d37c5eaaf15b1a9131e66
rmunzer <97919772+rmunzer@users.noreply.github.com>
2022-02-08 08:45:01 +0100

    Tpc calib workflows (#205)
    * Add Basisc scripts from TPC Standalone runs.
    
    * Change Naming of TPC workflows
    Include workflow in workflows.desc
    
    * Correct name of scipt in workflow.desc
    
    * tpc-pulser, tpc-pedestal, tpc-laser workflow production ok.
    tpc-pulser, tpc-pedestal tested sucessfully with AliECS
    
    * tpc-laser tested in AliECS
    tpc-krypton/ tpy-krypton-raw / tpc-standalone creation ok.
    
    * Update of workflows
    
    * Fixed few error in standalone and laser script.
    All scripts are runnng now with the local built, also with GPU disabled.
    
    Co-authored-by: Robert Helmut Munzer <rmunzer@login.internal>
    Co-authored-by: Robert Helmut Munzer <rmunzer@epn-test01.internal>

---
+++ DATA/testing/detectors/TPC/tpc-pedestal.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+source common/setenv.sh
+
+ARGS_ALL="--session default --severity $SEVERITY --shm-segment-id $NUMAID --shm-segment-size $SHMSIZE"
+if [ $EPNSYNCMODE == 1 ]; then
+  ARGS_ALL+=" --infologger-severity $INFOLOGGER_SEVERITY"
+  #ARGS_ALL+=" --monitoring-backend influxdb-unix:///tmp/telegraf.sock"
+  ARGS_ALL+=" --monitoring-backend no-op://"
+else
+  ARGS_ALL+=" --monitoring-backend no-op://"
+fi
+if [ $SHMTHROW == 0 ]; then
+  ARGS_ALL+=" --shm-throw-bad-alloc 0"
+fi
+if [ $NORATELOG == 1 ]; then
+  ARGS_ALL+=" --fairmq-rate-logging 0"
+fi
+if [ $NUMAGPUIDS != 0 ]; then
+  ARGS_ALL+=" --child-driver 'numactl --membind $NUMAID --cpunodebind $NUMAID'"
+fi
+
+PROXY_INSPEC="A:TPC/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0;eos:***/INFORMATION"
+CALIB_INSPEC="A:TPC/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0;eos:***/INFORMATION"
+
+
+
+CALIB_CONFIG="TPCCalibPedestal.LastTimeBin=12000"
+EXTRA_CONFIG=" "
+EXTRA_CONFIG=" --publish-after-tfs 100 --max-events 120 --lanes 36"
+CCDB_PATH="--ccdb-path http://ccdb-test.cern.ch:8080"
+
+o2-dpl-raw-proxy $ARGS_ALL \
+    --dataspec "$PROXY_INSPEC" \
+    --readout-proxy '--channel-config "name=readout-proxy,type=pull,method=connect,address=ipc://@tf-builder-pipe-0,transport=shmem,rateLogging=1"' \
+    | o2-tpc-calib-pad-raw $ARGS_ALL \
+    --input-spec "$CALIB_INSPEC" \
+    --configKeyValues "$CALIB_CONFIG;keyval.output_dir=/dev/null" \
+    $EXTRA_CONFIG \
+    | o2-calibration-ccdb-populator-workflow $ARGS_ALL \
+    $CCDB_PATH \
+    | o2-dpl-run $ARGS_ALL --dds
