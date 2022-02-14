2d451f60b1d5ab837e4bf00c28daa3117a06be5f
Rita Sadek <rita.sadek@cern.ch>
2022-02-14 12:25:28 +0100

    Enable option for saving parameters at plane for matching training with machine learning (#215)
    * Enable developer option for saving parameters at plane for matching training with machine learning (MFT)
    
    Co-authored-by: Rafael Pezzi <rafael.pezzi@cern.ch>

--- MC/run/PWGDQ/runBeautyToJpsi_fwdy_PbPb.sh
+++ MC/run/PWGDQ/runBeautyToJpsi_fwdy_PbPb.sh
@@ -16,7 +16,7 @@ NTIMEFRAMES=${NTIMEFRAMES:-1}
 
 ${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM 5020 -gen external -j ${NWORKERS} -ns ${NSIGEVENTS} -tf ${NTIMEFRAMES} -e TGeant4 -mod "--skipModules ZDC" \
 	-trigger "external" -ini $O2DPG_ROOT/MC/config/PWGDQ/ini/GeneratorHF_bbbar_fwdy.ini  \
-        -genBkg pythia8 -procBkg "heavy_ion" -colBkg PbPb --embedding -nb ${NBKGEVENTS} --mft-reco-full --mft-assessment-full --fwdmatching-assessment-full
+        -genBkg pythia8 -procBkg "heavy_ion" -colBkg PbPb --embedding -nb ${NBKGEVENTS} --mft-reco-full --mft-assessment-full --fwdmatching-assessment-full --fwdmatching-save-trainingdata
 
 # run workflow
 ${O2DPG_ROOT}/MC/bin/o2_dpg_workflow_runner.py -f workflow.json
