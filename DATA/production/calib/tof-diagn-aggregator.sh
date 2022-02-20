a053a08c1ac3dcd916c79eef8124a59a7aacc302
noferini <noferini@bo.infn.it>
2022-02-20 18:49:22 +0100

    fix for sync calib to update TOF ccdb asap

--- DATA/production/calib/tof-diagn-aggregator.sh
+++ DATA/production/calib/tof-diagn-aggregator.sh
@@ -18,7 +18,7 @@ ARGS_ALL_CONFIG="keyval.input_dir=$FILEWORKDIR;keyval.output_dir=/dev/null;$ALL_
 PROXY_INSPEC="diagWords:TOF/DIAFREQ/0;eos:***/INFORMATION"
 
 WORKFLOW="o2-dpl-raw-proxy $ARGS_ALL --proxy-name tof-diagn-input-proxy --dataspec \"$PROXY_INSPEC\" --network-interface ib0 --channel-config \"name=tof-diagn-input-proxy,method=bind,type=pull,rateLogging=0,transport=zeromq\" | "
-WORKFLOW+="o2-calibration-tof-diagnostic-workflow --tf-per-slot 25000 --max-delay 10 $ARGS_ALL | "
+WORKFLOW+="o2-calibration-tof-diagnostic-workflow --tf-per-slot 25000 --max-delay 1 $ARGS_ALL | "
 WORKFLOW+="o2-calibration-ccdb-populator-workflow $ARGS_ALL --configKeyValues \"$ARGS_ALL_CONFIG\" --ccdb-path=\"http://ccdb-test.cern.ch:8080\" | "
 WORKFLOW+="o2-dpl-run $ARGS_ALL $GLOBALDPLOPT"
 
