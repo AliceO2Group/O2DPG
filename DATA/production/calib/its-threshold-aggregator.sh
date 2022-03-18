8a19aafa1da06f35a47724aba58cc0106fe0f12b
iravasen <ivan.ravasenga@cern.ch>
2022-03-18 19:24:49 +0100

    adapt ITS threshold scripts to PR8346 in O2 + added QC

--- DATA/production/calib/its-threshold-aggregator.sh
+++ DATA/production/calib/its-threshold-aggregator.sh
@@ -18,7 +18,7 @@ ARGS_ALL_CONFIG="NameConf.mDirGRP=$FILEWORKDIR;NameConf.mDirGeom=$FILEWORKDIR;Na
 PROXY_INSPEC="tunestring:ITS/TSTR/0;runtype:ITS/RUNT/0;fittype:ITS/FITT/0;scantype:ITS/SCANT/0;eos:***/INFORMATION"
 
 WORKFLOW="o2-dpl-raw-proxy $ARGS_ALL --proxy-name its-thr-input-proxy --dataspec \"$PROXY_INSPEC\" --network-interface ib0 --channel-config \"name=its-thr-input-proxy,method=bind,type=pull,rateLogging=0,transport=zeromq\" | "
-WORKFLOW+="o2-its-threshold-aggregator-workflow -b --ccdb-url=\"http://alio2-cr1-flp199.cern.ch:8083\" $ARGS_ALL | "
+WORKFLOW+="o2-its-threshold-aggregator-workflow -b --ccdb-url=\"\" $ARGS_ALL | "
 WORKFLOW+="o2-calibration-ccdb-populator-workflow $ARGS_ALL --configKeyValues \"$ARGS_ALL_CONFIG\" --ccdb-path=\"http://alio2-cr1-flp199.cern.ch:8083\" | "
 WORKFLOW+="o2-dpl-run $ARGS_ALL $GLOBALDPLOPT"
 
