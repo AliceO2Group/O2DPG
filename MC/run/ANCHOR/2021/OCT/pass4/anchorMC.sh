7325fa8a78ff878fbea452f987bf06496ca637e9
Catalin Ristea <catalin.ristea@Catalins-MacBook-Pro.local>
2022-03-21 16:43:42 +0200

    LPMProductionTag swapping, full logs for grid, local qc

--- MC/run/ANCHOR/2021/OCT/pass4/anchorMC.sh
+++ MC/run/ANCHOR/2021/OCT/pass4/anchorMC.sh
@@ -50,7 +50,12 @@ sed -i 's/JDL_ANCHORYEAR/JDL_LPMANCHORYEAR/' async_pass.sh
 # create workflow ---> creates the file that can be parsed
 export IGNORE_EXISTING_SHMFILES=1
 touch list.list
+ALIEN_JDL_LPMPRODUCTIONTAG_KEEP=$ALIEN_JDL_LPMPRODUCTIONTAG
+echo "Substituting ALIEN_JDL_LPMPRODUCTIONTAG=$ALIEN_JDL_LPMPRODUCTIONTAG with ALIEN_JDL_LPMANCHORPRODUCTION=$ALIEN_JDL_LPMANCHORPRODUCTION for simulating reco pass..."
+ALIEN_JDL_LPMPRODUCTIONTAG=$ALIEN_JDL_LPMANCHORPRODUCTION
 ./async_pass.sh
+ALIEN_JDL_LPMPRODUCTIONTAG=$ALIEN_JDL_LPMPRODUCTIONTAG_KEEP
+echo "Setting back ALIEN_JDL_LPMPRODUCTIONTAG to $ALIEN_JDL_LPMPRODUCTIONTAG"
 
 # now create the local MC config file --> config-config.json
 ${O2DPG_ROOT}/UTILS/parse-async-WorkflowConfig.py
@@ -80,11 +85,14 @@ baseargs="-tf ${NTIMEFRAMES} --split-id ${ALIEN_JDL_SPLITID:-0} --prod-split ${A
 remainingargs="-eCM 900 -col pp -gen pythia8 -proc inel -ns ${NSIGEVENTS}                                                                                                 \
                -interactionRate 2000                                                                                                                                      \
                -confKey \"Diamond.width[2]=6.0;Diamond.width[0]=0.01;Diamond.width[1]=0.01;Diamond.position[0]=0.0;Diamond.position[1]=-0.035;Diamond.position[2]=0.41\"  \
-              --include-qc --include-analysis"
+              --include-local-qc --include-analysis"
 
 remainingargs="${remainingargs} -e ${SIMENGINE} -j ${NWORKERS}"
 remainingargs="${remainingargs} -productionTag ${ALIEN_JDL_LPMPRODUCTIONTAG:-alibi_anchorTest_tmp}"
 remainingargs="${remainingargs} --anchor-config config-json.json"
+
+echo "baseargs: ${baseargs}"
+echo "remainingargs: ${remainingargs}"
               
 ${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow_anchored.py ${baseargs} -- ${remainingargs}
 # -- RUN THE MC WORKLOAD TO PRODUCE AOD --
@@ -99,7 +107,7 @@ if [ "${MCRC}" = "0" ]; then
   [ ${ALIBI_EXECUTOR_FRAMEWORK} ] && copy_ALIEN "*AO2D*"
 
   # do QC tasks
-  if [[ "${remainingargs}" == *"--include-qc"* ]]; then
+  if [[ "${remainingargs}" == *"--include-local-qc"* ]]; then
     echo "Doing QC"
     ${O2DPG_ROOT}/MC/bin/o2_dpg_workflow_runner.py -f workflow.json --target-labels QC --cpu-limit ${ALIEN_JDL_CPULIMIT:-8}
     RC=$?
@@ -122,6 +130,14 @@ if [ ${ALIBI_EXECUTOR_FRAMEWORK} ]; then
   find ./ -name "localhos*_*" -delete
   tar -czf mcarchive.tar.gz workflow.json tf* QC pipeline*
   copy_ALIEN mcarchive.tar.gz
+fi
+
+#
+# full logs tar-ed for output, regardless the error code or validation - to catch also QC logs...
+#
+if [[ -n "$ALIEN_PROC_ID" ]]; then
+  find ./ \( -name "*.log*" -o -name "*mergerlog*" -o -name "*serverlog*" -o -name "*workerlog*" \) | tar -czvf debug_log_archive.tgz -T -
+  #find ./ \( -name "*.log*" -o -name "*mergerlog*" -o -name "*serverlog*" -o -name "*workerlog*" \) | tar cfJ debug_log_archive.xz -T -
 fi
 
 unset FAIRMQ_IPC_PREFIX
