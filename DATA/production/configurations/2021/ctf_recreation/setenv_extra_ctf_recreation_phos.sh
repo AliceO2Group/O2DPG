ecde23036008d214501aedcb0cf806685c5a45f3
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2022-03-23 16:02:19 +0100

    Change permissions + separate detectors in CALO partition

---
+++ DATA/production/configurations/2021/ctf_recreation/setenv_extra_ctf_recreation_phos.sh
@@ -0,0 +1,6 @@
+export WORKFLOW_DETECTORS_CTF="CPV" 
+export WORKFLOW_DETECTORS="PHS"
+export ARGS_EXTRA_PROCESS_o2_ctf_writer_workflow=" --no-grp "
+export ARGS_EXTRA_PROCESS_o2_raw_tf_reader_workflow=" --raw-only-det all "
+export ARGS_EXTRA_PROCESS_o2_phos_reco_workflow=" --fitmethod semigaus "
+
