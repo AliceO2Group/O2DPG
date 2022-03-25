a711c0fc94f14ad3dd28261f6dabf947cd184d5c
noferini <noferini@bo.infn.it>
2022-03-25 14:28:45 +0100

    add TOF cal in global workflow

---
+++ DATA/production/dpl-cal-workflow.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "0$O2_ROOT" == "0" ]; then
+    echo O2 environment not loaded 1>&2
+    exit 1
+fi
+
+export DPL_CONDITION_BACKEND="http://o2-ccdb.internal"
+
+export TOF_PROXY_OUTSPEC="diagWords:TOF/DIAFREQ"
+
+source $O2_ROOT/prodtests/full-system-test/dpl-workflow.sh
