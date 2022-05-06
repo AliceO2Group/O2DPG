666b1a80b87b28bf5ace687d856fae32a65ff90c
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2022-05-06 12:33:37 +0200

    Using function for proxies

--- DATA/common/setenv_calib.sh
+++ DATA/common/setenv_calib.sh
@@ -64,27 +64,67 @@ if [[ $BEAMTYPE != "cosmic" ]] && [[ $FORCECALIBRATIONS != 1 ]] ; then
 fi
 
 # prim vtx
-if [[ $CALIB_PRIMVTX_MEANVTX == 1 ]] ; then add_semicolon_separated CALIBDATASPEC "pvtx:GLO/PVTX/0"; fi
+if [[ $CALIB_PRIMVTX_MEANVTX == 1 ]] ; then add_semicolon_separated CALIBDATASPEC_BARREL "pvtx:GLO/PVTX/0"; fi
 
 # TOF
-if [[ $CALIB_TOF_LHCPHASE == 1 ]] || [[ $CALIB_TOF_CHANNELOFFSETS == 1 ]]; then add_semicolon_separated CALIBDATASPEC "calibTOF:TOF/CALIBDATA/0"; fi
-if [[ $CALIB_TOF_DIAGNOSTICS == 1 ]]; then add_semicolon_separated CALIBDATASPEC "diagWords:TOF/DIAFREQ/0"; fi
+if [[ $CALIB_TOF_LHCPHASE == 1 ]] || [[ $CALIB_TOF_CHANNELOFFSETS == 1 ]]; then add_semicolon_separated CALIBDATASPEC_BARREL "calibTOF:TOF/CALIBDATA/0"; fi
+if [[ $CALIB_TOF_DIAGNOSTICS == 1 ]]; then add_semicolon_separated CALIBDATASPEC_BARREL "diagWords:TOF/DIAFREQ/0"; fi
 
 # EMC
 if [[ $CALIB_EMC_CHANNELCALIB == 1 ]]; then
-    add_semicolon_separated CALIBDATASPEC "cellsEMC:EMC/CELLS/0"
-    add_semicolon_separated CALIBDATASPEC "cellsTrgREMC:EMC/CELLSTRGR/0"
+    add_semicolon_separated CALIBDATASPEC_CALO "cellsEMC:EMC/CELLS/0"
+    add_semicolon_separated CALIBDATASPEC_CALO "cellsTrgREMC:EMC/CELLSTRGR/0"
 fi
 
 # PHS
 if [[ $CALIB_PHS_ENERGYCALIB == 1 ]] || [[ $CALIB_PHS_TURNONCALIB == 1 ]] || [[ $CALIB_PHS_RUNBYRUNCALIB == 1 ]]; then
-    add_semicolon_separated CALIBDATASPEC "clsPHS:PHS/CLUSTERS/0;"
-    add_semicolon_separated CALIBDATASPEC "clTRPHS:PHS/CLUSTERTRIGREC/0;"
+    add_semicolon_separated CALIBDATASPEC_CALO "clsPHS:PHS/CLUSTERS/0;"
+    add_semicolon_separated CALIBDATASPEC_CALO "clTRPHS:PHS/CLUSTERTRIGREC/0;"
 fi
-if [[ $CALIB_PHS_ENERGYCALIB == 1 ]]; then add_semicolon_separated CALIBDATASPEC "cluelementsPHS:PHS/CLUELEMENTS/0;"; fi
-if [[ $CALIB_PHS_BADMAPCALIB == 1 ]] || [[ $CALIB_PHS_TURNONCALIB == 1 ]]; then add_semicolon_separated CALIBDATASPEC "cellsPHS:PHS/CELLS/0;"; fi
-if [[ $CALIB_PHS_TURNONCALIB == 1 ]]; then add_semicolon_separated CALIBDATASPEC "cellsTRPHS:PHS/CELLTRIGREC/0;"; fi
+if [[ $CALIB_PHS_ENERGYCALIB == 1 ]]; then add_semicolon_separated CALIBDATASPEC_CALO "cluelementsPHS:PHS/CLUELEMENTS/0;"; fi
+if [[ $CALIB_PHS_BADMAPCALIB == 1 ]] || [[ $CALIB_PHS_TURNONCALIB == 1 ]]; then add_semicolon_separated CALIBDATASPEC_CALO "cellsPHS:PHS/CELLS/0;"; fi
+if [[ $CALIB_PHS_TURNONCALIB == 1 ]]; then add_semicolon_separated CALIBDATASPEC_CALO "cellsTRPHS:PHS/CELLTRIGREC/0;"; fi
 
 # printing for debug
-echo CALIBDATASPEC = $CALIBDATASPEC 1>&2
+echo CALIBDATASPEC_BARREL = $CALIBDATASPEC_BARREL 1>&2
+echo CALIBDATASPEC_CALO = $CALIBDATASPEC_CALO 1>&2
 
+# proxies properties
+PORT_BARREL=30453
+PORT_CALO=30454
+get_proxy_connection()
+{
+  if (( $# < 2 )); then
+    echo "$# parameters received"
+    echo "Function name: ${FUNCNAME} expects at least 3 parameters:"
+    echo "first parameter is the string id of the proxy"
+    echo "second parameter is the type of connection (input/output)"
+    exit 1
+  fi
+  
+  # setting the port
+  if [[ $1 == "barrel" ]]; then
+      local PORT=$PORT_BARREL
+  elif [[ $1 == "calo" ]]; then
+      local PORT=$PORT_CALO
+  else
+      echo "parameter 1 should be either 'barrel' or 'calo'"
+      exit 3
+  fi
+
+  # setting the type of connection
+  if [[ $2 == "input" ]]; then
+      local CONNECTION="method=bind,type=pull"
+  elif [[ $2 == "output" ]]; then
+      local CONNECTION="method=connect,type=push"
+  else
+      echo "parameter 2 should be either 'input' or 'output'"
+      exit 2
+  fi
+  
+  local PROXY_CONN="--proxy-channel-name aggregator-proxy-$1 --channel-config \"name=aggregator-proxy-$1,$CONNECTION,rateLogging=1,transport=zeromq,address=tcp://localhost:$PORT\""
+
+  echo PROXY_CONN = $PROXY_CONN 1>&2
+  echo $PROXY_CONN
+ 
+}
