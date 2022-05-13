7a5d40146b8099cea5cb84432d78ecc1fef7f54c
Robert Muenzer <robert.muenzer@cern.ch>
2022-05-13 13:44:34 +0200

    Increase number of timeframes for Laser tracker

--- DATA/production/calib/tpc-laser.sh
+++ DATA/production/calib/tpc-laser.sh
@@ -69,7 +69,7 @@ o2-dpl-raw-proxy $ARGS_ALL \
     $GPU_CONFIG \
     --configKeyValues "$ARGS_ALL_CONFIG;align-geom.mDetectors=none;GPU_global.deviceType=$GPUTYPE;GPU_proc.tpcIncreasedMinClustersPerRow=500000;GPU_proc.ignoreNonFatalGPUErrors=1;$GPU_CONFIG_KEY" \
     | o2-tpc-laser-track-filter $ARGS_ALL \
-    | o2-tpc-calib-laser-tracks  $ARGS_ALL --use-filtered-tracks --min-tfs 50 \
+    | o2-tpc-calib-laser-tracks  $ARGS_ALL --use-filtered-tracks --min-tfs 500 \
     | o2-tpc-calib-pad-raw $ARGS_ALL \
     --configKeyValues "TPCCalibPulser.FirstTimeBin=450;TPCCalibPulser.LastTimeBin=550;TPCCalibPulser.NbinsQtot=150;TPCCalibPulser.XminQtot=2;TPCCalibPulser.XmaxQtot=302;TPCCalibPulser.MinimumQtot=8;TPCCalibPulser.MinimumQmax=6;TPCCalibPulser.XminT0=450;TPCCalibPulser.XmaxT0=550;TPCCalibPulser.NbinsT0=400;keyval.output_dir=/dev/null" \
     --lanes 36 \
