70ca28c333d29e0a7e331a15f0a62f1375e6a96a
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2022-06-16 15:49:33 +0200

    Scripts for apass1 of MayJune2022

---
+++ DATA/production/configurations/2022/MayJunePilotBeam/apass1/selectSettings.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $RUNNUMBER -le 518547 ]]; then
+  ln -s o2sim_grp_b5m_128HB.root o2sim_grp.root
+  # default collision context till 13.06.2022
+  COLLISIONCONTEXT=collisioncontext_Single_4b_2_2_2_noLR.root
+fi
+
+if [[ $RUNNUMBER -ge 517676 ]] && [[ $RUNNUMBER -le 517679 ]]; then
+  COLLISIONCONTEXT=collisioncontext_Single_3b_0_2_2.root
+fi
+
+if [[ $RUNNUMBER -ge 517684 ]] && [[ $RUNNUMBER -le 517693 ]]; then
+  COLLISIONCONTEXT=collisioncontext_Single_3b_3_1_1.root
+fi
+
+echo "filling scheme = $COLLISIONCONTEXT"
+
+ln -s $COLLISIONCONTEXT collisioncontext.root
+
