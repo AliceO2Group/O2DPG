84214138b97f9a066470487c8be590fc7a7c406a
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2022-07-04 08:42:10 +0200

    Settings for more runs

--- DATA/production/configurations/2022/MayJunePilotBeam/apass1/selectSettings.sh
+++ DATA/production/configurations/2022/MayJunePilotBeam/apass1/selectSettings.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [[ $RUNNUMBER -le 518547 ]]; then
-  ln -s o2sim_grp_b5m_128HB.root o2sim_grp.root
+  # default B GRP till run 518547 
+  GRPMAG=o2sim_grp_b5m_128HB.root
   # default collision context till 13.06.2022
   COLLISIONCONTEXT=collisioncontext_Single_4b_2_2_2_noLR.root
 fi
@@ -14,11 +15,21 @@ if [[ $RUNNUMBER -ge 517684 ]] && [[ $RUNNUMBER -le 517693 ]]; then
   COLLISIONCONTEXT=collisioncontext_Single_3b_3_1_1.root
 fi
 
-if [[ $RUNNUMBER -ge 519041 ]] && [[ $RUNNUMBER -le 519499 ]]; then
+if [[ $RUNNUMBER -ge 519041 ]] && [[ $RUNNUMBER -le 519507 ]]; then
   COLLISIONCONTEXT=collisioncontext_Single_16b_8_8_8_noLR.root
 fi
 
+if [[ $RUNNUMBER -ge 519903 ]] && [[ $RUNNUMBER -le 519908 ]]; then
+  COLLISIONCONTEXT=collisioncontext_Single_4b_2_2_2_noLR.root
+fi
+
+# B field update
+if [[ $RUNNUMBER -ge 519041 ]]; then
+  GRPMAG=o2sim_grp_b5p_128HB.root
+fi
+
+echo "GRP B field = $GRPMAG"
 echo "filling scheme = $COLLISIONCONTEXT"
 
 ln -s $COLLISIONCONTEXT collisioncontext.root
-
+ln -s $GRPMAG o2sim_grp.root
