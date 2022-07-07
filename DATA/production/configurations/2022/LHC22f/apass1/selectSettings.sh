2a56a0d062dd7362117502ade62380ead6d632ac
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2022-07-08 00:50:58 +0200

    strobe length 396

--- DATA/production/configurations/2022/LHC22f/apass1/selectSettings.sh
+++ DATA/production/configurations/2022/LHC22f/apass1/selectSettings.sh
@@ -23,6 +23,10 @@ if [[ $RUNNUMBER -ge 520259 ]] && [[ $RUNNUMBER -le 520290 ]]; then
   export ITS_STROBE=297
   export MFT_STROBE=297
 fi
+if [[ $RUNNUMBER -ge 520296 ]] && [[ $RUNNUMBER -le 520297 ]]; then
+  export ITS_STROBE=396
+  export MFT_STROBE=396
+fi
 
 echo "GRP B field = $GRPMAG"
 echo "filling scheme = $COLLISIONCONTEXT"
