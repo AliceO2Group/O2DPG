3de752749e4f7aa3d304df8c9ec15a99d555b31b
Catalin Ristea <catalin.ristea@Catalins-MacBook-Pro.local>
2022-07-13 22:58:43 +0300

    Removed ITS/MFT strobe, as now these are read from CCDB

--- DATA/production/configurations/2022/LHC22f/apass1/selectSettings.sh
+++ DATA/production/configurations/2022/LHC22f/apass1/selectSettings.sh
@@ -12,26 +12,8 @@ COLLISIONCONTEXT=collisioncontext_Single_3b_2_2_2.root
 #  GRPMAG=o2sim_grp_b5p_128HB.root
 #fi
 
-export ITS_STROBE=198
-export MFT_STROBE=198
-
-
-if [[ $RUNNUMBER -ge 520163 ]] && [[ $RUNNUMBER -le 520180 ]]; then
-  export MFT_STROBE=297
-fi
-if [[ $RUNNUMBER -ge 520259 ]] && [[ $RUNNUMBER -le 520290 ]]; then
-  export ITS_STROBE=297
-  export MFT_STROBE=297
-fi
-if [[ $RUNNUMBER -ge 520296 ]]; then
-  export ITS_STROBE=396
-  export MFT_STROBE=396
-fi
-
 echo "GRP B field = $GRPMAG"
 echo "filling scheme = $COLLISIONCONTEXT"
-echo "ITS strobe = $ITS_STROBE"
-echo "MFT strobe = $MFT_STROBE"
 
 ln -s $COLLISIONCONTEXT collisioncontext.root
 ln -s $GRPMAG o2sim_grp.root
