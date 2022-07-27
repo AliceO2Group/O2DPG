ff7a2716b8a632806f0be131227ed393a494a2d0
afurs <afurs@cern.ch>
2022-07-27 14:04:51 +0200

    FT0: some fixes for calibration task

--- DATA/production/calib/ft0-timeoffset-processing.sh
+++ DATA/production/calib/ft0-timeoffset-processing.sh
@@ -13,15 +13,15 @@ FT0_DPL_CHANNEL_CONFIG="name=readout-proxy,type=pull,method=connect,address=ipc:
 
 WORKFLOW="o2-dpl-raw-proxy $ARGS_ALL --dataspec \"$FT0_PROXY_INSPEC\" --channel-config \"$FT0_DPL_CHANNEL_CONFIG\" | "
 WORKFLOW+="o2-calibration-ft0-tf-processor $ARGS_ALL | "
-WORKFLOW+="o2-calibration-ft0-channel-offset-calibration $ARGS_ALL --tf-per-slot 500 | "
-WORKFLOW+="o2-calibration-ccdb-populator-workflow $ARGS_ALL --configKeyValues \"$ARGS_ALL_CONFIG\" --ccdb-path=\"http://ccdb-test.cern.ch:8080\" --sspec-min 1 --sspec-max 1 | "
-WORKFLOW+="o2-dpl-run $ARGS_ALL --dds ${WORKFLOWMODE_FILE}"
+WORKFLOW+="o2-calibration-ft0-channel-offset-calibration $ARGS_ALL --tf-per-slot 2000 | "
+WORKFLOW+="o2-calibration-ccdb-populator-workflow $ARGS_ALL --configKeyValues \"$ARGS_ALL_CONFIG\" --ccdb-path=\"http://ccdb-test.cern.ch:8080\" | "
+WORKFLOW+="o2-dpl-run $ARGS_ALL $GLOBALDPLOPT"
 
 if [ "$WORKFLOWMODE" == "print" ]; then
     echo Workflow command:
     echo "$WORKFLOW" | sed "s/| */|\n/g"
 else
     # Execute the command we have assembled
-    WORKFLOW+=" --$WORKFLOWMODE"
+    WORKFLOW+=" --$WORKFLOWMODE ${WORKFLOWMODE_FILE}"
     eval "$WORKFLOW"
 fi
