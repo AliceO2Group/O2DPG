414f43f4d4d2f3a8fac9dda46f13dbc13b7e10bd
Tomas Herman <tomas.herman@cern.ch>
2022-07-27 11:06:18 +0200

    Delete old line replaced by new selection.

--- DATA/production/qc-workflow.sh
+++ DATA/production/qc-workflow.sh
@@ -58,7 +58,6 @@ elif [[ -z $QC_JSON_FROM_OUTSIDE ]]; then
   elif [[ $SYNCMODE == 1 ]]; then
     [[ -z "$QC_JSON_TPC" ]] && QC_JSON_TPC=$O2DPG_ROOT/DATA/production/qc-sync/tpc.json
     [[ -z "$QC_JSON_ITS" ]] && QC_JSON_ITS=$O2DPG_ROOT/DATA/production/qc-sync/its.json
-    [[ -z "$QC_JSON_MFT" ]] && QC_JSON_MFT=$O2DPG_ROOT/DATA/production/qc-sync/mft.json
     if [[ -z "$QC_JSON_MFT" ]]; then
       if has_processing_step MFT_RECO; then
         QC_JSON_MFT=$O2DPG_ROOT/DATA/production/qc-sync/mft_track.json
