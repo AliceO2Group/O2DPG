b813567b5305af652a48fa775f07695cae1e2966
Francesco Noferini <noferini@bo.infn.it>
2022-07-29 12:45:19 +0200

    fix for lhc22f/../m setting (#558)

--- DATA/production/configurations/2022/LHC22f/apass1/selectSettings.sh
+++ DATA/production/configurations/2022/LHC22f/apass1/selectSettings.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
 GRPMAG=o2sim_grp_b5p_128HB.root
-COLLISIONCONTEXT=collisioncontext_Single_3b_2_2_2.root
 
-#if [[ $RUNNUMBER -ge 517676 ]] && [[ $RUNNUMBER -le 517679 ]]; then
-#  COLLISIONCONTEXT=collisioncontext_Single_3b_0_2_2.root
-#fi
+# LHC22f: 520143 - 520473
+if [[ $RUNNUMBER -ge 520143 ]] && [[ $RUNNUMBER -le 520473 ]]; then
+  # +30kA/+6kA
+  GRPMAG=o2sim_grp_b5p_128HB.root
+fi
 
-# B field update
-#if [[ $RUNNUMBER -ge 519041 ]]; then
-#  GRPMAG=o2sim_grp_b5p_128HB.root
-#fi
+# LHC22g: 520474 - 520477
+if [[ $RUNNUMBER -ge 520474 ]] && [[ $RUNNUMBER -le 520477 ]]; then
+  # +12kA/+6kA
+  GRPMAG=o2sim_grp_b2p_128HB.root
+fi
+
+# LHC22h: 520495 - 520509
+if [[ $RUNNUMBER -ge 520495 ]] && [[ $RUNNUMBER -le 520509 ]]; then
+  # 0kA/0kA
+  GRPMAG=o2sim_grp_b0_128HB.root
+fi
+
+# LHC22i: 520529 - 520542
+if [[ $RUNNUMBER -ge 520529 ]] && [[ $RUNNUMBER -le 520542 ]]; then
+  # -12kA/-6kA
+  GRPMAG=o2sim_grp_b2m_128HB.root
+fi
+
+# LHC22j: 520543 - 521150
+if [[ $RUNNUMBER -ge 520543 ]] && [[ $RUNNUMBER -le 521150 ]]; then
+  # -30kA/-6kA
+  GRPMAG=o2sim_grp_b5m_128HB.root
+fi
+
+# LHC22m: 521326 - 521907
+if [[ $RUNNUMBER -ge 521326 ]]; then
+  # -30kA/-6kA
+  GRPMAG=o2sim_grp_b5m_128HB.root
+fi
 
 echo "GRP B field = $GRPMAG"
-echo "filling scheme = $COLLISIONCONTEXT"
 
-ln -s $COLLISIONCONTEXT collisioncontext.root
 ln -s $GRPMAG o2sim_grp.root
