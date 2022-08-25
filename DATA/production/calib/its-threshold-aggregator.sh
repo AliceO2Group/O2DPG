d89aa6d2ead8e0ff3fd62e177afa39c2bba7f2e9
iravasen <ivan.ravasenga@cern.ch>
2022-08-25 14:13:36 +0200

    ITS Calibration scans in O2DPG (#589)
    * ITS calibration scans update
    
    * comment improved

--- DATA/production/calib/its-threshold-aggregator.sh
+++ DATA/production/calib/its-threshold-aggregator.sh
@@ -6,11 +6,25 @@ source common/setenv.sh
 # Set general arguments
 source common/getCommonArgs.sh
 
-PROXY_INSPEC="tunestring:ITS/TSTR/0;runtype:ITS/RUNT/0;fittype:ITS/FITT/0;scantype:ITS/SCANT/0;eos:***/INFORMATION"
+PROXY_INSPEC="tunestring:ITS/TSTR;runtype:ITS/RUNT;fittype:ITS/FITT;scantype:ITS/SCANT;chipdonestring:ITS/QCSTR;confdbv:ITS/CONFDBV;PixTypString:ITS/PIXTYP;eos:***/INFORMATION"
 
-WORKFLOW="o2-dpl-raw-proxy $ARGS_ALL --proxy-name its-thr-input-proxy --dataspec \"$PROXY_INSPEC\" --network-interface ib0 --channel-config \"name=its-thr-input-proxy,method=bind,type=pull,rateLogging=0,transport=zeromq\" | "
+CCDBPATH1=""
+CCDBPATH2=""
+if [ $RUNTYPE == "tuning" ] || [ $RUNTYPE == "digital" ]; then
+  CCDBPATH1="http://alio2-cr1-flp199.cern.ch:8083"
+  CCDBPATH2="http://localhost:8084"
+elif [ $RUNTYPE == "thrshort" ]; then
+  CCDBPATH1="http://localhost:8084"
+else
+  echo Ccdb paths are empty
+fi
+
+WORKFLOW="o2-dpl-raw-proxy $ARGS_ALL --exit-transition-timeout 20 --proxy-name its-thr-input-proxy --dataspec \"$PROXY_INSPEC\" --network-interface ib0 --channel-config \"name=its-thr-input-proxy,method=bind,type=pull,rateLogging=0,transport=zeromq\" | "
 WORKFLOW+="o2-its-threshold-aggregator-workflow -b $ARGS_ALL | "
-WORKFLOW+="o2-calibration-ccdb-populator-workflow $ARGS_ALL --configKeyValues \"$ARGS_ALL_CONFIG\" --ccdb-path=\"http://alio2-cr1-flp199.cern.ch:8083\" | "
+WORKFLOW+="o2-calibration-ccdb-populator-workflow $ARGS_ALL --configKeyValues \"$ARGS_ALL_CONFIG\" --ccdb-path=\"$CCDBPATH1\" --sspec-min 0 --sspec-max 0 | "
+if [ $RUNTYPE == "digital" ]; then
+  WORKFLOW+="o2-calibration-ccdb-populator-workflow $ARGS_ALL --configKeyValues \"$ARGS_ALL_CONFIG\" --ccdb-path=\"$CCDBPATH2\" --sspec-min 1 --sspec-max 1 | "
+fi
 WORKFLOW+="o2-dpl-run $ARGS_ALL $GLOBALDPLOPT"
 
 if [ $WORKFLOWMODE == "print" ]; then
--- DATA/production/calib/its-threshold-processing.sh
+++ DATA/production/calib/its-threshold-processing.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## WORKFLOW FOR ALL ITS CALIBRATION SCANS (TODO: ADD FULL THR SCAN)
+
 source common/setenv.sh
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -7,14 +9,20 @@ source common/setenv.sh
 source common/getCommonArgs.sh
 
 PROXY_INSPEC="A:ITS/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0;eos:***/INFORMATION"
-PROXY_OUTSPEC="tunestring:ITS/TSTR/0;runtype:ITS/RUNT/0;fittype:ITS/FITT/0;scantype:ITS/SCANT/0"
+PROXY_OUTSPEC="tunestring:ITS/TSTR;runtype:ITS/RUNT;fittype:ITS/FITT;scantype:ITS/SCANT;chipdonestring:ITS/QCSTR;confdbv:ITS/CONFDBV;PixTypString:ITS/PIXTYP"
 
-WORKFLOW="o2-dpl-raw-proxy $ARGS_ALL --dataspec \"$PROXY_INSPEC\" --channel-config \"name=readout-proxy,type=pull,method=connect,address=ipc://@$INRAWCHANNAME,rateLogging=0,transport=shmem\" | "
-WORKFLOW+="o2-itsmft-stf-decoder-workflow ${ARGS_ALL} --configKeyValues \"$ARGS_ALL_CONFIG\" --nthreads 1  --pipeline its-stf-decoder:6 --no-clusters --no-cluster-patterns --enable-calib-data --digits | "
-WORKFLOW+="o2-its-threshold-calib-workflow -b --nthreads 4 --enable-eos --chip-mod-selector 0 --chip-mod-base 3 --fittype derivative --output-dir \"/data/calibration\" --meta-output-dir \"/data/epn2eos_tool/epn2eos\" $ARGS_ALL | "
-WORKFLOW+="o2-its-threshold-calib-workflow -b --nthreads 4 --enable-eos --chip-mod-selector 1 --chip-mod-base 3 --fittype derivative --output-dir \"/data/calibration\" --meta-output-dir \"/data/epn2eos_tool/epn2eos\" $ARGS_ALL | "
-WORKFLOW+="o2-its-threshold-calib-workflow -b --nthreads 4 --enable-eos --chip-mod-selector 2 --chip-mod-base 3 --fittype derivative --output-dir \"/data/calibration\" --meta-output-dir \"/data/epn2eos_tool/epn2eos\" $ARGS_ALL | "
-WORKFLOW+="o2-qc --config consul-json://alio2-cr1-hv-aliecs:8500/o2/components/qc/ANY/any/its-qc-calibration --local --host epn $ARGS_ALL -b | "
+CHIPMODBASE=5
+if [ $RUNTYPE == "digital" ]; then
+  CHIPMODBASE=10
+fi
+
+WORKFLOW="o2-dpl-raw-proxy --exit-transition-timeout 20 $ARGS_ALL --dataspec \"$PROXY_INSPEC\" --channel-config \"name=readout-proxy,type=pull,method=connect,address=ipc://@$INRAWCHANNAME,rateLogging=0,transport=shmem\" | "
+WORKFLOW+="o2-itsmft-stf-decoder-workflow ${ARGS_ALL} --condition-tf-per-query -1 --condition-backend \"http://localhost:8084\" --ignore-dist-stf --configKeyValues \"$ARGS_ALL_CONFIG\" --nthreads 1  --no-clusters --no-cluster-patterns --pipeline its-stf-decoder:6 --enable-calib-data --digits | "
+for i in $(seq 0 $((CHIPMODBASE-1)))
+do
+  WORKFLOW+="o2-its-threshold-calib-workflow -b --enable-eos --enable-single-pix-tag --ccdb-mgr-url=\"http://localhost:8084\" --nthreads 1 --chip-mod-selector $i --chip-mod-base $CHIPMODBASE --fittype derivative --output-dir \"/data/calibration\" --meta-output-dir \"/data/epn2eos_tool/epn2eos\" --meta-type \"calibration\" $ARGS_ALL | "
+done
+WORKFLOW+="o2-qc --config consul-json://aliecs.cern.ch:8500/o2/components/qc/ANY/any/its-qc-calibration --local --host epn -b $ARGS_ALL | "
 WORKFLOW+="o2-dpl-output-proxy ${ARGS_ALL} --dataspec \"$PROXY_OUTSPEC\" --proxy-channel-name its-thr-input-proxy --channel-config \"name=its-thr-input-proxy,method=connect,type=push,transport=zeromq,rateLogging=0\" | "
 WORKFLOW+="o2-dpl-run ${ARGS_ALL} ${GLOBALDPLOPT}"
 
