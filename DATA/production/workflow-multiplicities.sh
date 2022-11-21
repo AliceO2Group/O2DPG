702eead55902c38888179ee164a1ce11439e539a
David Rohr <drohr@jwdt.org>
2022-11-21 20:58:24 +0100

    Consolidate existing workflow multiplicities

--- DATA/production/workflow-multiplicities.sh
+++ DATA/production/workflow-multiplicities.sh
@@ -30,27 +30,56 @@ N_F_RAW=$MULTIPLICITY_FACTOR_RAWDECODERS
 N_F_CTF=$MULTIPLICITY_FACTOR_CTFENCODERS
 
 N_TPCTRK=$NGPUS
-if [[ $OPTIMIZED_PARALLEL_ASYNC != 0 ]]; then
-  # Tuned multiplicities for async Pb-Pb processing
-  if [[ $SYNCMODE == "1" ]]; then echo "Must not use OPTIMIZED_PARALLEL_ASYNC with GPU or SYNCMODE" 1>&2; exit 1; fi
-  if [[ $NUMAGPUIDS != 0 ]]; then N_NUMAFACTOR=1; else N_NUMAFACTOR=2; fi
-  NGPURECOTHREADS=6
-  NTRDTRKTHREADS=2
-  if [[ $GPUTYPE == "CPU" ]]; then
-    N_TPCENTDEC=$((2 * $N_NUMAFACTOR))
-    N_MFTTRK=$((3 * $N_NUMAFACTOR))
-    N_ITSTRK=$((3 * $N_NUMAFACTOR))
-    N_TPCITS=$((2 * $N_NUMAFACTOR))
-    N_MCHTRK=$((1 * $N_NUMAFACTOR))
-    N_TOFMATCH=$((9 * $N_NUMAFACTOR))
-    N_TPCTRK=$((6 * $N_NUMAFACTOR))
-  else
-    N_TPCENTDEC=$(math_max $((3 * $NGPUS * $OPTIMIZED_PARALLEL_ASYNC * $N_NUMAFACTOR / 4)) 1)
-    N_MFTTRK=$(math_max $((6 * $NGPUS * $OPTIMIZED_PARALLEL_ASYNC * $N_NUMAFACTOR / 4)) 1)
-    N_ITSTRK=$(math_max $((6 * $NGPUS * $OPTIMIZED_PARALLEL_ASYNC * $N_NUMAFACTOR / 4)) 1)
-    N_TPCITS=$(math_max $((4 * $NGPUS * $OPTIMIZED_PARALLEL_ASYNC * $N_NUMAFACTOR / 4)) 1)
-    N_MCHTRK=$(math_max $((2 * $NGPUS * $OPTIMIZED_PARALLEL_ASYNC * $N_NUMAFACTOR / 4)) 1)
-    N_TOFMATCH=$(math_max $((20 * $NGPUS * $OPTIMIZED_PARALLEL_ASYNC * $N_NUMAFACTOR / 4)) 1)
+if [[ ! -z $OPTIMIZED_PARALLEL_ASYNC ]]; then
+  # Tuned multiplicities for async processing
+  if [[ $OPTIMIZED_PARALLEL_ASYNC == "pp_8cpu" ]]; then
+    NGPURECOTHREADS=5
+    [[ -z $TIMEFRAME_RATE_LIMIT ]] && TIMEFRAME_RATE_LIMIT=3
+  elif [[ $OPTIMIZED_PARALLEL_ASYNC == "pp_1gpu" ]]; then
+    [[ -z $TIMEFRAME_RATE_LIMIT ]] && TIMEFRAME_RATE_LIMIT=8
+    [[ -z $SHMSIZE ]] && SHMSIZE=24000000000
+    N_TOFMATCH=2
+    N_MCHCL=3
+    N_TPCENTDEC=3
+    N_TPCITS=3
+    N_MCHTRK=2
+    N_ITSTRK=3
+    NGPURECOTHREADS=8
+    NTRDTRKTHREADS=2
+  elif [[ $OPTIMIZED_PARALLEL_ASYNC == "pp_4gpu" ]]; then
+    [[ -z $TIMEFRAME_RATE_LIMIT ]] && TIMEFRAME_RATE_LIMIT=16
+    [[ -z $SHMSIZE ]] && SHMSIZE=64000000000
+    NGPURECOTHREADS=8
+    NTRDTRKTHREADS=2
+    MULTIPLICITY_PROCESS_globalfwd_track_matcher=2
+    MULTIPLICITY_PROCESS_pvertex_track_matching=2
+    MULTIPLICITY_PROCESS_primary_vertexing=2 
+    MULTIPLICITY_PROCESS_TRDTRACKLETTRANSFORMER=2
+    MULTIPLICITY_PROCESS_aod_producer_workflow=3
+    MULTIPLICITY_PROCESS_trd_globaltracking_TPC_ITS_TPC_=2
+    MULTIPLICITY_PROCESS_tof_matcher=8
+    MULTIPLICITY_PROCESS_mch_cluster_finder=12
+    MULTIPLICITY_PROCESS_mch_track_finder=6
+    MULTIPLICITY_PROCESS_tpc_entropy_decoder=8
+    MULTIPLICITY_PROCESS_itstpc_track_matcher=12
+    MULTIPLICITY_PROCESS_its_tracker=12
+  elif [[ $OPTIMIZED_PARALLEL_ASYNC == "PbPb_4gpu" ]]; then
+    N_TPCENTDEC=$(math_max $((3 * $NGPUS / 4)) 1)
+    N_MFTTRK=$(math_max $((6 * $NGPUS / 4)) 1)
+    N_ITSTRK=$(math_max $((6 * $NGPUS / 4)) 1)
+    N_TPCITS=$(math_max $((4 * $NGPUS / 4)) 1)
+    N_MCHTRK=$(math_max $((2 * $NGPUS / 4)) 1)
+    N_TOFMATCH=$(math_max $((20 * $NGPUS / 4)) 1)
+  elif [[ $OPTIMIZED_PARALLEL_ASYNC == "PbPb_64cpu" ]]; then
+    NGPURECOTHREADS=6
+    NTRDTRKTHREADS=2
+    N_TPCENTDEC=2
+    N_MFTTRK=3
+    N_ITSTRK=3
+    N_TPCITS=2
+    N_MCHTRK=1
+    N_TOFMATCH=9
+    N_TPCTRK=6
   fi
 elif [[ $EPNPIPELINES != 0 ]]; then
   RECO_NUM_NODES_WORKFLOW_CMP=$((($RECO_NUM_NODES_WORKFLOW > 15 ? $RECO_NUM_NODES_WORKFLOW : 15) * ($NUMAGPUIDS != 0 ? 2 : 1))) # Limit the lower scaling factor, multiply by 2 if we have 2 NUMA domains
