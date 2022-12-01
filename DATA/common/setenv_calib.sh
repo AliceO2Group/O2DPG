6f3744c715d8604032d541bd144471bc9e911283
Ole Schmidt <ole.schmidt@cern.ch>
2022-12-01 17:45:32 +0100

    TPC_SCDCALIB always sends trk references

--- DATA/common/setenv_calib.sh
+++ DATA/common/setenv_calib.sh
@@ -174,7 +174,10 @@ if [[ -z $CALIBDATASPEC_BARREL_TF ]]; then
   if [[ $CALIB_TOF_DIAGNOSTICS == 1 ]]; then add_semicolon_separated CALIBDATASPEC_BARREL_TF "diagWords:TOF/DIAFREQ/0"; fi
 
   # TPC
-  if [[ $CALIB_TPC_SCDCALIB == 1 ]]; then add_semicolon_separated CALIBDATASPEC_BARREL_TF "unbinnedTPCResiduals:GLO/UNBINNEDRES/0"; fi
+  if [[ $CALIB_TPC_SCDCALIB == 1 ]]; then
+    add_semicolon_separated CALIBDATASPEC_BARREL_TF "unbinnedTPCResiduals:GLO/UNBINNEDRES/0"
+    add_semicolon_separated CALIBDATASPEC_BARREL_TF "trackReferences:GLO/TRKREFS/0"
+  fi
   if [[ $CALIB_TPC_SCDCALIB == 1 ]] && [[ 0$CALIB_TPC_SCDCALIB_SENDTRKDATA == "01" ]]; then add_semicolon_separated CALIBDATASPEC_BARREL_TF "tpcInterpTrkData:GLO/TRKDATA/0"; fi
   if [[ $CALIB_TPC_SCDCALIB == 1 ]] && [[ $CALIB_TPC_SCDCALIB_CTP_INPUT == "--enable-ctp" ]]; then
     add_semicolon_separated CALIBDATASPEC_BARREL_TF "lumi:CTP/LUMI/0"
