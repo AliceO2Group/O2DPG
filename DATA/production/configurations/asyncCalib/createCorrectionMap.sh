f543a926e4cd8ba50816d361f973252b34c96bbf
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2022-12-06 13:08:26 +0100

    Adding TPC calib scripts to extract distortion maps

---
+++ DATA/production/configurations/asyncCalib/createCorrectionMap.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Create correction maps for a list of input files.
+# The scripts assumes to get as input parameters 
+# - the xml collection of the o2tpc_residuals*.root files created for a single run
+# - the pattern of the calibration interval extracted by the 'createJobList.sh' macro.
+# It creates an two output files
+# - TPCDebugVoxRes*.root, containing the tree of extracted residuals
+# - TPCFastTransform*.root, containing the spline object to be used in the reconstruction
+
+# ===| input parameters & output file names  |==================================
+inputCollection=$1
+mergePattern=$2
+filePattern="o2tpc_residuals_${mergePattern}\.root"
+
+# This could be used for the old file format
+if [[ $ALIEN_JDL_OLDFILENAME == "1" ]]; then
+  filePattern="o2tpc_residuals${mergePattern}.*\.root"
+fi
+
+fileList=inputFileList.txt
+voxResOutFile=TPCDebugVoxRes_${mergePattern}.root
+splineOutFile=TPCFastTransform_${mergePattern}.root
+
+[[ -z "${ALIEN_JDL_MAPCREATORARGS}" ]] && ALIEN_JDL_MAPCREATORARGS='--configKeyValues "scdcalib.maxSigY=2;scdcalib.maxSigZ=2"'
+
+# ===| create intput list |=====================================================
+sed -rn 's|.*turl="([^"]*)".*|\1|p' $inputCollection | grep $filePattern | sort -n > ${fileList}
+
+declare -i nLines=$(wc -l ${fileList} | awk '{print $1}')
+if [[ $nLines -eq 0 ]]; then
+  echo "ERROR: Could not find lines matching '${mergePattern}' in file '${inputCollection}'. Cannot continue with SCD correction."
+  exit
+fi
+echo "Creating residual map for $nLines input files"
+
+# ===| create tree with residuals |=============================================
+cmd="o2-tpc-static-map-creator $ALIEN_JDL_MAPCREATORARGS --residuals-infiles ${fileList} --outfile ${voxResOutFile} &> residuals.log"
+echo "running: '$cmd'"
+if [[ $ALIEN_JDL_DONTEXTRACTTPCCALIB != "1" ]]; then
+  eval $cmd
+  if [[ ! -s ${voxResOutFile} ]]; then
+    echo "ERROR: Output file '${voxResOutFile}' not created, or zero size. Cannot continue with SCD correction."
+    exit
+  fi
+fi
+
+# ===| create spline object |===================================================
+cmd="root.exe -q -x -l -n $O2_ROOT/share/macro/TPCFastTransformInit.C'(\"${voxResOutFile}\", \"${splineOutFile}\")' &> splines.log"
+echo "running: '$cmd'"
+if [[ $ALIEN_JDL_DONTEXTRACTTPCCALIB != "1" ]]; then
+  eval $cmd
+fi
---
+++ DATA/production/configurations/asyncCalib/createJobList.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Create a list to be used for job submission of the TPC SCD correction map creation
+# The scripts assumes to get as input parameter the xml collection of the o2tpc_residuals*.root file
+# created for a single run.
+# It creates an output file with one line per job that should be submitted and the parameters
+# to be passed to each job.
+
+
+# ===| input parameters |=======================================================
+inputCollection=$1
+
+# ===| timebin log file |=======================================================
+TIMEBINLOGFILENAME=timeBins.log
+
+# ===| output file creation |===================================================
+sed -rn 's|.*turl="([^"]*)".*|\1|;s|.*o2tpc_residuals_([0-9_]+)\.root|\1 0 0|p' $inputCollection | sort -u > $TIMEBINLOGFILENAME
+
+# This could be used for the old file format
+if [[ $ALIEN_JDL_OLDFILENAME == "1" ]]; then
+  sed -rn 's|.*turl="([^"]*)".*|\1|;s|.*/o2tpc_residuals([0-9]{7})[0-9]+\.root|\1 0 0|p' $inputCollection | sort -u > $TIMEBINLOGFILENAME
+fi
+
+# ===| sanity check |===========================================================
+declare -i nLines=$(wc -l $TIMEBINLOGFILENAME | awk '{print $1}')
+
+if [[ $nLines -eq 0 ]]; then
+  echo "ERROR: Problem creating list for jobs to submit"
+else
+  echo "Number of jobs to submit for SCD map creation: $nLines"
+fi
