e96a2eece449061bc6a6b86c9e0f6b51365b890b
noferini <noferini@bo.infn.it>
2022-12-06 11:25:51 +0100

    parsing pass name for digitization

--- MC/bin/o2dpg_sim_workflow.py
+++ MC/bin/o2dpg_sim_workflow.py
@@ -680,6 +680,8 @@ for tf in range(1, NTIMEFRAMES + 1):
      returnstring = returnstring + '"'
      return returnstring
 
+   # parsing passName from env variable
+   PASSNAME='${ALIEN_JDL_LPMANCHORPASSNAME:-unanchored}'
 
    # This task creates the basic setup for all digitizers! all digitization configKeyValues need to be given here
    contextneeds = [LinkGRPFileTask['name'], SGNtask['name']]
@@ -687,7 +689,7 @@ for tf in range(1, NTIMEFRAMES + 1):
    # this is just to have the digitizer ini file
    ContextTask['cmd'] = '${O2_ROOT}/bin/o2-sim-digitizer-workflow --only-context --interactionRate ' + str(INTRATE) \
                         + ' ' + getDPL_global_options(ccdbbackend=False) + ' -n ' + str(args.ns) + simsoption       \
-                        + ' ' + putConfigValues({"DigiParams.maxOrbitsToDigitize" : str(orbitsPerTF)}) + ('',' --incontext ' + CONTEXTFILE)[args.pregenCollContext] 
+                        + ' ' + putConfigValuesNew({"DigiParams.maxOrbitsToDigitize" : str(orbitsPerTF)},{"DigiParams.passName" : str(PASSNAME)}) + ('',' --incontext ' + CONTEXTFILE)[args.pregenCollContext] 
 
    if BCPATTERN != '':
       ContextTask['cmd'] += ' --bcPatternFile "' + BCPATTERN + '"'
