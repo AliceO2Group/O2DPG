c462fdf020ec1a5f9b910ec7c90397077d1b6652
Benedikt Volkel <benedikt.volkel@cern.ch>
2023-02-23 17:56:25 +0100

    [TEST] Make CI complain

--- MC/bin/tests/embedding_test.sh
+++ MC/bin/tests/embedding_test.sh
@@ -32,4 +32,4 @@ ${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM 5020 -col pp -gen pythia8 -proc 
                                            -tf ${NTIMEFRAMES} -nb ${NBKGEVENTS}              \
                                            -ns ${NSIGEVENTS} -e ${SIMENGINE}                 \
                                            -j ${NWORKERS} --embedding -interactionRate 50000 \
-                                           --include-analysis -run 310000 ${SEED}
+                                           --include-analysis -run 310000 ${SEED} -abc
