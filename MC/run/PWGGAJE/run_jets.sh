760f549d5d1c62081388e2798721b6fa8748920a
Benedikt Volkel <benedikt.volkel@cern.ch>
2023-03-17 13:28:57 +0100

    Run scripts must return/exit with reasonable code
    * if runner is the last thing called, leave it at that
    
    * if more complex, make sure to emit reasonable ode
    
    * remove exit 0 everywhere, impossible to know by the caller

--- MC/run/PWGGAJE/run_jets.sh
+++ MC/run/PWGGAJE/run_jets.sh
@@ -8,7 +8,7 @@
 # ----------- LOAD UTILITY FUNCTIONS --------------------------
 . ${O2_ROOT}/share/scripts/jobutils.sh
 
-# ----------- START ACTUAL JOB  ----------------------------- 
+# ----------- START ACTUAL JOB  -----------------------------
 
 RNDSEED=${RNDSEED:-0}   # [default = 0] time-based random seed
 
@@ -29,7 +29,7 @@ pthatbin_loweredges=(0 5 7 9 12 16 21 28 36 45 57 70 85 99 115 132 150 169 190 2
 pthatbin_higheredges=( 5 7 9 12 16 21 28 36 45 57 70 85 99 115 132 150 169 190 212 235 -1)
 
 # Recover environmental vars for pt binning
-#PTHATBIN=${PTHATBIN:-1} 
+#PTHATBIN=${PTHATBIN:-1}
 
 if [ -z "$PTHATBIN" ]; then
     echo "Open Pt-hat range set"
@@ -47,5 +47,3 @@ ${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM ${CONFIG_ENERGY} -col pp -gen py
 
 # run workflow
 ${O2DPG_ROOT}/MC/bin/o2_dpg_workflow_runner.py -f workflow.json
-
-exit 0
