71142a0b5c6732f6f56d49cdb814346c6ee87cc5
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2023-03-23 15:13:29 +0100

    Update json

--- MC/config/analysis_testing/json/format_json.sh
+++ MC/config/analysis_testing/json/format_json.sh
@@ -3,6 +3,11 @@
 # Script to format the json files
 
 for i in $(ls *.json); do
+    case "$i" in
+    *analyses_config.json*)
+        continue
+        ;;
+    esac
     echo "Formatting $i"
     jq -S . $i >a.json
     mv a.json $i
