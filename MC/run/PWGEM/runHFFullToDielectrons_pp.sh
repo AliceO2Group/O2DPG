623ba8b4819ff81c52d85a0366b95f411e5252ae
Raphaelle Bailhache <raphaelle.bailhache@cern.ch>
2023-05-25 23:00:42 +0200

    Fix external trigger

--- MC/run/PWGEM/runHFFullToDielectrons_pp.sh
+++ MC/run/PWGEM/runHFFullToDielectrons_pp.sh
@@ -38,7 +38,7 @@ fi
 
 ${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM 13600 -col pp -gen external -j ${NWORKERS} -ns ${NSIGEVENTS} -tf ${NTIMEFRAMES} -e TGeant4 -mod "--skipModules ZDC" \
      ${SEED} \
-     -ini $O2DPG_ROOT/MC/config/PWGEM/ini/$CONFIGNAME  \
+     -trigger "external" -ini $O2DPG_ROOT/MC/config/PWGEM/ini/$CONFIGNAME  \
      -confKeyBkg "Diamond.width[2]=6" -interactionRate ${INTRATE} 
 
 # run workflow
