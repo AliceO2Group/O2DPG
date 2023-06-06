cb998468d7be8127f6e7e1983e6a4da9c7632d0e
fmazzasc <fmazzasc@cern.ch>
2023-06-06 10:50:00 +0200

    Enable strangeness tracking in production

--- DATA/production/configurations/asyncReco/setenv_extra.sh
+++ DATA/production/configurations/asyncReco/setenv_extra.sh
@@ -373,9 +373,13 @@ fi
 
 # secondary vertexing
 export SVTX="svertexer.checkV0Hypothesis=false;svertexer.checkCascadeHypothesis=false"
+# strangeness tracking
+export STRK=""
 
 export CONFIG_EXTRA_PROCESS_o2_primary_vertexing_workflow+=";$PVERTEXER;$VDRIFTPARAMOPTION;"
 export CONFIG_EXTRA_PROCESS_o2_secondary_vertexing_workflow+=";$SVTX"
+export CONFIG_EXTRA_PROCESS_o2_strangeness_tracking_workflow+=";$STRK"
+
 
 export CONFIG_EXTRA_PROCESS_o2_tpcits_match_workflow+=";$ITSEXTRAERR;$ITSTPCMATCH;$TRACKTUNETPC;$VDRIFTPARAMOPTION;"
 [[ ! -z "${TPCITSTIMEBIAS}" ]] && export CONFIG_EXTRA_PROCESS_o2_tpcits_match_workflow+=";tpcitsMatch.globalTimeBiasMUS=$TPCITSTIMEBIAS;"
