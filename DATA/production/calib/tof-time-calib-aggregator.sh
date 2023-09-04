43dd4c04c69c4ec811832ee48e9a2eee49a3e9ea
Francesco Noferini <noferini@bo.infn.it>
2023-09-04 09:38:04 +0200

    modernization TOF workflows (#1210)

--- DATA/production/calib/tof-time-calib-aggregator.sh
+++ DATA/production/calib/tof-time-calib-aggregator.sh
@@ -5,14 +5,22 @@ source common/setenv.sh
 # ---------------------------------------------------------------------------------------------------------------------
 # Set general arguments
 source common/getCommonArgs.sh
-ARGS_ALL_CONFIG="keyval.input_dir=$FILEWORKDIR;keyval.output_dir=/dev/null;$ALL_EXTRA_CONFIG"
+source common/gen_topo_helper_functions.sh
 
 PROXY_INSPEC="calclus:TOF/INFOCALCLUS/0;cosmics:TOF/INFOCOSMICS/0;trkcos:TOF/INFOTRACKCOS/0;trksiz:TOF/INFOTRACKSIZE/0;eos:***/INFORMATION"
 
-WORKFLOW="o2-dpl-raw-proxy $ARGS_ALL --proxy-name tof-time-calib-input-proxy --dataspec \"$PROXY_INSPEC\" --network-interface ib0 --channel-config \"name=tof-time-calib-input-proxy,method=bind,type=pull,rateLogging=0,transport=zeromq\" | "
-WORKFLOW+="o2-calibration-tof-calib-workflow --cosmics --do-channel-offset --min-entries 1000 $ARGS_ALL | "
-WORKFLOW+="o2-calibration-ccdb-populator-workflow $ARGS_ALL --configKeyValues \"$ARGS_ALL_CONFIG\" --ccdb-path=\"http://ccdb-test.cern.ch:8080\" | "
-WORKFLOW+="o2-dpl-run $ARGS_ALL $GLOBALDPLOPT"
+WORKFLOW=
+add_W o2-dpl-raw-proxy "$ARGS_ALL --proxy-name tof-time-calib-input-proxy --dataspec ${PROXY_INSPEC} --network-interface ib0 --channel-config \"name=tof-time-calib-input-proxy,method=bind,type=pull,rateLogging=0,transport=zeromq\" "
+add_W o2-calibration-tof-calib-workflow "--cosmics --do-channel-offset --min-entries 1000 ${ARGS_ALL} "
+
+if [[ $RUNTYPE == "SYNTHETIC" || "${GEN_TOPO_DEPLOYMENT_TYPE:-}" == "ALICE_STAGING" ]]; then
+    CCDB_POPULATOR_UPLOAD_PATH="http://ccdb-test.cern.ch:8080"
+else
+    CCDB_POPULATOR_UPLOAD_PATH="http://localhost:8084"
+fi
+add_W o2-calibration-ccdb-populator-workflow "${ARGS_ALL} --configKeyValues ${ARGS_ALL_CONFIG} --ccdb-path=${CCDB_POPULATOR_UPLOAD_PATH} "
+
+WORKFLOW+="o2-dpl-run ${ARGS_ALL} ${GLOBALDPLOPT}"
 
 if [ $WORKFLOWMODE == "print" ]; then
   echo Workflow command:
