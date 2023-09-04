04f000353ced169990d46e19cc94500c96a7ae63
Francesco Noferini <noferini@bo.infn.it>
2023-09-04 09:38:04 +0200

    modernization TOF workflows (#1210)

--- DATA/production/calib/tof-standalone-cosmic-reco-time-calib.sh
+++ DATA/production/calib/tof-standalone-cosmic-reco-time-calib.sh
@@ -5,17 +5,18 @@ source common/setenv.sh
 # ---------------------------------------------------------------------------------------------------------------------
 # Set general arguments
 source common/getCommonArgs.sh
-ARGS_ALL_CONFIG="keyval.input_dir=$FILEWORKDIR;keyval.output_dir=/dev/null;$ALL_EXTRA_CONFIG"
+source common/gen_topo_helper_functions.sh
 
 PROXY_INSPEC="x:TOF/CRAWDATA;dd:FLP/DISTSUBTIMEFRAME/0"
 NTHREADS=1
 PROXY_OUTSPEC="calclus:TOF/INFOCALCLUS;cosmics:TOF/INFOCOSMICS;trkcos:TOF/INFOTRACKCOS;trksiz:TOF/INFOTRACKSIZE"
 MYDIR="$(dirname $(readlink -f $0))/../../testing/detectors/TOF"
 
-WORKFLOW="o2-dpl-raw-proxy ${ARGS_ALL} --dataspec \"$PROXY_INSPEC\" --channel-config \"name=readout-proxy,type=pull,method=connect,address=ipc://@$INRAWCHANNAME,transport=shmem,rateLogging=0\" | "
-WORKFLOW+="o2-tof-reco-workflow --input-type raw --output-type clusters ${ARGS_ALL} --configKeyValues \"$ARGS_ALL_CONFIG\" --disable-root-output --calib-cluster --cluster-time-window 10000 --cosmics --pipeline \"tof-compressed-decoder:${NTHREADS},TOFClusterer:${NTHREADS}\" | "
-WORKFLOW+="o2-qc ${ARGS_ALL} --config json://${MYDIR}/qc-full.json --local --host epn | "
-WORKFLOW+="o2-dpl-output-proxy ${ARGS_ALL} --dataspec \"$PROXY_OUTSPEC\" --proxy-channel-name tof-time-calib-input-proxy --channel-config \"name=tof-time-calib-input-proxy,method=connect,type=push,transport=zeromq,rateLogging=0\" | "
+WORKFLOW=
+add_W o2-dpl-raw-proxy "${ARGS_ALL} --dataspec ${PROXY_INSPEC} --channel-config \"name=readout-proxy,type=pull,method=connect,address=ipc://@$INRAWCHANNAME,transport=shmem,rateLogging=0\" "
+add_W o2-tof-reco-workflow "--input-type raw --output-type clusters ${ARGS_ALL} --configKeyValues ${ARGS_ALL_CONFIG} --disable-root-output --calib-cluster --cluster-time-window 10000 --cosmics --pipeline \"tof-compressed-decoder:${NTHREADS},TOFClusterer:${NTHREADS}\" "
+add_W o2-qc "${ARGS_ALL} --config json://${MYDIR}/qc-full.json --local --host epn "
+add_W o2-dpl-output-proxy "${ARGS_ALL} --dataspec ${PROXY_OUTSPEC} --proxy-channel-name tof-time-calib-input-proxy --channel-config \"name=tof-time-calib-input-proxy,method=connect,type=push,transport=zeromq,rateLogging=0\" "
 WORKFLOW+="o2-dpl-run ${ARGS_ALL} ${GLOBALDPLOPT}"
 
 if [ $WORKFLOWMODE == "print" ]; then
