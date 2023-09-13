e21d0a1a5f638b97e839a172e912e636f32a5b94
rmunzer <97919772+rmunzer@users.noreply.github.com>
2023-09-13 21:53:43 +0200

    Tpc laser (#1225)
    * Change of laser workflows for Dense packade format
    
    * Add laser raw-filter workflow and move pattern_generator workflow to TPC/test
    
    * laser raw-filter cleanup
    
    * Bugfix
    
    * Add new options for laser run
    
    * fixed for tpc-laser-multi
    
    * Remove only-publsihed-on-eos
    
    * cosmetic correction
    
    * add optin to add publish ony on eos

--- DATA/production/calib/tpc-laser-aggregator.sh
+++ DATA/production/calib/tpc-laser-aggregator.sh
@@ -6,7 +6,7 @@ source common/getCommonArgs.sh
 
 PROXY_INSPEC="A:TPC/LASERTRACKS;B:TPC/CEDIGITS;eos:***/INFORMATION;D:TPC/CLUSREFS"
 
-CALIB_CONFIG= "TPCCalibPulser.FirstTimeBin=450;TPCCalibPulser.LastTimeBin=550;TPCCalibPulser.NbinsQtot=250;TPCCalibPulser.XminQtot=2;TPCCalibPulser.XmaxQtot=502;TPCCalibPulser.MinimumQtot=8;TPCCalibPulser.MinimumQmax=6;TPCCalibPulser.XminT0=450;TPCCalibPulser.XmaxT0=550;TPCCalibPulser.NbinsT0=400;keyval.output_dir=/dev/null" \
+CALIB_CONFIG="TPCCalibPulser.FirstTimeBin=450;TPCCalibPulser.LastTimeBin=550;TPCCalibPulser.NbinsQtot=250;TPCCalibPulser.XminQtot=2;TPCCalibPulser.XmaxQtot=502;TPCCalibPulser.MinimumQtot=8;TPCCalibPulser.MinimumQmax=6;TPCCalibPulser.XminT0=450;TPCCalibPulser.XmaxT0=550;TPCCalibPulser.NbinsT0=400;keyval.output_dir=/dev/null"
 
 CCDB_PATH="http://o2-ccdb.internal"
 
@@ -18,6 +18,7 @@ max_events=300
 publish_after=440
 min_tracks=0
 num_lanes=36
+
 
 if [[ ! -z ${TPC_CALIB_MAX_EVENTS:-} ]]; then
     max_events=${TPC_CALIB_MAX_EVENTS}
@@ -33,7 +34,14 @@ if [[ ! -z ${TPC_CALIB_LANES_PAD_RAW:-} ]]; then
     num_lanes=${TPC_CALIB_LANES_PAD_RAW}
 fi
 
-EXTRA_CONFIG="--calib-type ce --publish-after-tfs ${publish_after} --max-events ${max_events} --lanes #{num_lanes} --check-calib-infos" 
+EXTRA_CONFIG="--calib-type ce --publish-after-tfs ${publish_after} --max-events ${max_events} --lanes ${num_lanes} --check-calib-infos"
+
+EXTRA_CONFIG_TRACKS=""
+
+if [[ ${TPC_CALIB_TRACKS_PUBLISH_EOS:-} == 1 ]]; then
+    EXTRA_CONFIG_TRACKS="--only-publish-on-eos"
+fi
+
 
 
 o2-dpl-raw-proxy $ARGS_ALL \
@@ -41,12 +49,11 @@ o2-dpl-raw-proxy $ARGS_ALL \
   --dataspec "${PROXY_INSPEC}" \
   --network-interface ib0 \
   --channel-config "name=tpc-laser-input-proxy,method=bind,type=pull,rateLogging=0,transport=zeromq" \
- | o2-tpc-calib-laser-tracks  ${ARGS_ALL} \
- --use-filtered-tracks --only-publish-on-eos --min-tfs=${min_tracks}\
+ | o2-tpc-calib-laser-tracks  ${ARGS_ALL} --use-filtered-tracks ${EXTRA_CONFIG_TRACKS} --min-tfs=${min_tracks} \
+ --condition-remap "file:///home/wiechula/processData/inputFilesTracking/triggeredLaser/=GLO/Config/GRPECS;file:///home/wiechula/processData/inputFilesTracking/triggeredLaser/=GLO/Config/GRPMagField;file:///home/wiechula/processData/inputFilesTracking/triggeredLaser=TPC/Calib/LaserTracks" \
  | o2-tpc-calib-pad-raw ${ARGS_ALL} \
- --configKeyValues ${CALIB_CONFIG} \ 
- ${EXTRA_CONFIG} \
+ --configKeyValues ${CALIB_CONFIG}  ${EXTRA_CONFIG} \
  | o2-calibration-ccdb-populator-workflow  ${ARGS_ALL} \
  --ccdb-path ${CCDB_PATH} \
- | o2-qc ${ARGS_ALL} --config ${QC_CONFIG} --local --host ${HOST} \
  | o2-dpl-run $ARGS_ALL --dds ${WORKFLOWMODE_FILE}
+
