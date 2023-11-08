341bf9f416fa6ea6ebce8abda9f223fb03aaff76
aimeric-landou <46970521+aimeric-landou@users.noreply.github.com>
2023-11-08 12:08:30 +0100

    HF Jet MC - fix weighting, using new HF jet generator (#1319)

--- MC/run/PWGGAJE/run_jets_HF_ccbar.sh
+++ MC/run/PWGGAJE/run_jets_HF_ccbar.sh
@@ -15,42 +15,21 @@
 
 RNDSEED=${RNDSEED:-0}   # [default = 0] time-based random seed
 
-NSIGEVENTS=${NSIGEVENTS:-10}
+NSIGEVENTS=${NSIGEVENTS:-5}
 NTIMEFRAMES=${NTIMEFRAMES:-1}
 NWORKERS=${NWORKERS:-8}
 MODULES="--skipModules ZDC" #"PIPE ITS TPC EMCAL"
 CONFIG_ENERGY=${CONFIG_ENERGY:-13600.0}
 SIMENGINE=${SIMENGINE:-TGeant4}
-WEIGHTPOW=${WEIGHTPOW:-6.0}
 [[ ${SPLITID} != "" ]] && SEED="-seed ${SPLITID}" || SEED=""
 
-# Default for weighted productions
-PTHATMIN=${PTHATMIN:-5.0}
-PTHATMAX=${PTHATMAX:-300.0}
+#ccbar filter and bias2SelectionPow and PtHat settings are in the ini file given below
+${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM ${CONFIG_ENERGY} -col pp -gen external -proc "jets"                 \
+                                            -tf ${NTIMEFRAMES} -ns ${NSIGEVENTS} -e ${SIMENGINE}                    \
+                                            -j ${NWORKERS} -mod "--skipModules ZDC"                                 \
+                                            -interactionRate 500000 -confKey "Diamond.width[2]=6." ${SEED}          \
+                                            -ini $O2DPG_ROOT/MC/config/PWGGAJE/ini/GeneratorHFJETrigger_ccbar.ini
 
-# Define the pt hat bin arrays
-pthatbin_loweredges=(0 5 7 9 12 16 21 28 36 45 57 70 85 99 115 132 150 169 190 212 235)
-pthatbin_higheredges=( 5 7 9 12 16 21 28 36 45 57 70 85 99 115 132 150 169 190 212 235 -1)
-
-# Recover environmental vars for pt binning
-#PTHATBIN=${PTHATBIN:-1}
-
-if [ -z "$PTHATBIN" ]; then
-    echo "Open Pt-hat range set"
-else
-  PTHATMIN=${pthatbin_loweredges[$PTHATBIN]}
-  PTHATMAX=${pthatbin_higheredges[$PTHATBIN]}
-fi
-
-
-#ccbar filter
-${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM ${CONFIG_ENERGY} -col pp -gen external -proc "jets"           \
-                                            -ptHatMin ${PTHATMIN} -ptHatMax ${PTHATMAX}                       \
-                                            -tf ${NTIMEFRAMES} -ns ${NSIGEVENTS} -e ${SIMENGINE}              \
-                                            -j ${NWORKERS} -mod "--skipModules ZDC"                           \
-                                            -interactionRate 500000 -confKey "Diamond.width[2]=6." ${SEED}    \
-                                            -ini $O2DPG_ROOT/MC/config/PWGHF/ini/GeneratorHFTrigger_ccbar.ini \
-                                            -weightPow ${WEIGHTPOW}
 
 # run workflow
 # allow increased timeframe parallelism with --cpu-limit 32
