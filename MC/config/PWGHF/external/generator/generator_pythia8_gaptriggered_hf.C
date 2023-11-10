102057cf932fa4a24db0c741f23b0b34b732f87b
atriolo <andrea.sofia.triolo@cern.ch>
2023-11-10 12:28:01 +0100

    Generalizing alternate injection of quarks and hadrons

--- MC/config/PWGHF/external/generator/generator_pythia8_gaptriggered_hf.C
+++ MC/config/PWGHF/external/generator/generator_pythia8_gaptriggered_hf.C
@@ -2,36 +2,66 @@
 #include "Generators/GeneratorPythia8.h"
 #include "Pythia8/Pythia.h"
 #include "TRandom.h"
+#include <fairlogger/Logger.h>
 
 #include <string>
 #include <vector>
 
 using namespace Pythia8;
 
-class GeneratorPythia8GapTriggeredHF : public o2::eventgen::GeneratorPythia8 {
+class GeneratorPythia8GapTriggeredHF : public o2::eventgen::GeneratorPythia8
+{
 public:
-
   /// default constructor
   GeneratorPythia8GapTriggeredHF() = default;
 
   /// constructor
-  GeneratorPythia8GapTriggeredHF(int inputTriggerRatio = 5, std::vector<int> quarkPdgList = {4}, bool doAltInjection = false) {
+  GeneratorPythia8GapTriggeredHF(int inputTriggerRatio = 5, std::vector<int> quarkPdgList = {}, std::vector<int> hadronPdgList = {})
+  {
 
     mGeneratedEvents = 0;
-    mHadronPdg = 0; // unless differently set, we do not trigger on specific hadron species
-    mQuarkPdg = quarkPdgList[0];
     mInverseTriggerRatio = inputTriggerRatio;
     mQuarkRapidityMin = -1.5;
     mQuarkRapidityMax = -1.5;
     mHadRapidityMin = -1.5;
     mHadRapidityMax = -1.5;
-    mDoNoQuarkTrigger = false;
-    mDoAltInjection = doAltInjection;
+    mDoNoQuarkTrigger = quarkPdgList.size() == 0;
     mQuarkPdgList = quarkPdgList;
+    mHadronPdgList = hadronPdgList;
+    if (mQuarkPdgList.size() > 1 && mHadronPdgList.size() > 1)
+    {
+      LOG(fatal)<<"GeneratorPythia8GapTriggeredHF: Only one between hadron list and quark list can have more than one element";
+    }
+    Print();
   }
 
   ///  Destructor
   ~GeneratorPythia8GapTriggeredHF() = default;
+
+  ///  Print the input
+  void Print()
+  {
+    std::cout << "********** GeneratorPythia8GapTriggeredHF configuration dump **********" << std::endl;
+    fmt::printf("* Trigger ratio: %d\n", mInverseTriggerRatio);
+    fmt::printf("* Quark pdg: %d\n", mQuarkPdg);
+    fmt::printf("* Quark rapidity: %f - %f\n", mQuarkRapidityMin, mQuarkRapidityMax);
+    fmt::printf("* Hadron pdg: %d\n", mHadronPdg);
+    fmt::printf("* Hadron rapidity: %f - %f\n", mHadRapidityMin, mHadRapidityMax);
+    fmt::printf("* No quark trigger: %d\n", mDoNoQuarkTrigger);
+    fmt::printf("* Quark pdg list: ");
+    for (auto pdg : mQuarkPdgList)
+    {
+      fmt::printf("%d ", pdg);
+    }
+    fmt::printf("\n");
+    fmt::printf("* Hadron pdg list: ");
+    for (auto pdg : mHadronPdgList)
+    {
+      fmt::printf("%d ", pdg);
+    }
+    fmt::printf("\n");
+    std::cout << "***********************************************************************" << std::endl;
+  }
 
   bool Init() override
   {
@@ -41,8 +71,8 @@ public:
     return o2::eventgen::GeneratorPythia8::Init();
   }
 
-  void addTriggerOnHadron(int hadPdg) { mHadronPdg = hadPdg; };
-  void setQuarkTrigger (bool doNoQuarkTrigger) { mDoNoQuarkTrigger = doNoQuarkTrigger; };
+  void addTriggerOnHadron(int hadPdg) { mHadronPdgList.push_back(hadPdg); };
+  void setQuarkTrigger(bool doNoQuarkTrigger) { mDoNoQuarkTrigger = doNoQuarkTrigger; };
   void setQuarkRapidity(float yMin, float yMax)
   {
     mQuarkRapidityMin = yMin;
@@ -56,28 +86,51 @@ public:
 
 protected:
   //__________________________________________________________________
-  bool generateEvent() override {
+  bool generateEvent() override
+  {
 
     // Simple straightforward check to alternate generators
-    if (mGeneratedEvents % mInverseTriggerRatio == 0) {
+    if (mGeneratedEvents % mInverseTriggerRatio == 0)
+    {
+
+      if (mQuarkPdgList.size() > 1)
+      {
+        int indexq = (mGeneratedEvents / mInverseTriggerRatio) % mQuarkPdgList.size();
+        mQuarkPdg = mQuarkPdgList[indexq];
+      }
+      else if (mQuarkPdgList.size() == 1)
+      {
+        mQuarkPdg = mQuarkPdgList[0];
+      }
+
+      // Alternate Omega and Xi if enabled (with the same ratio)
+      if (mHadronPdgList.size())
+      {
+        int indexh = (mGeneratedEvents / mInverseTriggerRatio) % mHadronPdgList.size();
+        mHadronPdg = mHadronPdgList[indexh];
+      }
+      else if (mHadronPdgList.size() == 1)
+      {
+        mHadronPdg = mHadronPdgList[0];
+      }
+
       // Generate event of interest
       bool genOk = false;
-      while (!genOk) {
-        if (GeneratorPythia8::generateEvent()) {
+      while (!genOk)
+      {
+        if (GeneratorPythia8::generateEvent())
+        {
           genOk = selectEvent(mPythia.event);
-        }        
+        }
       }
       notifySubGenerator(mQuarkPdg);
-
-      // Alternate charm and beauty if enabled (with the same ratio)
-      if(mDoAltInjection) {
-        mQuarkPdg = (mQuarkPdg == mQuarkPdgList[0]) ? mQuarkPdgList[1] : mQuarkPdgList[0];
-      }
-
-    } else {
+    }
+    else
+    {
       // Generate minimum-bias event
       bool genOk = false;
-      while (!genOk) {
+      while (!genOk)
+      {
         genOk = GeneratorPythia8::generateEvent();
       }
       notifySubGenerator(0);
@@ -88,54 +141,60 @@ protected:
     return true;
   }
 
-  bool selectEvent(const Pythia8::Event& event)
+  bool selectEvent(const Pythia8::Event &event)
   {
-    bool isGoodAtPartonLevel;
-    bool isGoodAtHadronLevel;
-    if(mDoNoQuarkTrigger){
-      isGoodAtPartonLevel = (mHadronPdg != 0) ? true : false;
-      isGoodAtHadronLevel = (mHadronPdg != 0) ? false : true;
-    } else {
-      isGoodAtPartonLevel = false;
-      isGoodAtHadronLevel = (mHadronPdg != 0) ? false : true;
-    }
 
-    for (auto iPart{0}; iPart < event.size(); ++iPart) {
+    bool isGoodAtPartonLevel{mQuarkPdgList.size() == 0};
+    bool isGoodAtHadronLevel{mHadronPdgList.size() == 0};
+
+    for (auto iPart{0}; iPart < event.size(); ++iPart)
+    {
 
       // search for Q-Qbar mother with at least one Q in rapidity window
-      if (!isGoodAtPartonLevel) {
+      if (!isGoodAtPartonLevel)
+      {
         auto daughterList = event[iPart].daughterList();
         bool hasQ = false, hasQbar = false, atSelectedY = false;
-        for (auto iDau : daughterList) {
-          if (event[iDau].id() == mQuarkPdg) {
+        for (auto iDau : daughterList)
+        {
+          if (event[iDau].id() == mQuarkPdg)
+          {
             hasQ = true;
-            if ((event[iDau].y() > mQuarkRapidityMin) && (event[iDau].y() < mQuarkRapidityMax)) {
+            if ((event[iDau].y() > mQuarkRapidityMin) && (event[iDau].y() < mQuarkRapidityMax))
+            {
               atSelectedY = true;
             }
           }
-          if (event[iDau].id() == -mQuarkPdg) {
+          if (event[iDau].id() == -mQuarkPdg)
+          {
             hasQbar = true;
-            if ((event[iDau].y() > mQuarkRapidityMin) && (event[iDau].y() < mQuarkRapidityMax)) {
+            if ((event[iDau].y() > mQuarkRapidityMin) && (event[iDau].y() < mQuarkRapidityMax))
+            {
               atSelectedY = true;
             }
-          }  
+          }
         }
-        if (hasQ && hasQbar && atSelectedY) {
+        if (hasQ && hasQbar && atSelectedY)
+        {
           isGoodAtPartonLevel = true;
         }
       }
 
       // search for hadron in rapidity window
-      if (!isGoodAtHadronLevel) {
+      if (!isGoodAtHadronLevel)
+      {
         int id = std::abs(event[iPart].id());
         float rap = event[iPart].y();
-        if (id == mHadronPdg && rap > mHadRapidityMin && rap < mHadRapidityMax) {
+        if (id == mHadronPdg && rap > mHadRapidityMin && rap < mHadRapidityMax)
+        {
           isGoodAtHadronLevel = true;
         }
       }
 
       // we send the trigger
-      if (isGoodAtPartonLevel && isGoodAtHadronLevel) {
+      if (isGoodAtPartonLevel && isGoodAtHadronLevel)
+      {
+        LOG(debug)<<"EVENT SELECTED: Found particle "<<event[iPart].id()<<" at rapidity "<<event[iPart].y()<<"\n";
         return true;
       }
     }
@@ -148,10 +207,10 @@ private:
   Pythia8::Event mOutputEvent;
 
   // Properties of selection
-  int mQuarkPdg;
+  int mQuarkPdg = 0;
   float mQuarkRapidityMin;
   float mQuarkRapidityMax;
-  int mHadronPdg;
+  int mHadronPdg = 0;
   float mHadRapidityMin;
   float mHadRapidityMax;
   bool mDoNoQuarkTrigger;
@@ -161,52 +220,71 @@ private:
   int mInverseTriggerRatio;
 
   // Control alternate injection of charm and beauty
-  bool mDoAltInjection;
-  std::vector<int> mQuarkPdgList;
+  std::vector<int> mQuarkPdgList = {};
+
+  // Control alternate injection of Omega and Xi
+  std::vector<int> mHadronPdgList = {};
 };
 
 // Predefined generators:
 // Charm-enriched
-FairGenerator *GeneratorPythia8GapTriggeredCharm(int inputTriggerRatio, float yQuarkMin=-1.5, float yQuarkMax=1.5, float yHadronMin=-1.5, float yHadronMax=1.5, int pdgCodeCharmHadron=0, bool doNoQuarkTrigger=false) {
-  auto myGen = new GeneratorPythia8GapTriggeredHF(inputTriggerRatio, std::vector<int>{4}, false);
+FairGenerator *GeneratorPythia8GapTriggeredCharm(int inputTriggerRatio, float yQuarkMin = -1.5, float yQuarkMax = 1.5, float yHadronMin = -1.5, float yHadronMax = 1.5, std::vector<int> hadronPdgList = {})
+{
+  auto myGen = new GeneratorPythia8GapTriggeredHF(inputTriggerRatio, std::vector<int>{4}, hadronPdgList);
   auto seed = (gRandom->TRandom::GetSeed() % 900000000);
   myGen->readString("Random:setSeed on");
   myGen->readString("Random:seed " + std::to_string(seed));
   myGen->setQuarkRapidity(yQuarkMin, yQuarkMax);
-  if(pdgCodeCharmHadron != 0) {
-    myGen->setQuarkTrigger(doNoQuarkTrigger);
-    myGen->addTriggerOnHadron(pdgCodeCharmHadron);
+  if (hadronPdgList.size() != 0)
+  {
     myGen->setHadronRapidity(yHadronMin, yHadronMax);
   }
   return myGen;
 }
 
 // Beauty-enriched
-FairGenerator *GeneratorPythia8GapTriggeredBeauty(int inputTriggerRatio, float yQuarkMin=-1.5, float yQuarkMax=1.5, float yHadronMin=-1.5, float yHadronMax=1.5, int pdgCodeCharmHadron=0, bool doNoQuarkTrigger=false) {
-  auto myGen = new GeneratorPythia8GapTriggeredHF(inputTriggerRatio, std::vector<int>{5}, false);
+FairGenerator *GeneratorPythia8GapTriggeredBeauty(int inputTriggerRatio, float yQuarkMin = -1.5, float yQuarkMax = 1.5, float yHadronMin = -1.5, float yHadronMax = 1.5, std::vector<int> hadronPdgList = {})
+{
+  auto myGen = new GeneratorPythia8GapTriggeredHF(inputTriggerRatio, std::vector<int>{5}, hadronPdgList);
   auto seed = (gRandom->TRandom::GetSeed() % 900000000);
   myGen->readString("Random:setSeed on");
   myGen->readString("Random:seed " + std::to_string(seed));
   myGen->setQuarkRapidity(yQuarkMin, yQuarkMax);
-  if(pdgCodeCharmHadron != 0) {
-    myGen->setQuarkTrigger(doNoQuarkTrigger);
-    myGen->addTriggerOnHadron(pdgCodeCharmHadron);
+  if (hadronPdgList.size() != 0)
+  {
     myGen->setHadronRapidity(yHadronMin, yHadronMax);
   }
   return myGen;
 }
 
 // Charm and beauty enriched (with same ratio)
-FairGenerator *GeneratorPythia8GapTriggeredCharmAndBeauty(int inputTriggerRatio, float yQuarkMin=-1.5, float yQuarkMax=1.5, float yHadronMin=-1.5, float yHadronMax=1.5, int pdgCodeCharmHadron=0, bool doNoQuarkTrigger=false) {
-  auto myGen = new GeneratorPythia8GapTriggeredHF(inputTriggerRatio,  std::vector<int>{4, 5}, true);
+FairGenerator *GeneratorPythia8GapTriggeredCharmAndBeauty(int inputTriggerRatio, float yQuarkMin = -1.5, float yQuarkMax = 1.5, float yHadronMin = -1.5, float yHadronMax = 1.5, std::vector<int> hadronPdgList = {})
+{
+  auto myGen = new GeneratorPythia8GapTriggeredHF(inputTriggerRatio, std::vector<int>{4, 5}, hadronPdgList);
   auto seed = (gRandom->TRandom::GetSeed() % 900000000);
   myGen->readString("Random:setSeed on");
   myGen->readString("Random:seed " + std::to_string(seed));
   myGen->setQuarkRapidity(yQuarkMin, yQuarkMax);
-  if(pdgCodeCharmHadron != 0) {
-    myGen->setQuarkTrigger(doNoQuarkTrigger);
-    myGen->addTriggerOnHadron(pdgCodeCharmHadron);
+  if (hadronPdgList.size() != 0)
+  {
     myGen->setHadronRapidity(yHadronMin, yHadronMax);
   }
+  return myGen;
+}
+
+FairGenerator *GeneratorPythia8GapHF(int inputTriggerRatio, float yQuarkMin = -1.5, float yQuarkMax = 1.5, float yHadronMin = -1.5, float yHadronMax = 1.5, std::vector<int> quarkPdgList = {}, std::vector<int> hadronPdgList = {})
+{
+  if (hadronPdgList.size() == 0 && quarkPdgList.size() == 0)
+  {
+    LOG(fatal) << "GeneratorPythia8GapHF: At least one quark or hadron PDG code must be specified";
+  }
+  auto myGen = new GeneratorPythia8GapTriggeredHF(inputTriggerRatio, quarkPdgList, hadronPdgList);
+  auto seed = (gRandom->TRandom::GetSeed() % 900000000);
+  myGen->readString("Random:setSeed on");
+  myGen->readString("Random:seed " + std::to_string(seed));
+  myGen->setQuarkRapidity(yQuarkMin, yQuarkMax);
+  myGen->setQuarkTrigger(quarkPdgList.size());
+  myGen->setHadronRapidity(yHadronMin, yHadronMax);
+
   return myGen;
 }
---
+++ MC/run/PWGHF/run_pp_HFtriggers_xi_omega_c_gaptrigger.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#
+# A example workflow MC->RECO->AOD for a simple pp min bias
+# production, targetting test beam conditions.
+
+# make sure O2DPG + O2 is loaded
+[ ! "${O2DPG_ROOT}" ] && echo "Error: This needs O2DPG loaded" && exit 1
+[ ! "${O2_ROOT}" ] && echo "Error: This needs O2 loaded" && exit 1
+
+# ----------- LOAD UTILITY FUNCTIONS --------------------------
+. ${O2_ROOT}/share/scripts/jobutils.sh
+
+# ----------- START ACTUAL JOB  -----------------------------
+
+NWORKERS=${NWORKERS:-8}
+CPU_LIMIT=${CPU_LIMIT:-32}
+MODULES="--skipModules ZDC"
+SIMENGINE=${SIMENGINE:-TGeant4}
+NSIGEVENTS=${NSIGEVENTS:-30}
+NTIMEFRAMES=${NTIMEFRAMES:-1}
+[[ ${SPLITID} != "" ]] && SEED="-seed ${SPLITID}" || SEED=""
+# create workflow
+
+#ccbar filter
+${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM 13600 -col pp -gen external -j ${NWORKERS} -ns ${NSIGEVENTS} -tf ${NTIMEFRAMES} -interactionRate 500000 -confKey "Diamond.width[2]=6." -e ${SIMENGINE} ${SEED} -mod "--skipModules ZDC" \
+        -ini $O2DPG_ROOT/MC/config/PWGHF/ini/GeneratorHFTrigger_Xi_Omega_C.ini \
+
+# run workflow
+# allow increased timeframe parallelism with --cpu-limit 32
+${O2DPG_ROOT}/MC/bin/o2_dpg_workflow_runner.py -f workflow.json -tt aod --cpu-limit ${CPU_LIMIT}
