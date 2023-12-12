749681ff7a229798a24e9ac5168f3a29f067eaf5
mcoquet642 <maurice.louis.coquet@cern.ch>
2023-12-12 19:53:53 +0100

    REmoving corrected geometry option

--- MC/run/ANCHOR/2021/OCT/pass4/anchorMC.sh
+++ MC/run/ANCHOR/2021/OCT/pass4/anchorMC.sh
@@ -104,7 +104,7 @@ baseargs="-tf ${NTIMEFRAMES} --split-id ${ALIEN_JDL_SPLITID:-1} --prod-split ${A
 # THIS NEEDS TO COME FROM OUTSIDE
 remainingargs="-gen pythia8 -proc cdiff -ns ${NSIGEVENTS}                                                                                                                 \
                -interactionRate ${INTERACTIONRATE}                                                                                                                        \
-               -confKey \"Diamond.width[2]=6.0;Diamond.width[0]=0.01;Diamond.width[1]=0.01;Diamond.position[0]=0.0;Diamond.position[1]=-0.035;Diamond.position[2]=0.41;MFTBase.buildAlignment=true\"  \
+               -confKey \"Diamond.width[2]=6.0;Diamond.width[0]=0.01;Diamond.width[1]=0.01;Diamond.position[0]=0.0;Diamond.position[1]=-0.035;Diamond.position[2]=0.41\"  \
               --include-local-qc --include-analysis"
 
 remainingargs="${remainingargs} -e ${SIMENGINE} -j ${NWORKERS}"
