0b2efd059681c21bfb9eb301c2a326aa9f22812a
catalinristea <catalin.ristea@live.com>
2024-01-31 17:22:39 +0200

    Update anchorMC.sh - removing the use of ideal MFT alignment (#1420)
    * Update anchorMC.sh - removing the use of ideal MFT alignment
    
    * Update anchorMC.sh - removed CCDB prefetching

--- MC/run/ANCHOR/anchorMC.sh
+++ MC/run/ANCHOR/anchorMC.sh
@@ -212,21 +212,8 @@ fi
 TIMESTAMP=`grep "Determined timestamp to be" timestampsampling_${ALIEN_JDL_LPMRUNNUMBER}.log | awk '//{print $6}'`
 echo "TIMESTAMP IS ${TIMESTAMP}"
 
-# -- PREFETCH CCDB OBJECTS TO DISC      --
-# (make sure the right objects at the right timestamp are fetched
-#  until https://alice.its.cern.ch/jira/browse/O2-2852 is fixed)
-# NOTE: In fact, looking at the ticket, it should be fixed now. However, not changing at the moment as further tests would be needed to confirm here.
-
-CCDBOBJECTS="/CTP/Calib/OrbitReset /GLO/Config/GRPMagField/ /GLO/Config/GRPLHCIF /ITS/Calib/DeadMap /ITS/Calib/NoiseMap /ITS/Calib/ClusterDictionary /TPC/Calib/PadGainFull /TPC/Calib/TopologyGain /TPC/Calib/TimeGain /TPC/Calib/PadGainResidual /TPC/Config/FEEPad /TOF/Calib/Diagnostic /TOF/Calib/LHCphase /TOF/Calib/FEELIGHT /TOF/Calib/ChannelCalib /MFT/Calib/DeadMap /MFT/Calib/NoiseMap /MFT/Calib/ClusterDictionary /FV0/Calibration/ChannelTimeOffset"
-
-${O2_ROOT}/bin/o2-ccdb-downloadccdbfile --host http://alice-ccdb.cern.ch/ -p ${CCDBOBJECTS} -d ${ALICEO2_CCDB_LOCALCACHE} --timestamp ${TIMESTAMP}
-if [ ! "$?" == "0" ]; then
-  echo "Problem during CCDB prefetching of ${CCDBOBJECTS}. Exiting."
-  exit 1
-fi
-
-# -- Create aligned geometry using ITS and MFT ideal alignments to avoid overlaps in geant
-CCDBOBJECTS_IDEAL_MC="ITS/Calib/Align MFT/Calib/Align"
+# -- Create aligned geometry using ITS ideal alignment to avoid overlaps in geant
+CCDBOBJECTS_IDEAL_MC="ITS/Calib/Align"
 TIMESTAMP_IDEAL_MC=1
 ${O2_ROOT}/bin/o2-ccdb-downloadccdbfile --host http://alice-ccdb.cern.ch/ -p ${CCDBOBJECTS_IDEAL_MC} -d ${ALICEO2_CCDB_LOCALCACHE} --timestamp ${TIMESTAMP_IDEAL_MC}
 if [ ! "$?" == "0" ]; then
@@ -234,7 +221,7 @@ if [ ! "$?" == "0" ]; then
   exit 1
 fi
 
-echo "run with echo in pipe" | ${O2_ROOT}/bin/o2-create-aligned-geometry-workflow --configKeyValues "HBFUtils.startTime=${TIMESTAMP}" --condition-remap=file://${ALICEO2_CCDB_LOCALCACHE}=ITS/Calib/Align,MFT/Calib/Align -b
+echo "run with echo in pipe" | ${O2_ROOT}/bin/o2-create-aligned-geometry-workflow --configKeyValues "HBFUtils.startTime=${TIMESTAMP}" --condition-remap=file://${ALICEO2_CCDB_LOCALCACHE}=ITS/Calib/Align -b
 mkdir -p $ALICEO2_CCDB_LOCALCACHE/GLO/Config/GeometryAligned
 ln -s -f $PWD/o2sim_geometry-aligned.root $ALICEO2_CCDB_LOCALCACHE/GLO/Config/GeometryAligned/snapshot.root
 
