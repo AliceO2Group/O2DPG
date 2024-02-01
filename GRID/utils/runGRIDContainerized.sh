50ec7dc3ae2ae95327f4787fc7ed74cfaaf4f86d
swenzel <sandro.wenzel@cern.ch>
2024-02-01 13:35:05 +0100

    platform independence of containerized execution

--- GRID/utils/runGRIDContainerized.sh
+++ GRID/utils/runGRIDContainerized.sh
@@ -7,12 +7,30 @@ SCRIPT=$1
 [ $SCRIPT == "" ] && echo "Please provide a script to run" && exit 1
 echo "Trying to run script ${SCRIPT} in a container environment"
 
+# detect architecture (ARM or X86)
+ARCH=$(uname -i)
+if [ "$ARCH" == "aarch64" ] || [ "$arch" == "x86" ]; then
+    echo "Detected hardware architecture : $ARCH"
+else
+    echo "Invalid architecture ${ARCH} detected. Exiting"
+    exit 1
+fi
+if [ "$ARCH" == "aarch64" ]; then
+  ISAARCH64="1"
+fi
 # we just use the default singularity container
-APPTAINER_CONTAINER=/cvmfs/alice.cern.ch/containers/fs/singularity/default
+APPTAINER_CONTAINER=/cvmfs/alice.cern.ch/containers/fs/singularity/default${ISAARCH64+"-aarch64"}
 
-# create workdir
-WORK_DIR=$(mktemp -d /tmp/alien-job-XXXXXX)
+# create workdir if not specified externally
+if [ ! "${WORK_DIR}" ]; then
+  WORK_DIR=$(mktemp -d /tmp/alien-job-XXXXXX)
+fi
 echo "This job will be run in $WORK_DIR"
+
+if [ ! -d "${WORK_DIR}" ]; then
+    echo "working directory ${WORK_DIR} does not exist; Please create before running"
+    exit 1
+fi
 
 # copy script to WORK_DIR
 cp ${SCRIPT} ${WORK_DIR}/job.sh
@@ -31,5 +49,5 @@ echo "JALIEN_TOKEN_CERT=/workdir/usercert.pem" > ${WORK_DIR}/envfile
 echo "JALIEN_TOKEN_KEY=/workdir/userkey.pem" >> ${WORK_DIR}/envfile
 
 # launch job = script inside the container in the workdir
-/cvmfs/alice.cern.ch/containers/bin/apptainer/current/bin/apptainer exec -C -B /cvmfs:/cvmfs,${WORK_DIR}:/workdir  \
+/cvmfs/alice.cern.ch/containers/bin/apptainer/current${ISAARCH64+"-aarch64"}/bin/apptainer exec -C -B /cvmfs:/cvmfs,${WORK_DIR}:/workdir  \
                                                                     --pwd /workdir --env-file ${WORK_DIR}/envfile ${APPTAINER_CONTAINER} /workdir/job.sh
