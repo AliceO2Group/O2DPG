90c27424166ffa04800a4229c6b13ecc195ac750
Nasir Mehdi Malik <89008506+nasirmehdimalik@users.noreply.github.com>
2024-02-16 18:26:55 +0530

    TRD;) tracking task  in mc workflow

--- MC/bin/o2dpg_sim_workflow.py
+++ MC/bin/o2dpg_sim_workflow.py
@@ -1210,6 +1210,11 @@ for tf in range(1, NTIMEFRAMES + 1):
                 readerCommand='o2-trd-trap-sim',
                 configFilePath='json://${O2DPG_ROOT}/MC/config/QC/json/trd-digits-task.json')
 
+     addQCPerTF(taskName='trdTrackingQC',
+                needs=[TRDTRACKINGtask2['name']],
+                readerCommand='o2-global-track-cluster-reader --track-types "ITS-TPC-TRD" --cluster-types none',
+                configFilePath='json://${O2DPG_ROOT}/MC/config/QC/json/trd-tracking-task.json')
+
      ### TOF
      addQCPerTF(taskName='tofDigitsQC',
                 needs=[getDigiTaskName("TOF")],
