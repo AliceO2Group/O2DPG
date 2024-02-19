26319e42c27e7f5c15a17d0b351ebbfc1f18d403
David Rohr <drohr@jwdt.org>
2024-02-19 10:09:28 +0100

    dpl-workflow: Change default FLP processing in sync mode to CTP only

--- DATA/common/setenv.sh
+++ DATA/common/setenv.sh
@@ -134,7 +134,7 @@ else # Defaults when running on the EPN
   if [[ -z "${SHMTHROW:-}" ]];             then export SHMTHROW=0; fi
   if [[ -z "${TIMEFRAME_SHM_LIMIT:-}" ]];  then export TIMEFRAME_SHM_LIMIT=$(( $SHMSIZE / 2 )); fi
   if [[ -z "${EDJSONS_DIR:-}" ]];          then export EDJSONS_DIR="/scratch/services/ed/jsons_${RUNTYPE}"; fi
-  if [[ -z "${WORKFLOW_DETECTORS_FLP_PROCESSING+x}" ]]; then export WORKFLOW_DETECTORS_FLP_PROCESSING="TOF,CTP"; fi # Current default in sync processing is that FLP processing is only enabled for TOF
+  if [[ -z "${WORKFLOW_DETECTORS_FLP_PROCESSING+x}" ]]; then export WORKFLOW_DETECTORS_FLP_PROCESSING="CTP"; fi # Current default in sync processing is that FLP processing is only enabled for TOF
   if [[ -z "${GEN_TOPO_AUTOSCALE_PROCESSES:-}" ]];      then export GEN_TOPO_AUTOSCALE_PROCESSES=1; fi # On the EPN we should make sure to always use the node to the full extent
 fi
 # Some more options for running on the EPN
