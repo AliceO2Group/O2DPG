5d52ce4a9613e8b647b48af4ac83ed4a26ce05a3
Jeremy Wilkinson <jeremy.wilkinson@cern.ch>
2024-02-23 17:10:36 +0100

    fix converter logic for o2v0converter in test workflow

--- MC/analysis_testing/o2dpg_analysis_test_workflow.py
+++ MC/analysis_testing/o2dpg_analysis_test_workflow.py
@@ -190,7 +190,7 @@ def get_additional_workflows(input_aod):
         o2_analysis_converters = {"O2collision_001": "o2-analysis-collision-converter --doNotSwap",
                                   "O2zdc_001": "o2-analysis-zdc-converter",
                                   "O2bc_001": "o2-analysis-bc-converter",
-                                  "O2v0_001": "o2-analysis-v0converter",
+                                  "O2v0_002": "o2-analysis-v0converter",
                                   "O2trackextra_001": "o2-analysis-tracks-extra-converter"}
         for i in froot.GetListOfKeys():
             if "DF_" not in i.GetName():
