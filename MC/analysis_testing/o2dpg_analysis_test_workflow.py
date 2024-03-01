d6f2fa3c393660fe2f9858482e520762ae0c7f48
Jeremy Wilkinson <jeremy.wilkinson@cern.ch>
2024-03-01 14:52:24 +0100

    typo in ft0

--- MC/analysis_testing/o2dpg_analysis_test_workflow.py
+++ MC/analysis_testing/o2dpg_analysis_test_workflow.py
@@ -192,7 +192,7 @@ def get_additional_workflows(input_aod):
                                   "O2bc_001": "o2-analysis-bc-converter",
                                   "O2v0_002": "o2-analysis-v0converter",
                                   "O2trackextra_001": "o2-analysis-tracks-extra-converter",
-                                  "O2ft0corrected": "o2-ft0-corrected-table"}
+                                  "O2ft0corrected": "o2-analysis-ft0-corrected-table"}
         for i in froot.GetListOfKeys():
             if "DF_" not in i.GetName():
                 continue
