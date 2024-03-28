0fa2f33f2e2d52cd9bf2ad717486a301f84f35ee
Markus Fasel <markus.fasel@cern.ch>
2024-03-28 15:01:34 +0100

    [EMCAL-1118] Fixes in EMCAL pedestal run scripts
    - Fix multiprocessing setting in processor workflow
    - Attach QC to aggregator

--- DATA/production/calib/emc-pedestal-aggregator.sh
+++ DATA/production/calib/emc-pedestal-aggregator.sh
@@ -16,11 +16,14 @@ if [[ $RUNTYPE == "SYNTHETIC" || "${GEN_TOPO_DEPLOYMENT_TYPE:-}" == "ALICE_STAGI
   CCDBPATH2="http://ccdb-test.cern.ch:8080"
 fi
 
+QC_CONFIG="/o2/components/qc/ANY/any/emc-pedestal-qc"
+
 WORKFLOW=
 add_W o2-dpl-raw-proxy "--proxy-name emc-pedestal-input-proxy --dataspec \"$PROXY_INSPEC\" --network-interface ib0 --channel-config \"name=emc-pedestal-input-proxy,method=bind,type=pull,rateLogging=1,transport=zeromq\"" "" 0
 add_W o2-calibration-emcal-pedestal-calib-workflow
 add_W o2-calibration-ccdb-populator-workflow "--ccdb-path=\"$CCDBPATH1\" --sspec-min 0 --sspec-max 0"
 add_W o2-calibration-ccdb-populator-workflow "--ccdb-path=\"$CCDBPATH2\" --sspec-min 1 --sspec-max 1 --name-extention dcs"
+add_QC_from_consul "${QC_CONFIG}"
 WORKFLOW+="o2-dpl-run $ARGS_ALL $GLOBALDPLOPT"
 
 if [ $WORKFLOWMODE == "print" ]; then
