7521fceb8ea8a5edbb190232592287003b4155fb
benedikt-voelkel <benedikt.voelkel@gmx.de>
2024-04-10 17:21:47 +0200

    [AnalysisQC] Change configuration logic (#1590)
    * remove unused analyses
    
    * disable EMCAL analysis
      (to enable, need to revise resource needs, crashes or hangs
       sporadically)
    
    * split config JSONs into executable-specific single JSONs
      Most of those are seen as configurations for "service wagons".
      Each analyses is forced to use those comon configurations and it is
      not possible to have analysis-specific configurations for service
      wagons.
    
    * possible to use special configuration strings in configs; this is done
      to avoid duplications of configs
      * "!ANALYSIS_QC_is_mc!" ("!ANALYSIS_QC_is_data!") will be replaced
        with "true"("false") or "false"("true") depending on whether the
        analyses are run on MC(data)
    
    * new default is to pipe all analyses together;
      to split into single analyses, run
      o2dpg_analysis_testing_workflow.py [...] --split-analyses
    
    Co-authored-by: Benedikt Volkel <benedikt.volkel@cern.ch>
    (cherry picked from commit 1df34c066f1be7a182724c696ec55437d81fa6b9)

--- MC/analysis_testing/o2dpg_analysis_test_utils.py
+++ MC/analysis_testing/o2dpg_analysis_test_utils.py
@@ -4,8 +4,10 @@
 # Analsysis task utilities
 #
 import sys
-from os import environ
-from os.path import join, exists, abspath, expanduser
+from os import environ, listdir
+from os.path import join, abspath
+
+import json
 
 # make sure O2DPG + O2 is loaded
 O2DPG_ROOT=environ.get('O2DPG_ROOT')
@@ -22,40 +24,32 @@ ANALYSIS_VALID_MC = "mc"
 ANALYSIS_VALID_DATA = "data"
 ANALYSIS_COLLISION_SYSTEM_PP = "pp"
 ANALYSIS_COLLISION_SYSTEM_PBPB = "pbpb"
-ANALYSIS_CONFIGURATION_PREFIX = "analysis-testing"
-ANALYSIS_DEFAULT_CONFIGURATION = {ANALYSIS_COLLISION_SYSTEM_PP: {ANALYSIS_VALID_MC: join(O2DPG_ROOT, "MC", "config", "analysis_testing", "json", "default", ANALYSIS_COLLISION_SYSTEM_PP, f"{ANALYSIS_CONFIGURATION_PREFIX}-{ANALYSIS_VALID_MC}.json"),
-                                                                 ANALYSIS_VALID_DATA: join(O2DPG_ROOT, "MC", "config", "analysis_testing", "json", "default", ANALYSIS_COLLISION_SYSTEM_PP, f"{ANALYSIS_CONFIGURATION_PREFIX}-{ANALYSIS_VALID_DATA}.json")},
-                                  ANALYSIS_COLLISION_SYSTEM_PBPB: {ANALYSIS_VALID_MC: join(O2DPG_ROOT, "MC", "config", "analysis_testing", "json", "default", ANALYSIS_COLLISION_SYSTEM_PBPB, f"{ANALYSIS_CONFIGURATION_PREFIX}-{ANALYSIS_VALID_MC}.json"),
-                                                                   ANALYSIS_VALID_DATA: join(O2DPG_ROOT, "MC", "config", "analysis_testing", "json", "default", ANALYSIS_COLLISION_SYSTEM_PBPB, f"{ANALYSIS_CONFIGURATION_PREFIX}-{ANALYSIS_VALID_DATA}.json")}}
 
 
-def sanitize_configuration_path(path):
-    # sanitize path
-    path = path.replace("json://", "")
-    if path[0] != "$":
-        # only do this if there is no potential environment variable given as the first part of the path
-        path = abspath(expanduser(path))
-    return f"json://{path}"
+def adjust_configuration_line(line, data_or_mc, collision_system):
+    line = line.replace('!ANALYSIS_QC_is_mc!', str(data_or_mc == ANALYSIS_VALID_MC).lower())
+    line = line.replace('!ANALYSIS_QC_is_data!', str(data_or_mc == ANALYSIS_VALID_DATA).lower())
+    return line
 
 
-def get_default_configuration(data_or_mc, collision_system):
-    path = ANALYSIS_DEFAULT_CONFIGURATION.get(collision_system, None)
-    if not path:
-        print(f"ERROR: Unknown collision system {collision_system}")
-        return None
-    return path[data_or_mc]
+def adjust_and_get_configuration_path(data_or_mc, collision_system, output_dir):
 
+    final_config = {}
+    path = join(O2DPG_ROOT, "MC", "config", "analysis_testing", "json", "dpl")
+    for config_path in listdir(path):
+        if not config_path.endswith('.json'):
+            continue
+        json_string = ""
+        with open(join(path, config_path), 'r') as f:
+            for line in f:
+                json_string += adjust_configuration_line(line, data_or_mc, collision_system)
+            final_config |= json.loads(json_string)
+        # now we can do some adjustments
+    output_path = abspath(join(output_dir, 'dpl-config.json'))
+    with open(output_path, 'w') as f:
+        json.dump(final_config, f, indent=2)
 
-def get_configuration(analysis_name, data_or_mc, collision_system):
-    path = join(O2DPG_ROOT, "MC", "config", "analysis_testing", "json", analysis_name, collision_system, f"{ANALYSIS_CONFIGURATION_PREFIX}-{data_or_mc}.json")
-    if not exists(path):
-        path = get_default_configuration(data_or_mc, collision_system)
-        if not path:
-            return None
-        print(f"INFO: Use default configuration for {analysis_name}")
-        return sanitize_configuration_path(path)
-
-    return sanitize_configuration_path(path)
+    return output_path
 
 
 def get_collision_system(collision_system=None):
