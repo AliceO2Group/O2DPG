efad493fdf6094134599861eb0955c460c0aa859
jesgum <je8022ka-s@student.lu.se>
2024-05-03 15:15:22 +0200

    Allow external generator to accept seed during initialization

--- MC/config/ALICE3/pythia8/generator_pythia8_ALICE3.C
+++ MC/config/ALICE3/pythia8/generator_pythia8_ALICE3.C
@@ -1,28 +1,40 @@
-
 #if !defined(__CLING__) || defined(__ROOTCLING__)
 #include "Pythia8/Pythia.h"
 #include "FairGenerator.h"
 #include "FairPrimaryGenerator.h"
 #include "Generators/GeneratorPythia8.h"
-#include "TRandom3.h"
-#include "TParticlePDG.h"
-#include "TDatabasePDG.h"
-#include "TMath.h"
-#include <cmath>
+#include <stdlib.h>
+
 using namespace Pythia8;
 #endif
 
 // Default pythia8 minimum bias generator
-// Please do not change
 
 class GeneratorPythia8ALICE3 : public o2::eventgen::GeneratorPythia8
 {
 public:
+
   /// Constructor
-  GeneratorPythia8ALICE3() {}
+  GeneratorPythia8ALICE3() {
+
+    char* job_id = getenv("JOB_ID");
+    int seed;
+
+    if (job_id != NULL) {
+      LOG(info) << "Seed set to JOB_ID: " << seed;
+      seed = atoi(job_id);
+    } else {
+      LOG(info) << "Unable to retrieve JOB_ID";
+      LOG(info) << "Setting seed to 0 (random)";
+      seed = 0;
+    }
+
+    mPythia.readString("Random:seed = "+std::to_string(seed));
+  }
 
   ///  Destructor
   ~GeneratorPythia8ALICE3() = default;
+
 };
 
  FairGenerator *generator_pythia8_ALICE3() 
