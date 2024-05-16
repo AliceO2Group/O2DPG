d104cf3eeee582f5a0b6083c4b8af412c3f9b5d3
iravasen <ivan.ravasenga@cern.ch>
2024-05-16 09:56:29 +0200

    DCSCCDBSERVER_PERS for noise-calib

--- DATA/production/calib/its-noise-aggregator.sh
+++ DATA/production/calib/its-noise-aggregator.sh
@@ -17,7 +17,7 @@ fi
 if [[ -z $NTHREADS ]] ; then NTHREADS=1; fi
 
 CCDBPATH1="http://o2-ccdb.internal"
-CCDBPATH2="$DCSCCDBSERVER"
+CCDBPATH2="$DCSCCDBSERVER_PERS"
 if [[ $RUNTYPE == "SYNTHETIC" || "${GEN_TOPO_DEPLOYMENT_TYPE:-}" == "ALICE_STAGING" || ! -z $ISTEST ]]; then
   CCDBPATH1="http://ccdb-test.cern.ch:8080"
   CCDBPATH2="http://ccdb-test.cern.ch:8080"
