a4f07f2c9e05cb2d23f645c9496e3ec4a24881ad
shahoian <ruben.shahoyan@cern.ch>
2024-05-17 16:10:34 +0200

    Restore space in the var. expansion and add default ALIEN_PROC_ID

--- DATA/production/common/setVarsFromALIEN_PROC_ID.sh
+++ DATA/production/common/setVarsFromALIEN_PROC_ID.sh
@@ -6,17 +6,22 @@
 # let's get the last 9 (for int) and 8 (for int16) digits of ALIEN_PROC_ID, to be passed to define NUMAID and shm-segment-id via O2JOBID, which are int and int16 respectively. Then we make them an int or int16
 ALIEN_PROC_ID_MAX_NDIGITS_INT32=9
 ALIEN_PROC_ID_MAX_NDIGITS_INT16=8
-echo "ALIEN_PROC_ID for current job = ${ALIEN_PROC_ID}"
+if [[ -n $ALIEN_PROC_ID ]]; then
+  echo "ALIEN_PROC_ID for current job = ${ALIEN_PROC_ID}"
+else
+  ALIEN_PROC_ID=123456789
+  echo "ALIEN_PROC_ID was not defined for current job, setting to ${ALIEN_PROC_ID}"
+fi
 
 if [[ -n ${ALIEN_JDL_PACKAGES} ]] && [[ ${#ALIEN_PROC_ID} -lt ${ALIEN_PROC_ID_MAX_NDIGITS_INT32} ]]; then # we are on the grid, and we expect to have the PROC_ID
   echo "We cannot determine O2JOBID, the job id is too short (${ALIEN_PROC_ID}), we need at least ${ALIEN_PROC_ID_MAX_NDIGITS_INT32} digits, returning error"
   exit 2
 fi
 
-ALIEN_PROC_ID_OFFSET_INT32=$((10#${ALIEN_PROC_ID:-${ALIEN_PROC_ID_MAX_NDIGITS_INT32}}))
+ALIEN_PROC_ID_OFFSET_INT32=$((10#${ALIEN_PROC_ID: -${ALIEN_PROC_ID_MAX_NDIGITS_INT32}}))
 echo "ALIEN_PROC_ID_OFFSET_INT32 = $ALIEN_PROC_ID_OFFSET_INT32"
 
-ALIEN_PROC_ID_OFFSET_INT16=$((10#${ALIEN_PROC_ID:-${ALIEN_PROC_ID_MAX_NDIGITS_INT16}}))
+ALIEN_PROC_ID_OFFSET_INT16=$((10#${ALIEN_PROC_ID: -${ALIEN_PROC_ID_MAX_NDIGITS_INT16}}))
 echo "ALIEN_PROC_ID_OFFSET_INT16 = $ALIEN_PROC_ID_OFFSET_INT16"
 
 # let's make them int32 or int16, but not with the max possible value (which would be 0x7fffffff and 0x7fff respectively)
