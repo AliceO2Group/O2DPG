c93e72d8ece54021113f1e7268ce6a965be8a234
alcaliva <32872606+alcaliva@users.noreply.github.com>
2024-05-20 12:50:27 +0200

    Pythia8 events with pt>ptLeading and PDG of interest (#1638)
    (cherry picked from commit 903362d8d80b9a287b7d70b6d2e0ccd8021dc0a8)

---
+++ MC/config/PWGLF/ini/tests/GeneratorLF_HighPt.C
@@ -0,0 +1,25 @@
+int External() {
+  std::string path{"o2sim_Kine.root"};
+
+  TFile file(path.c_str(), "READ");
+  if (file.IsZombie()) {
+    std::cerr << "Cannot open ROOT file " << path << "\n";
+    return 1;
+  }
+
+  auto tree = (TTree *)file.Get("o2sim");
+  if (!tree) {
+    std::cerr << "Cannot find tree o2sim in file " << path << "\n";
+    return 1;
+  }
+  std::vector<o2::MCTrack> *tracks{};
+  tree->SetBranchAddress("MCTrack", &tracks);
+
+  auto nEvents = tree->GetEntries();
+  auto nSelected = tree->Scan("MCTrack.GetPdgCode()", "MCTrack.GetPdgCode() == -2212");
+  if (nSelected == 0) {
+    std::cerr << "No event of interest\n";
+    return 1;
+  }
+  return 0;
+}
