e30a5db99c7c83eb89c368668a118612c64961c7
mbroz84 <michal.broz@cern.ch>
2024-06-25 13:50:45 +0200

    PWGUD Starlight interface (#1663)
    * PWGUD Starlight interface
    
    (cherry picked from commit f79426f195d72ee2f3fe45227fb7150758f5dbcf)

---
+++ MC/config/PWGUD/external/generator/GeneratorStarlightToEvtGen.C
@@ -0,0 +1,33 @@
+// usage (fwdy) :
+// o2-sim -j 4 -n 10 -g external -t external -m "PIPE ITS TPC" -o sgn --configFile GeneratorHF_bbbar_fwdy.ini
+// usage (midy) :
+// o2-sim -j 4 -n 10 -g external -t external -m "PIPE ITS TPC" -o sgn --configFile GeneratorHF_bbbar_midy.ini
+//
+//
+R__ADD_INCLUDE_PATH($O2DPG_ROOT/MC/config/PWGDQ/EvtGen)
+R__ADD_INCLUDE_PATH($O2DPG_ROOT/MC/config/PWGUD/external/generator)
+#include "GeneratorEvtGen.C"
+#include "GeneratorStarlight.C"
+
+FairGenerator*
+  GeneratorStarlightToEvtGen(std::string configuration = "empty",float energyCM = 5020, int beam1Z = 82, int beam1A = 208, int beam2Z = 82, int beam2A = 208)
+{
+  auto gen = new o2::eventgen::GeneratorEvtGen<o2::eventgen::GeneratorStarlight_class>();
+  gen->selectConfiguration(configuration);
+  gen->setCollisionSystem(energyCM, beam1Z, beam1A, beam2Z, beam2A);
+  
+  gen->SetSizePdg(3);
+  gen->AddPdg(443,0);
+  gen->AddPdg(100443,1);
+  gen->AddPdg(223,2);
+  gen->SetPolarization(1); //Transversal
+  
+  TString pathO2 = gSystem->ExpandPathName("$O2DPG_ROOT/MC/config/PWGUD/external/generator/DecayTablesEvtGen");
+  if      (configuration.find("Psi2sToMuPi") != std::string::npos) gen->SetDecayTable(Form("%s/PSI2S.MUMUPIPI.DEC",pathO2.Data()));
+  else if (configuration.find("Psi2sToElPi") != std::string::npos) gen->SetDecayTable(Form("%s/PSI2S.EEPIPI.DEC",pathO2.Data()));
+  else if (configuration.find("RhoPrime") != std::string::npos)    gen->SetDecayTable(Form("%s/RHOPRIME.RHOPIPI.DEC",pathO2.Data()));
+  else if (configuration.find("OmegaTo3Pi") != std::string::npos)  gen->SetDecayTable(Form("%s/OMEGA.3PI.DEC",pathO2.Data()));
+  else if (configuration.find("JpsiToElRad") != std::string::npos) gen->SetDecayTable(Form("%s/JPSI.EE.DEC",pathO2.Data()));
+ 
+  return gen;
+}
