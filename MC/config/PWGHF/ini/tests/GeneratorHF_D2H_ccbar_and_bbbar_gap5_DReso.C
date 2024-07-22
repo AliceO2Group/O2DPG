a51cb3db97e8e1d8f6cf09b0ac75d20f1a8ee645
Luca610 <luca.aglietta@edu.unito.it>
2024-07-22 18:04:00 +0200

    fixed typo

--- MC/config/PWGHF/ini/tests/GeneratorHF_D2H_ccbar_and_bbbar_gap5_DReso.C
+++ MC/config/PWGHF/ini/tests/GeneratorHF_D2H_ccbar_and_bbbar_gap5_DReso.C
@@ -1,5 +1,5 @@
 int External() {
-    std::string path{"/home/luca/alice/Ds_reso/MCReso/07_22_16-59-12/tf1/genevents_Kine.root"};
+    std::string path{"o2sim_Kine.root"};
 
     int checkPdgQuarkOne{4};
     int checkPdgQuarkTwo{5};
