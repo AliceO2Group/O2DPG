5329bae8c662e2e387fae43e7e9f7f7dff232161
motomioya <95481703+motomioya@users.noreply.github.com>
2024-07-31 20:40:42 +0900

    Add scripts for LF to mumu (#1707)
    * Add bbtomuons without forcing semileptonic decay
    * Add test file for bb->mumu with natural decay
    * Add LF to mumu
    * Change gap

---
+++ MC/run/PWGEM/runPythia8GapTriggeredLFmumu_pp.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# make sure O2DPG + O2 is loaded
+#[ ! "${O2DPG_ROOT}" ] && echo "Error: This needs O2DPG loaded" && exit 1
+#[ ! "${O2_ROOT}" ] && echo "Error: This needs O2 loaded" && exit 1
+
+# ----------- SETUP LOCAL CCDB CACHE --------------------------
+#export ALICEO2_CCDB_LOCALCACHE=$PWD/.ccdb
+
+
+# ----------- LOAD UTILITY FUNCTIONS --------------------------
+#. ${O2_ROOT}/share/scripts/jobutils.sh
+
+#NSIGEVENTS=${NSIGEVENTS:-10}
+#NWORKERS=${NWORKERS:-8}
+#NTIMEFRAMES=${NTIMEFRAMES:-1}
+#INTRATE=${INTRATE:-500000}
+
+#CONFIGNAME="Generator_GapTriggered_LFmumu_np1_gap5.ini"
+
+#${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM 13600 -col pp -gen external -j ${NWORKERS} -ns ${NSIGEVENTS} -tf ${NTIMEFRAMES} -e TGeant4 -mod "--skipModules ZDC" \
+     #-ini $O2DPG_ROOT/MC/config/PWGEM/ini/$CONFIGNAME  \
+     #-confKeyBkg "Diamond.width[2]=6" -interactionRate ${INTRATE} 
+
+# run workflow
+#${O2DPG_ROOT}/MC/bin/o2_dpg_workflow_runner.py -f workflow.json -tt aod --cpu-limit 32
+
+#!/usr/bin/env bash
+
+# make sure O2DPG + O2 is loaded
+[ ! "${O2DPG_ROOT}" ] && echo "Error: This needs O2DPG loaded" && exit 1
+[ ! "${O2_ROOT}" ] && echo "Error: This needs O2 loaded" && exit 1
+
+# ----------- SETUP LOCAL CCDB CACHE --------------------------
+export ALICEO2_CCDB_LOCALCACHE=$PWD/.ccdb
+
+
+# ----------- LOAD UTILITY FUNCTIONS --------------------------
+. ${O2_ROOT}/share/scripts/jobutils.sh
+
+NSIGEVENTS=${NSIGEVENTS:-10}
+NWORKERS=${NWORKERS:-8}
+NTIMEFRAMES=${NTIMEFRAMES:-1}
+INTRATE=${INTRATE:-500000}
+
+CONFIGNAME="Generator_GapTriggered_LFmumu.ini"
+
+${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM 13600 -col pp -gen external -j ${NWORKERS} -ns ${NSIGEVENTS} -tf ${NTIMEFRAMES} -e TGeant4 -mod "--skipModules ZDC" \
+     -ini $O2DPG_ROOT/MC/config/PWGEM/ini/$CONFIGNAME  \
+     -confKeyBkg "Diamond.width[2]=6" -interactionRate ${INTRATE} 
+
+# run workflow
+${O2DPG_ROOT}/MC/bin/o2_dpg_workflow_runner.py -f workflow.json -tt aod --cpu-limit 32
