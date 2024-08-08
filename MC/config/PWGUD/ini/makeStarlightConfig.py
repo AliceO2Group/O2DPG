333e3aa9718c2e00d9005353703bfe352d2267d0
mbroz84 <michal.broz@cern.ch>
2024-08-08 16:34:18 +0200

    Adding eta cuts to the trigger (#1700)
    * PWGUD Starlight interface
    * Generator tests
    * Adjusting links
    * Eta cuts in trigger
    * Skip photons
    
    (cherry picked from commit 99e5525440b9c4782df2519943e891a3ffce6bed)

--- MC/config/PWGUD/ini/makeStarlightConfig.py
+++ MC/config/PWGUD/ini/makeStarlightConfig.py
@@ -14,7 +14,7 @@ parser.add_argument('--collType',default='PbPb', choices=['PbPb', 'pPb', 'Pbp', 
 parser.add_argument('--eCM', type=float, default='5360',
                     help='Centre-of-mass energy')
 
-parser.add_argument('--rapidity', default='cent', choices=['cent', 'muon'],
+parser.add_argument('--rapidity', default='cent', choices=['cent_rap', 'muon_rap', 'cent_eta', 'muon_eta'],
                     help='Rapidity to select')
 
 parser.add_argument('--process',default=None, choices=['kTwoGammaToMuLow', 'kTwoGammaToElLow', 'kTwoGammaToMuMedium', 'kTwoGammaToElMedium', 'kTwoGammaToMuHigh', 'kTwoGammaToElHigh', 'kTwoGammaToRhoRho', 'kTwoGammaToF2', 'kCohRhoToPi', 'kCohRhoToElEl', 'kCohRhoToMuMu', 'kCohRhoToPiWithCont', 'kCohRhoToPiFlat', 'kCohPhiToKa', 'kDirectPhiToKaKa','kCohOmegaTo2Pi', 'kCohOmegaTo3Pi', 'kCohOmegaToPiPiPi', 'kCohJpsiToMu', 'kCohJpsiToEl', 'kCohJpsiToElRad', 'kCohJpsiToProton', 'kCohPsi2sToMu','kCohPsi2sToEl', 'kCohPsi2sToMuPi', 'kCohPsi2sToElPi', 'kCohUpsilonToMu', 'kCohUpsilonToEl', 'kIncohRhoToPi', 'kIncohRhoToElEl', 'kIncohRhoToMuMu', 'kIncohRhoToPiWithCont', 'kIncohRhoToPiFlat', 'kIncohPhiToKa', 'kIncohOmegaTo2Pi', 'kIncohOmegaTo3Pi', 'kIncohOmegaToPiPiPi', 'kIncohJpsiToMu', 'kIncohJpsiToEl', 'kIncohJpsiToElRad', 'kIncohJpsiToProton', 'kIncohJpsiToLLbar', 'kIncohPsi2sToMu', 'kIncohPsi2sToEl', 'kIncohPsi2sToMuPi', 'kIncohPsi2sToElPi', 'kIncohUpsilonToMu', 'kIncohUpsilonToEl'],
@@ -78,10 +78,20 @@ else:
 ###Trigger
 fout.write('[TriggerExternal] \n')
 fout.write('fileName = ${O2DPG_ROOT}/MC/config/PWGUD/trigger/selectParticlesInAcceptance.C \n')
-if args.rapidity == 'cent':
-    fout.write('funcName = selectMotherPartInAcc(-0.9,0.9) \n')
-if args.rapidity == 'muon':
-    fout.write('funcName = selectMotherPartInAcc(-4.0,-2.5) \n')
+if 'kTwoGamma' in args.process:
+    if args.rapidity == 'cent_eta':
+        fout.write('funcName = selectDirectPartInAcc(-0.9,0.9) \n')
+    if args.rapidity == 'muon_eta':
+        fout.write('funcName = selectDirectPartInAcc(-4.0,-2.5) \n')
+else:
+    if args.rapidity == 'cent_rap':
+        fout.write('funcName = selectMotherPartInAcc(-0.9,0.9) \n')
+    if args.rapidity == 'muon_rap':
+        fout.write('funcName = selectMotherPartInAcc(-4.0,-2.5) \n')
+    if args.rapidity == 'cent_eta':
+        fout.write('funcName = selectDaughterPartInAcc(-0.9,0.9) \n')
+    if args.rapidity == 'muon_eta':
+        fout.write('funcName = selectDaughterPartInAcc(-4.0,-2.5) \n')
 
 ### close outout file
 fout.close()
