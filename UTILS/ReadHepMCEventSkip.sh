24cdc4fed487255db0d7d853aa91f9fae2d3f9e5
Paul Buehler <pbuehler@users.noreply.github.com>
2024-09-20 13:28:01 +0200

    Added possibility to define a general HEPMCOFFSET. Is usefull when simulating different SPLITIDs of a run in separate jobs. (#1730)
    Co-authored-by: Paul Buehler <paul.buhler@cern.ch>
    (cherry picked from commit 9780b4622fdb82bc8e55d54249e77760dbaa989d)

--- UTILS/ReadHepMCEventSkip.sh
+++ UTILS/ReadHepMCEventSkip.sh
@@ -3,5 +3,22 @@
 # Path to the JSON file
 JSON_FILE=$1
 tf=$2
+
+# get event offset
+JQCOMMAND="jq '.[] | select(.HepMCEventOffset) | .HepMCEventOffset' ${JSON_FILE}"
+offset=`eval ${JQCOMMAND}`
+if [ ! $offset ]
+then
+  offset=0
+fi
+
+# count generated events
 JQCOMMAND="jq '[.[] | select(.tf < ${tf}) | .HepMCEventCount] | add' ${JSON_FILE}"
-eval ${JQCOMMAND}
+events=`eval ${JQCOMMAND}`
+if [ ! $events ]
+then
+  events=0
+fi
+
+# total number of events to skip
+echo $((offset + events))
