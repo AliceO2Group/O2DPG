5869eb1b695e2c0ba60b1cfcdf18d866c507170d
Fabrizio <fabrizio.grosa@cern.ch>
2024-11-05 14:21:04 +0100

    Add simple script to find corrupted AO2Ds in MC productions (#1783)

---
+++ UTILS/findCorruptedAO2Ds.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Simple script to find corrupted AO2Ds using the checkCorruptedAO2Ds.C macro
+
+PRODUCTION=LHC24h2
+RUN=* # use * for all runs
+NJOBS=90
+
+OUTPUTFILE=corrupted_files_$PRODUCTION.txt
+if [ -e "$OUTPUTFILE" ]; then
+  rm $OUTPUTFILE
+fi
+
+# find all files in alien
+if [ "$variable" == "*" ]; then
+  alien_find alien:///alice/sim/2024/${PRODUCTION} 5*/AOD/*/AO2D.root > files_to_check.txt
+else
+  alien_find alien:///alice/sim/2024/${PRODUCTION} ${RUN}/AOD/*/AO2D.root > files_to_check.txt
+fi
+mapfile -t FILESTOCHECK < files_to_check.txt
+
+# process AO2Ds
+process_file() {
+  IFS='/' read -a num <<< "$1"
+  INPUT=$1
+  echo '.x checkCorruptedAO2Ds.C("'${INPUT}'", true)' | root -l -b > log_${num[5]}_${num[7]}
+  echo '.q'
+}
+export -f process_file
+
+parallel -j $NJOBS process_file ::: "${FILESTOCHECK[@]}"
+
+# create list of corrupted files
+touch $OUTPUTFILE
+ERRORSTR="Found corrupted file!"
+for FILE in "${FILESTOCHECK[@]}"; do
+  IFS='/' read -a num <<< "$FILE"
+  if grep -q "$ERRORSTR" log_${num[5]}_${num[7]}; then
+    echo $FILE >> $OUTPUTFILE
+  fi
+done
+
+rm files_to_check.txt
+rm log*
