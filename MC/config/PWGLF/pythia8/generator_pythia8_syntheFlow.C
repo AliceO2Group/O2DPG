ae7fa312e6a251ad1487930f9ed5b3b359c9e3b2
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2024-11-12 22:42:22 +0100

    PWGLF: fix seeding issues (#1798)

--- MC/config/PWGLF/pythia8/generator_pythia8_syntheFlow.C
+++ MC/config/PWGLF/pythia8/generator_pythia8_syntheFlow.C
@@ -99,5 +99,9 @@ private:
 
  FairGenerator *generator_syntheFlow()
  {
-   return new GeneratorPythia8SyntheFlow();
+  auto generator = new GeneratorPythia8SyntheFlow();
+  gRandom->SetSeed(0);
+  generator.readString("Random:setSeed = on");
+  generator.readString("Random:seed =" + std::to_string(gRandom->Integer(900000000 - 2) + 1));
+  return generator;
  }
