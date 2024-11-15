8585daaa357a288a32e498a6fd3101e978fd598e
Marco Giacalone <mgiacalo@gmail.com>
2024-11-15 10:55:11 +0100

    Created EPOS4 external example generator (#1789)
    * Created EPOS4 external example generator
    
    * Updated center-of-mass energy of EPOS4 example
    
    * Implemented GeneratorEPOS4 test
    
    * Testing fix for EPOS4 external
    
    * Revert "Implemented GeneratorEPOS4 test"
    
    This reverts commit e0b88b19613cedc016d497df211d8338257caba9.
    
    * Reapply "Implemented GeneratorEPOS4 test"
    
    This reverts commit 53671e82f12a5b4886d859a1e12f859000ad3fb4.
    
    * Revert "Testing fix for EPOS4 external"
    
    This reverts commit 920cf42cf3cb006814792cf90b606919b9fde3ee.
    
    * Check for LIBDIR
    
    * Added EPO4 as env variable in external script
    
    * Explicit definition of EPOS4 version
    
    * Reorganised and improved fix for CI

---
+++ MC/config/examples/epos4/generator_EPOS4.C
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <fairlogger/Logger.h>
+
+class GeneratorEPOS4 : public o2::eventgen::GeneratorHepMC
+{
+    public:
+        GeneratorEPOS4() = default;
+        ~GeneratorEPOS4() = default;
+
+};
+
+// Next function takes the optns file as argument and edits the maximum number of events to be generated.
+// When used as an external generator it is important that the events passed with the -n flag are the same
+// or lower of the events set in the optns file, otherwise the generation will crash. That is why the .ini
+// example file contains the maximum integer available, assuming less events than that are generated in a real
+// life scenario. Unfortunately a larger number cannot be used at the moment since EPOS4 has a signed integer
+// type for the nfull parameter. Might be changed in the future.
+// When running locally, or on the GRID (not in hyperloop), the default parameters provided in the .ini file of the
+// external generation can be overwritten using the confKeyValues option (or similar depending on the tool used).
+FairGenerator* generateEPOS4(const std::string &name, const int& nEvents)
+{
+    // check if the file exists
+    auto filename = gSystem->ExpandPathName(name.c_str());
+    if (!std::filesystem::exists(filename))
+    {
+        LOG(fatal) << "Options file " << filename << " does not exist!";
+        exit(1);
+    }
+    // cache all the lines of the optns file and replace the number of events
+    std::ifstream file(filename);
+    std::string line;
+    bool found = false;
+    std::stringstream buffer;
+    while (std::getline(file, line))
+    {
+        if (line.find("nfull") != std::string::npos){
+            // replace the number of events
+            found = true;
+            line = "set nfull " + std::to_string(nEvents);
+        }
+        buffer << line << "\n";
+    }
+    file.close();
+    // Write the updated content back to a file in the current directory
+    std::ofstream outFile("cfg.optns");
+    outFile << buffer.str();
+    if (!found)
+    {
+        outFile << "set nfull " + std::to_string(nEvents);
+    }
+    outFile.close();
+    auto gen = new GeneratorEPOS4();
+    auto& param0 = o2::eventgen::GeneratorFileOrCmdParam::Instance();
+    auto& param = o2::eventgen::GeneratorHepMCParam::Instance();
+    auto& conf = o2::conf::SimConfig::Instance();
+    // setup the HepMC generator to run with automatic FIFOs
+    gen->setup(param0, param, conf);
+    return gen;
+}
