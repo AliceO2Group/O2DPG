035f85656d4e5a591ddd2f9dbf045aaf51ae3714
Marco Giacalone <mgiacalo@gmail.com>
2024-11-21 07:49:33 +0100

    Fix failing (test) scripts (#1808)
    include mandatory interactionRate argument

--- MC/run/PWGDQ/runPromptCharmonia_midy_pp.sh
+++ MC/run/PWGDQ/runPromptCharmonia_midy_pp.sh
@@ -16,7 +16,7 @@ NTIMEFRAMES=${NTIMEFRAMES:-1}
 
 ${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py -eCM 900 -gen external -j ${NWORKERS} -ns ${NSIGEVENTS} -tf ${NTIMEFRAMES} -e TGeant4 -mod "--skipModules ZDC" \
 	-confKey "GeneratorExternal.fileName=${O2DPG_ROOT}/MC/config/PWGDQ/external/generator/GeneratorCocktailPromptCharmoniaToElectronEvtGen_pp13TeV.C;GeneratorExternal.funcName=GeneratorCocktailPromptCharmoniaToElectronEvtGen_pp13TeV()"  \
-    -genBkg pythia8 -procBkg inel -colBkg pp --embedding -nb ${NBKGEVENTS}
+    -genBkg pythia8 -procBkg inel -colBkg pp --embedding -nb ${NBKGEVENTS} -interactionRate 500000
 
 
 # run workflow
