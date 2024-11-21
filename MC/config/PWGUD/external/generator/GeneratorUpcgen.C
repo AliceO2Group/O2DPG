fd339dc9a5f94c3ae588bec741ea72058addf13e
Paul Buehler <paul.buehler@oeaw.ac.at>
2024-11-21 14:44:17 +0100

    clang-format

--- MC/config/PWGUD/external/generator/GeneratorUpcgen.C
+++ MC/config/PWGUD/external/generator/GeneratorUpcgen.C
@@ -5,19 +5,16 @@ R__ADD_INCLUDE_PATH($upcgen_ROOT/include)
 
 // usage: o2-sim -n 100 -g external --configKeyValues 'GeneratorExternal.fileName=GeneratorUpcgen.C;GeneratorExternal.funcName=GeneratorUpcgen("kDiTau")'
 
-namespace o2
-{
-namespace eventgen
-{
-class GeneratorUPCgen_class : public Generator
-{
- public:
-  GeneratorUPCgen_class() {
-    mUPCgen = new UpcGenerator();
-  };
+namespace o2 {
+namespace eventgen {
+class GeneratorUPCgen_class : public Generator {
+public:
+  GeneratorUPCgen_class() { mUPCgen = new UpcGenerator(); };
   ~GeneratorUPCgen_class() = default;
   void selectConfiguration(std::string val) { mSelectedConfiguration = val; };
-  void setLumiFileDirectory(std::string lumiFileDirectory) { mUPCgen->setLumiFileDirectory(lumiFileDirectory); };
+  void setLumiFileDirectory(std::string lumiFileDirectory) {
+    mUPCgen->setLumiFileDirectory(lumiFileDirectory);
+  };
   void setCollisionSystem(float energyCM, int beamZ, int beamA) {
     eCM = energyCM;
     projZ = beamZ;
@@ -28,33 +25,26 @@ class GeneratorUPCgen_class : public Generator
   // predefined generator configurations
   const static int nProcess = 5;
   const static int nElements = 9;
-  const struct UpcgenConfig
-  {
-    std::string pnames[nElements]{
-      "PROC_ID",
-      "LEP_A",
-      "ALP_MASS",
-      "ALP_WIDTH",
-      "PT_MIN",
-      "ETA_MIN",
-      "ETA_MAX",
-      "FLUX_POINT",
-      "BREAKUP_MODE"
-    };
+  const struct UpcgenConfig {
+    std::string pnames[nElements]{"PROC_ID",   "LEP_A",      "ALP_MASS",
+                                  "ALP_WIDTH", "PT_MIN",     "ETA_MIN",
+                                  "ETA_MAX",   "FLUX_POINT", "BREAKUP_MODE"};
     const struct definitions {
       const char *name;
       std::string elements[nElements];
     } sets[nProcess] = {
-      {"kDiElectron", {"11", "0.0", "0.0", "0.0", "0.0", "-1.0", "1.0", "1", "1"}},
-      {"kDiMuon", {"13", "0.0", "0.0", "0.0", "0.0", "-1.0", "1.0", "1", "1"}},
-      {"kDiTau", {"15", "0.0", "0.0", "0.0", "0.0", "-1.0", "1.0", "1", "1"}},
-      {"kLightByLight", {"22", "0.0", "0.0", "0.0", "0.0", "-1.0", "1.0", "1", "1"}},
-      {"kAxionLike", {"51", "0.0", "0.0", "0.0", "0.0", "-1.0", "1.0", "1", "1"}}
-    };
+        {"kDiElectron",
+         {"11", "0.0", "0.0", "0.0", "0.0", "-1.0", "1.0", "1", "1"}},
+        {"kDiMuon",
+         {"13", "0.0", "0.0", "0.0", "0.0", "-1.0", "1.0", "1", "1"}},
+        {"kDiTau", {"15", "0.0", "0.0", "0.0", "0.0", "-1.0", "1.0", "1", "1"}},
+        {"kLightByLight",
+         {"22", "0.0", "0.0", "0.0", "0.0", "-1.0", "1.0", "1", "1"}},
+        {"kAxionLike",
+         {"51", "0.0", "0.0", "0.0", "0.0", "-1.0", "1.0", "1", "1"}}};
   } upcgenConfig;
 
-  bool Config()
-  {
+  bool Config() {
     // select a specific set of parameters
     int idx = -1;
     for (int i = 0; i < nProcess; ++i) {
@@ -73,7 +63,7 @@ class GeneratorUPCgen_class : public Generator
     // new generator
     mUPCgen->setDebugLevel(0);
     mUPCgen->setNumThreads(1);
-    
+
     // update generator parameters - configure
     mUPCgen->setParameterValue("DO_PT_CUT", "1");
     mUPCgen->setParameterValue("DO_ETA_CUT", "1");
@@ -81,29 +71,26 @@ class GeneratorUPCgen_class : public Generator
     mUPCgen->setParameterValue("USE_HEPMC_OUTPUT", "0");
 
     for (int i = 0; i < nElements; ++i) {
-      mUPCgen->setParameterValue(upcgenConfig.pnames[i], upcgenConfig.sets[idx].elements[i]);
+      mUPCgen->setParameterValue(upcgenConfig.pnames[i],
+                                 upcgenConfig.sets[idx].elements[i]);
     }
-    
+
     return true;
-  }    
-  
-  bool Init() override
-  {
+  }
+
+  bool Init() override {
     Generator::Init();
 
     // initialize the generator
     mUPCgen->init();
-    
+
     return true;
   };
- 
 
-  bool generateEvent() override
-  {
+  bool generateEvent() override {
     if (!mUPCgen) {
-      std::cout
-          << "GenerateEvent: upcgen class/object not properly constructed"
-          << std::endl;
+      std::cout << "GenerateEvent: upcgen class/object not properly constructed"
+                << std::endl;
       return false;
     }
 
@@ -123,44 +110,36 @@ class GeneratorUPCgen_class : public Generator
         goon = false;
       }
     }
-    
+
     return true;
   };
 
-  bool importParticles() override
-  {
+  bool importParticles() override {
     std::cout << "\n";
     auto upcgenParticles = mUPCgen->getParticles();
     for (auto part : upcgenParticles) {
       part.Print();
-      TParticle particle(part.GetPdgCode(),
-	  			               1,
-	  			               part.GetFirstMother(),
-	  			               -1,
-	  			               part.GetFirstDaughter(),
-	  			               part.GetLastDaughter(),
-	  			               part.Px(),
-	  			               part.Py(),
-	  			               part.Pz(),
-	  			               part.Energy(),
-	  			               0., 0., 0., 0.);
+      TParticle particle(part.GetPdgCode(), 1, part.GetFirstMother(), -1,
+                         part.GetFirstDaughter(), part.GetLastDaughter(),
+                         part.Px(), part.Py(), part.Pz(), part.Energy(), 0., 0.,
+                         0., 0.);
       mParticles.push_back(particle);
-      o2::mcutils::MCGenHelper::encodeParticleStatusAndTracking(mParticles.back(), true);
+      o2::mcutils::MCGenHelper::encodeParticleStatusAndTracking(
+          mParticles.back(), true);
     }
     return true;
   };
-  
+
   long int acceptedEvents() { return nAccepted; }
   long int rejectedEvents() { return nRejected; }
-  double fiducialXSection()
-  {
-    return mUPCgen->totNuclX()*nAccepted/(nAccepted+nRejected);
+  double fiducialXSection() {
+    return mUPCgen->totNuclX() * nAccepted / (nAccepted + nRejected);
   }
 
- private:
+private:
   UpcGenerator *mUPCgen = 0x0;
   std::string mSelectedConfiguration = "";
-  
+
   // keep track of the rejected and accepted events
   long int nAccepted{0};
   long int nRejected{0};
@@ -173,15 +152,13 @@ class GeneratorUPCgen_class : public Generator
 } // namespace eventgen
 } // namespace o2
 
-
 FairGenerator *GeneratorUpcgen(std::string configuration = "kDiTau",
                                std::string lumiFileDirectory = ".",
-                               float energyCM = 5020.,
-                               int beamZ = 82,
+                               float energyCM = 5020., int beamZ = 82,
                                int beamA = 208) {
   // create generator
   auto gen = new o2::eventgen::GeneratorUPCgen_class();
-  
+
   // set generator parameters
   gen->selectConfiguration(configuration);
   gen->setLumiFileDirectory(lumiFileDirectory);
@@ -193,6 +170,6 @@ FairGenerator *GeneratorUpcgen(std::string configuration = "kDiTau",
   cout << " config done ...";
   gen->Init();
   cout << " init done!\n";
-  
+
   return gen;
 }
