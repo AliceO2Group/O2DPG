73425a398a9b689141ee838abbe735548ffbb6da
Chiara Zampolli <chiara.zampolli@cern.ch>
2024-12-05 15:19:28 +0100

    Possibility to set the input specs for GLO QC dynamically, for K0s (#1834)
    * Possibility to set the inputs from the qc-wf
    
    * Forcing K0QC to be off if there is no SECVTX

--- DATA/production/configurations/asyncReco/setenv_extra.sh
+++ DATA/production/configurations/asyncReco/setenv_extra.sh
@@ -712,7 +712,10 @@ fi
 if [[ $ALIEN_JDL_QCOFF != "1" ]]; then
   export WORKFLOW_PARAMETERS="QC,${WORKFLOW_PARAMETERS}"
 fi
-export QC_CONFIG_PARAM="--local-batch=QC.root --override-values \"qc.config.Activity.number=$RUNNUMBER;qc.config.Activity.passName=$PASS;qc.config.Activity.periodName=$PERIOD\""
+
+export QC_CONFIG_OVERRIDE+=";qc.config.Activity.number=$RUNNUMBER;qc.config.Activity.passName=$PASS;qc.config.Activity.periodName=$PERIOD;"
+
+export QC_CONFIG_PARAM+=" --local-batch=QC.root "
 export GEN_TOPO_WORKDIR="./"
 #export QC_JSON_FROM_OUTSIDE="QC-20211214.json"
 
