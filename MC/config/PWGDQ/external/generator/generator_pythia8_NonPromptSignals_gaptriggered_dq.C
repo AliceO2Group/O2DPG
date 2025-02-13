b5303c15293ada7ee7594044f7bd72bc262a1eea
Ionut Cristian Arsene <Ionut.Cristian.Arsene@cern.ch>
2025-02-13 10:54:44 +0100

    [PWG-DQ] Renaming the generator config for pythia inclusive jpsi, adding several B hadrons together with the Bplus generator (#1904)
    * Adding a prompt Jpsi generator using pythia8 CharmoniumShower:all option
    
    * switch off the jpsi decays
    
    * adding the trigger gap as a parameter
    
    * Adding tags for min bias and events containing signal
    
    * Renaming inclusive jpsi generator and updating the bbbar -> B plus generator
    
    ---------
    
    Co-authored-by: Ionut Cristian Arsene <iarsene@cern.ch>

--- MC/config/PWGDQ/external/generator/generator_pythia8_NonPromptSignals_gaptriggered_dq.C
+++ MC/config/PWGDQ/external/generator/generator_pythia8_NonPromptSignals_gaptriggered_dq.C
@@ -233,18 +233,18 @@ FairGenerator*
 }
 
 FairGenerator*
-  GeneratorBplusToJpsiKaon_EvtGen(double rapidityMin = -1.5, double rapidityMax = 1.5, bool verbose = false, TString pdgs = "521")
+  GeneratorBplusToJpsiKaon_EvtGen(double rapidityMin = -1.5, double rapidityMax = 1.5, bool verbose = false, TString pdgs = "511;521;531;541;5112;5122;5232;5132;5332")
 {
   auto gen = new o2::eventgen::GeneratorEvtGen<o2::eventgen::GeneratorPythia8NonPromptInjectedGapTriggeredDQ>();
   gen->setRapidity(rapidityMin, rapidityMax);
   gen->setPDG(5);
-  gen->addHadronPDGs(521);
+  //gen->addHadronPDGs(521);
   gen->setRapidityHadron(rapidityMin,rapidityMax);
   gen->setHadronMultiplicity(2);
-  TString pathO2table = gSystem->ExpandPathName("${O2DPG_MC_CONFIG_ROOT}/MC/config/PWGDQ/pythia8/decayer/switchOffBplus.cfg");
+  TString pathO2table = gSystem->ExpandPathName("${O2DPG_MC_CONFIG_ROOT}/MC/config/PWGDQ/pythia8/decayer/switchOffBhadrons.cfg");
   gen->readFile(pathO2table.Data());
   gen->setConfigMBdecays(pathO2table); 
-  gen->setVerbose(verbose);
+  gen->setVerbose(true);
   
   std::string spdg;
   TObjArray* obj = pdgs.Tokenize(";");
@@ -258,7 +258,8 @@ FairGenerator*
   
   TString pathO2 = gSystem->ExpandPathName("${O2DPG_MC_CONFIG_ROOT}/MC/config/PWGDQ/EvtGen/DecayTablesEvtgen");
   //gen->SetDecayTable(Form("%s/BPLUSTOKAONJPSITOELE.DEC", pathO2.Data()));
-  gen->SetDecayTable(Form("%s/BPLUSTOKAONJPSITOELEALLMODES.DEC", pathO2.Data())); // decay table including decay modes for correlated background
+  //gen->SetDecayTable(Form("%s/BPLUSTOKAONJPSITOELEALLMODES.DEC", pathO2.Data())); // decay table including decay modes for correlated background
+  gen->SetDecayTable(Form("%s/BTOJPSITOELE.DEC", pathO2.Data())); // decay table including decay modes for correlated background
   // print debug
   // gen->PrintDebug();
   // set random seed
