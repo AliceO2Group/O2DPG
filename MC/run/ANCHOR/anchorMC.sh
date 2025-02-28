981a89dd197a7853efd029120055f4aae5203cc9
Mario Ciacco <mario.ciacco@cern.ch>
2025-02-28 10:19:18 +0100

    set parallel world in test script + toggle ideal ITS in anchored simulation (#1908)
    * set pw in test script + toggle ideal ITS in anchored simulation
    
    * remove duplicated flag for pw activation
    
    * fix string check
    
    * fix variable declaration
    
    * fix check

--- MC/run/ANCHOR/anchorMC.sh
+++ MC/run/ANCHOR/anchorMC.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bashMore actions
 
 # add distortion maps
 # https://alice.its.cern.ch/jira/browse/O2-3346?focusedCommentId=300982&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-300982
@@ -244,6 +244,11 @@ TIMESTAMP=`grep "Determined timestamp to be" timestampsampling_${ALIEN_JDL_LPMRU
 echo_info "TIMESTAMP IS ${TIMESTAMP}"
 
 # -- Create aligned geometry using ITS ideal alignment to avoid overlaps in geant
+ENABLEPW=0
+if [[ ${remainingargs} == *"GeometryManagerParam.useParallelWorld=1"* ]]; then
+  ENABLEPW=1
+fi
+
 if [ "${ENABLEPW}" == "0" ]; then
   CCDBOBJECTS_IDEAL_MC="ITS/Calib/Align"
   TIMESTAMP_IDEAL_MC=1
@@ -255,7 +260,6 @@ if [ "${ENABLEPW}" == "0" ]; then
   fi
 fi
 
-# TODO This can potentially be removed or if needed, should be taken over by o2dpg_sim_workflow_anchored.py and O2_dpg_workflow_runner.py
 if [ "${ENABLEPW}" == "0" ]; then
   echo "run with echo in pipe" | ${O2_ROOT}/bin/o2-create-aligned-geometry-workflow ${ALIEN_JDL_CCDB_CONDITION_NOT_AFTER:+--condition-not-after ${ALIEN_JDL_CCDB_CONDITION_NOT_AFTER}} --configKeyValues "HBFUtils.startTime=${TIMESTAMP}" --condition-remap=file://${ALICEO2_CCDB_LOCALCACHE}=ITS/Calib/Align -b --run
 else
@@ -263,7 +267,9 @@ else
 fi
 mkdir -p $ALICEO2_CCDB_LOCALCACHE/GLO/Config/GeometryAligned
 ln -s -f $PWD/o2sim_geometry-aligned.root $ALICEO2_CCDB_LOCALCACHE/GLO/Config/GeometryAligned/snapshot.root
-[[ -f $PWD/its_GeometryTGeo.root ]] && mkdir -p $ALICEO2_CCDB_LOCALCACHE/ITS/Config/Geometry && ln -s -f $PWD/its_GeometryTGeo.root $ALICEO2_CCDB_LOCALCACHE/ITS/Config/Geometry/snapshot.root
+if [ "${ENABLEPW}" == "0" ]; then
+  [[ -f $PWD/its_GeometryTGeo.root ]] && mkdir -p $ALICEO2_CCDB_LOCALCACHE/ITS/Config/Geometry && ln -s -f $PWD/its_GeometryTGeo.root $ALICEO2_CCDB_LOCALCACHE/ITS/Config/Geometry/snapshot.root
+fi
 [[ -f $PWD/mft_GeometryTGeo.root ]] && mkdir -p $ALICEO2_CCDB_LOCALCACHE/MFT/Config/Geometry && ln -s -f $PWD/mft_GeometryTGeo.root $ALICEO2_CCDB_LOCALCACHE/MFT/Config/Geometry/snapshot.root
 
 # -- RUN THE MC WORKLOAD TO PRODUCE AOD --
