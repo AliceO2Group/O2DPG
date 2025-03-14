4ac71704480a619611bbf564338f00b81cf2562a
jesgum <jesper.gumprecht@gmail.com>
2025-03-14 15:11:02 +0100

    fix CI error

--- MC/config/ALICE3/ini/tests/xi_PbPb.C
+++ MC/config/ALICE3/ini/tests/xi_PbPb.C
@@ -1,6 +1,6 @@
 int External()
 {
-  std::string path = "o2sim_Kine.root";
+  std::string path{"o2sim_Kine.root"};
   int pdgToCheck = 3312;
 
   TFile file(path.c_str(), "read");
@@ -11,8 +11,14 @@ int External()
 
   int nInjectedParticles = 0;
   TTree* tree = (TTree*)file.Get("o2sim");
-  std::vector<o2::MCTrack>* tracks{};
-  tree->SetBranchAdress("MCTrack", &tracks)
+  
+  if (!tree) {
+    std::cerr << "Cannot find tree o2sim in file " << path << "\n";
+    return 1;
+  }
+
+  std::vector<o2::MCTrack> *tracks{};
+  tree->SetBranchAddress("MCTrack", &tracks);
 
   int nEvents = tree->GetEntries();
   for (int i = 0; i < nEvents; i++) {
@@ -27,10 +33,14 @@ int External()
   }
 
   if (nInjectedParticles < nEvents) {
-    // Check that we are correctly injecting one
+    // Check that we are correctly injecting 15
     // particle per event
     return 1;
   }
 
   return 0;
 }
+
+
+
+
