856d118dd73c6f002293b6b953357184a4a713a3
jesgum <jesper.gumprecht@gmail.com>
2025-03-14 14:20:16 +0100

    Add simple test for the external generator

--- MC/config/ALICE3/ini/tests/xi_PbPb.C
+++ MC/config/ALICE3/ini/tests/xi_PbPb.C
@@ -1,3 +1,36 @@
-int External() {
-    return 0;
+int External()
+{
+  std::string path = "o2sim_Kine.root";
+  int pdgToCheck = 3312;
+
+  TFile file(path.c_str(), "read");
+  if (file.IsZombie()) {
+    std::err << "Cannot open ROOT file " << path << std::endl;
+    return 1;
+  }
+
+  int nInjectedParticles = 0;
+  TTree* tree = (TTree*)file.Get("o2sim");
+  std::vector<o2::MCTrack>* tracks;
+  tree->SetBranchAdress("MCTrack", &tracks)
+
+  int nEvents = tree->GetEntries();
+  for (int i = 0; i < nEvents; i++) {
+    tree->GetEntry(i);
+    for (const auto& track : tracks) {
+      auto pdgCode = track.getPdgCode();
+      if (pdgCode == pdgToCheck) {
+        // not injecting anti-particle
+        nInjectedParticles++;
+      }
+    }
+  }
+
+  if (nInjectedParticles < nEvents) {
+    // Check that we are correctly injecting one
+    // particle per event
+    return 1;
+  }
+
+  return 0;
 }
