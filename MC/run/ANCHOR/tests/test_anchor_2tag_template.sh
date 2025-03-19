e0cf052648af450f40924a36a5ac81d9aa521056
swenzel <sandro.wenzel@cern.ch>
2025-03-19 16:43:28 +0100

    Setup to test anchorMC in the 2tag approach
    Provides
    * Template file to generate anchored MC jobs
    * A driver script creating actual test jobs and executing them on the GRID
    * CSV data file specifying which software tags should be tested
    
    The idea is that this should be used to check if
    MC (daily) releases are compatible with the 2tag approach relative
    to a given set of async software releases.

---
+++ MC/run/ANCHOR/tests/test_anchor_2tag_template.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#JDL_OUTPUT=*.txt@disk=1,AO2D.root@disk=2,*.log@disk=1,*stat*@disk=1,*.json@disk=1,debug*tgz@disk=2
+#JDL_ERROROUTPUT=*.txt@disk=1,AO2D.root@disk=2,*.log@disk=1,*.json@disk=1,debug*tgz@disk=2
+#JDL_PACKAGE=%{SOFTWARETAG_SIM}
+#JDL_REQUIRE=%{JDL_REQUIREMENT}
+
+#
+# A template anchoring script to test various anchoring setups
+# and software combinations
+#
+
+# only relevant if executed locally
+if [ ! ${O2_ROOT} ]; then
+  source <(/cvmfs/alice.cern.ch/bin/alienv printenv %{SOFTWARETAG_SIM})
+fi
+
+# meta configuration of the job (influences reco config)
+export ALIEN_JDL_LPMPRODUCTIONTYPE=MC
+export ALIEN_JDL_CPULIMIT=8
+
+export ALIEN_JDL_LPMANCHORPASSNAME=%{PASSNAME}
+export ALIEN_JDL_MCANCHOR=%{PASSNAME}
+export ALIEN_JDL_COLLISIONSYSTEM=%{COL_SYSTEM}
+export ALIEN_JDL_LPMPASSNAME=%{PASSNAME}
+export ALIEN_JDL_LPMRUNNUMBER=%{RUN_NUMBER}
+export ALIEN_JDL_LPMANCHORRUN=%{RUN_NUMBER}
+
+export ALIEN_JDL_LPMINTERACTIONTYPE=%{INTERACTIONTYPE}
+export ALIEN_JDL_LPMPRODUCTIONTAG=%{PRODUCTION_TAG}
+export ALIEN_JDL_LPMANCHORPRODUCTION=%{ANCHOR_PRODUCTION}
+export ALIEN_JDL_LPMANCHORYEAR=%{ANCHORYEAR}
+export ALIEN_JDL_O2DPG_ASYNC_RECO_TAG="%{SOFTWARETAG_ASYNC}"
+
+# get custom O2DPG for 2tag treatment (could be used to test different O2DPG branches)
+# git clone https://github.com/AliceO2Group/O2DPG O2DPG
+# export O2DPG_ROOT=${PWD}/O2DPG
+# export ALIEN_JDL_O2DPG_OVERWRITE=${PWD}/O2DPG
+
+# dimension the job
+export NTIMEFRAMES=1
+
+# further configuration of the job
+export ALIEN_JDL_ADDTIMESERIESINMC=0
+export DISABLE_QC=1
+export ALIEN_JDL_MC_ORBITS_PER_TF=10000:10000000:2 # puts just 2 orbit for large enough interaction rates
+export ALIEN_JDL_O2DPGWORKFLOWTARGET="aod"
+
+# select anchoring points
+export PRODSPLIT=${ALIEN_O2DPG_GRIDSUBMIT_PRODSPLIT:-100}
+export SPLITID=${ALIEN_O2DPG_GRIDSUBMIT_SUBJOBID:-50}
+export CYCLE=0
+
+# generator and other sim configuration
+export ALIEN_JDL_ANCHOR_SIM_OPTIONS="%{SIM_OPTIONS}"
+
+# execute MC
+# ${O2DPG_ROOT}/MC/run/ANCHOR/anchorMC.sh
