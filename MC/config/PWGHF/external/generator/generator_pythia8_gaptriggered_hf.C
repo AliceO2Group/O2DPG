05fde30127dc6955a652d795a238e0e40082b9de
Fabrizio <fabrizio.grosa@cern.ch>
2025-04-30 17:45:17 +0200

    [PWGHF] Add possibility to replace particles in the stack for HF generator (#1985)
    * Add possibility to replace particles in the stack for HF generator
    
    * Minor change
    
    * Add Xic0 excited states
    
    * Fix test
    
    * Add protection
    
    * Fix file name
    
    * Minor fix
    
    * Minor fix
    
    * Make test more robust against small amount of test events

--- MC/config/PWGHF/external/generator/generator_pythia8_gaptriggered_hf.C
+++ MC/config/PWGHF/external/generator/generator_pythia8_gaptriggered_hf.C
@@ -2,6 +2,7 @@
 #include "Generators/GeneratorPythia8.h"
 #include "Pythia8/Pythia.h"
 #include "TRandom.h"
+#include "TDatabasePDG.h"
 #include <fairlogger/Logger.h>
 
 #include <string>
@@ -16,21 +17,39 @@ public:
   GeneratorPythia8GapTriggeredHF() = default;
 
   /// constructor
-  GeneratorPythia8GapTriggeredHF(int inputTriggerRatio = 5, std::vector<int> quarkPdgList = {}, std::vector<int> hadronPdgList = {})
+  GeneratorPythia8GapTriggeredHF(int inputTriggerRatio = 5, std::vector<int> quarkPdgList = {}, std::vector<int> hadronPdgList = {}, std::vector<std::array<int, 2>> partPdgToReplaceList = {}, std::vector<float> freqReplaceList = {})
   {
 
     mGeneratedEvents = 0;
     mInverseTriggerRatio = inputTriggerRatio;
     mQuarkRapidityMin = -1.5;
-    mQuarkRapidityMax = -1.5;
+    mQuarkRapidityMax = 1.5;
     mHadRapidityMin = -1.5;
-    mHadRapidityMax = -1.5;
+    mHadRapidityMax = 1.5;
     mQuarkPdg = 0;
     mHadronPdg = 0;
     mQuarkPdgList = quarkPdgList;
     mHadronPdgList = hadronPdgList;
+    mPartPdgToReplaceList = partPdgToReplaceList;
+    mFreqReplaceList = freqReplaceList;
+    // Ds1*(2700), Ds1*(2860), Ds3*(2860), Xic(3055)+, Xic(3080)+, Xic(3055)0, Xic(3080)0
+    mCustomPartPdgs = {30433, 40433, 437, 4315, 4316, 4325, 4326};
+    mCustomPartMasses[30433] = 2.714f;
+    mCustomPartMasses[40433] =  2.859f;
+    mCustomPartMasses[437] = 2.860f;
+    mCustomPartMasses[4315] = 3.0590f;
+    mCustomPartMasses[4316] = 3.0799f;
+    mCustomPartMasses[4325] = 3.0559f;
+    mCustomPartMasses[4326] = 3.0772f;
+    mCustomPartWidths[30433] = 0.122f;
+    mCustomPartWidths[40433] =  0.160f;
+    mCustomPartWidths[437] = 0.053f;
+    mCustomPartWidths[4315] = 0.0064f;
+    mCustomPartWidths[4316] = 0.0056f;
+    mCustomPartWidths[4325] = 0.0078f;
+    mCustomPartWidths[4326] = 0.0036f;
     Print();
-  }
+  }    
 
   ///  Destructor
   ~GeneratorPythia8GapTriggeredHF() = default;
@@ -54,6 +73,11 @@ public:
     {
       LOG(info)<<Form("* %d ", pdg);
     }
+    LOG(info)<<Form("* Replacements: ");
+    for (auto iRepl{0u}; iRepl<mPartPdgToReplaceList.size(); ++iRepl)
+    {
+      LOGP(info, "* {} -> {} (freq. {})", mPartPdgToReplaceList[iRepl].at(0), mPartPdgToReplaceList[iRepl].at(1), mFreqReplaceList[iRepl]);
+    }
     LOG(info)<<"***********************************************************************";
   }
 
@@ -62,6 +86,21 @@ public:
     addSubGenerator(0, "Minimum bias");
     addSubGenerator(4, "Charm injected");
     addSubGenerator(5, "Beauty injected");
+
+    std::vector<int> pdgToReplace{};
+    for (auto iRepl{0u}; iRepl<mPartPdgToReplaceList.size(); ++iRepl)
+    {
+      for (auto iPdgRep{0u}; iPdgRep<pdgToReplace.size(); ++iPdgRep) {
+        if (mPartPdgToReplaceList[iRepl].at(0) == pdgToReplace[iPdgRep]) {
+          mFreqReplaceList[iRepl] += mFreqReplaceList[iPdgRep];
+        }
+      }
+      if (mFreqReplaceList[iRepl] > 1.f) {
+        LOGP(fatal, "Replacing more than 100% of a particle!");
+      }
+      pdgToReplace.push_back(mPartPdgToReplaceList[iRepl].at(0));
+    }
+
     return o2::eventgen::GeneratorPythia8::Init();
   }
 
@@ -115,7 +154,7 @@ protected:
       {
         if (GeneratorPythia8::generateEvent())
         {
-          genOk = selectEvent(mPythia.event);
+          genOk = selectEvent();
         }
       }
       notifySubGenerator(mQuarkPdg);
@@ -136,34 +175,35 @@ protected:
     return true;
   }
 
-  bool selectEvent(const Pythia8::Event &event)
+  bool selectEvent()
   {
 
     bool isGoodAtPartonLevel{mQuarkPdgList.size() == 0};
     bool isGoodAtHadronLevel{mHadronPdgList.size() == 0};
+    bool anyPartToReplace{mPartPdgToReplaceList.size() > 0};
 
-    for (auto iPart{0}; iPart < event.size(); ++iPart)
+    for (auto iPart{0}; iPart < mPythia.event.size(); ++iPart)
     {
 
       // search for Q-Qbar mother with at least one Q in rapidity window
       if (!isGoodAtPartonLevel)
       {
-        auto daughterList = event[iPart].daughterList();
+        auto daughterList = mPythia.event[iPart].daughterList();
         bool hasQ = false, hasQbar = false, atSelectedY = false;
         for (auto iDau : daughterList)
         {
-          if (event[iDau].id() == mQuarkPdg)
+          if (mPythia.event[iDau].id() == mQuarkPdg)
           {
             hasQ = true;
-            if ((event[iDau].y() > mQuarkRapidityMin) && (event[iDau].y() < mQuarkRapidityMax))
+            if ((mPythia.event[iDau].y() > mQuarkRapidityMin) && (mPythia.event[iDau].y() < mQuarkRapidityMax))
             {
               atSelectedY = true;
             }
           }
-          if (event[iDau].id() == -mQuarkPdg)
+          if (mPythia.event[iDau].id() == -mQuarkPdg)
           {
             hasQbar = true;
-            if ((event[iDau].y() > mQuarkRapidityMin) && (event[iDau].y() < mQuarkRapidityMax))
+            if ((mPythia.event[iDau].y() > mQuarkRapidityMin) && (mPythia.event[iDau].y() < mQuarkRapidityMax))
             {
               atSelectedY = true;
             }
@@ -178,26 +218,91 @@ protected:
       // search for hadron in rapidity window
       if (!isGoodAtHadronLevel)
       {
-        int id = std::abs(event[iPart].id());
-        float rap = event[iPart].y();
+        int id = std::abs(mPythia.event[iPart].id());
+        float rap = mPythia.event[iPart].y();
         if (id == mHadronPdg && rap > mHadRapidityMin && rap < mHadRapidityMax)
         {
           isGoodAtHadronLevel = true;
         }
       }
 
-      // we send the trigger
-      if (isGoodAtPartonLevel && isGoodAtHadronLevel)
+      // if requested, we replace the particle
+      const double pseudoRndm = mPythia.event[iPart].pT() * 1000. - (int64_t)(mPythia.event[iPart].pT() * 1000);
+      for (auto iPartToReplace{0u}; iPartToReplace<mPartPdgToReplaceList.size(); ++iPartToReplace) {
+        if (std::abs(mPythia.event[iPart].id()) == mPartPdgToReplaceList[iPartToReplace][0] && pseudoRndm < mFreqReplaceList[iPartToReplace]) {
+          LOGP(debug, "REPLACING PARTICLE {} WITH {}, BEING RNDM {}", mPartPdgToReplaceList[iPartToReplace][0], mPartPdgToReplaceList[iPartToReplace][1], pseudoRndm);
+          replaceParticle(iPart, mPartPdgToReplaceList[iPartToReplace][1]);
+          break;
+        }
+      }
+
+      // we send the trigger immediately (if there are no particles to replace, that can be different from the trigger ones)
+      if (isGoodAtPartonLevel && isGoodAtHadronLevel && !anyPartToReplace)
       {
-        LOG(debug)<<"EVENT SELECTED: Found particle "<<event[iPart].id()<<" at rapidity "<<event[iPart].y()<<"\n";
+        LOG(debug)<<"EVENT SELECTED: Found particle "<<mPythia.event[iPart].id()<<" at rapidity "<<mPythia.event[iPart].y()<<"\n";
         return true;
       }
+    }
+
+    // we send the trigger
+    if (isGoodAtPartonLevel && isGoodAtHadronLevel) {
+      return true;
     }
 
     return false;
   };
 
-private:
+  bool replaceParticle(int iPartToReplace, int pdgCodeNew) {
+
+    std::array<int, 2> mothers = {mPythia.event[iPartToReplace].mother1(), mPythia.event[iPartToReplace].mother2()};
+
+    std::array<int, 25> pdgDiquarks = {1103, 2101, 2103, 2203, 3101, 3103, 3201, 3203, 3303, 4101, 4103, 4201, 4203, 4301, 4303, 4403, 5101, 5103, 5201, 5203, 5301, 5303, 5401, 5403, 5503};
+
+    for (auto const& mother: mothers) {
+      auto pdgMother = std::abs(mPythia.event[mother].id());
+      if (pdgMother > 100 && std::find(pdgDiquarks.begin(), pdgDiquarks.end(), pdgMother) == pdgDiquarks.end()) {
+        return false;
+      }
+    }
+
+    int charge = mPythia.event[iPartToReplace].id() / std::abs(mPythia.event[iPartToReplace].id());
+    float px = mPythia.event[iPartToReplace].px();
+    float py = mPythia.event[iPartToReplace].py();
+    float pz = mPythia.event[iPartToReplace].pz();
+    float mass = 0.f;
+
+    if (std::find(mCustomPartPdgs.begin(), mCustomPartPdgs.end(), pdgCodeNew) == mCustomPartPdgs.end()) { // not a custom particle
+      float width = TDatabasePDG::Instance()->GetParticle(pdgCodeNew)->Width();
+      float massRest = TDatabasePDG::Instance()->GetParticle(pdgCodeNew)->Mass();
+      if (width > 0.f) {
+        mass = gRandom->BreitWigner(massRest, width);
+      } else {
+        mass = massRest;
+      }
+    } else {
+      if (mCustomPartWidths[pdgCodeNew] > 0.f) {
+        mass = gRandom->BreitWigner(mCustomPartMasses[pdgCodeNew], mCustomPartWidths[pdgCodeNew]);
+      } else {
+        mass = mCustomPartMasses[pdgCodeNew];
+      }
+    }
+    float energy = std::sqrt(px*px + py*py + pz*pz + mass*mass);
+
+    // we remove particle to replace and its daughters
+    mPythia.event[iPartToReplace].undoDecay();
+    mPythia.event.remove(iPartToReplace, iPartToReplace, true); // we remove the original particle
+
+    int status = std::abs(mPythia.event[iPartToReplace].status());
+    if (status < 81 || status > 89) {
+      status = 81;
+    }
+    mPythia.event.append(charge * pdgCodeNew, status, mothers[0], mothers[1], 0, 0, 0, 0, px, py, pz, energy, mass);
+    mPythia.moreDecays(); // we need to decay the new particle
+
+    return true;
+  }
+
+  private:
   // Interface to override import particles
   Pythia8::Event mOutputEvent;
 
@@ -209,6 +314,11 @@ private:
   float mHadRapidityMin;
   float mHadRapidityMax;
   unsigned int mUsedSeed;
+  std::vector<std::array<int, 2>> mPartPdgToReplaceList;
+  std::vector<float> mFreqReplaceList;
+  std::vector<int> mCustomPartPdgs;
+  std::map<int, float> mCustomPartMasses;
+  std::map<int, float> mCustomPartWidths;
 
   // Control gap-triggering
   unsigned long long mGeneratedEvents;
@@ -223,9 +333,9 @@ private:
 
 // Predefined generators:
 // Charm-enriched
-FairGenerator *GeneratorPythia8GapTriggeredCharm(int inputTriggerRatio, float yQuarkMin = -1.5, float yQuarkMax = 1.5, float yHadronMin = -1.5, float yHadronMax = 1.5, std::vector<int> hadronPdgList = {})
+FairGenerator *GeneratorPythia8GapTriggeredCharm(int inputTriggerRatio, float yQuarkMin = -1.5, float yQuarkMax = 1.5, float yHadronMin = -1.5, float yHadronMax = 1.5, std::vector<int> hadronPdgList = {}, std::vector<std::array<int, 2>> partPdgToReplaceList = {}, std::vector<float> freqReplaceList = {})
 {
-  auto myGen = new GeneratorPythia8GapTriggeredHF(inputTriggerRatio, std::vector<int>{4}, hadronPdgList);
+  auto myGen = new GeneratorPythia8GapTriggeredHF(inputTriggerRatio, std::vector<int>{4}, hadronPdgList, partPdgToReplaceList, freqReplaceList);
   auto seed = (gRandom->TRandom::GetSeed() % 900000000);
   myGen->setUsedSeed(seed);
   myGen->readString("Random:setSeed on");
@@ -239,9 +349,9 @@ FairGenerator *GeneratorPythia8GapTriggeredCharm(int inputTriggerRatio, float yQ
 }
 
 // Beauty-enriched
-FairGenerator *GeneratorPythia8GapTriggeredBeauty(int inputTriggerRatio, float yQuarkMin = -1.5, float yQuarkMax = 1.5, float yHadronMin = -1.5, float yHadronMax = 1.5, std::vector<int> hadronPdgList = {})
+FairGenerator *GeneratorPythia8GapTriggeredBeauty(int inputTriggerRatio, float yQuarkMin = -1.5, float yQuarkMax = 1.5, float yHadronMin = -1.5, float yHadronMax = 1.5, std::vector<int> hadronPdgList = {}, std::vector<std::array<int, 2>> partPdgToReplaceList = {}, std::vector<float> freqReplaceList = {})
 {
-  auto myGen = new GeneratorPythia8GapTriggeredHF(inputTriggerRatio, std::vector<int>{5}, hadronPdgList);
+  auto myGen = new GeneratorPythia8GapTriggeredHF(inputTriggerRatio, std::vector<int>{5}, hadronPdgList, partPdgToReplaceList, freqReplaceList);
   auto seed = (gRandom->TRandom::GetSeed() % 900000000);
   myGen->setUsedSeed(seed);
   myGen->readString("Random:setSeed on");
@@ -255,9 +365,9 @@ FairGenerator *GeneratorPythia8GapTriggeredBeauty(int inputTriggerRatio, float y
 }
 
 // Charm and beauty enriched (with same ratio)
-FairGenerator *GeneratorPythia8GapTriggeredCharmAndBeauty(int inputTriggerRatio, float yQuarkMin = -1.5, float yQuarkMax = 1.5, float yHadronMin = -1.5, float yHadronMax = 1.5, std::vector<int> hadronPdgList = {})
+FairGenerator *GeneratorPythia8GapTriggeredCharmAndBeauty(int inputTriggerRatio, float yQuarkMin = -1.5, float yQuarkMax = 1.5, float yHadronMin = -1.5, float yHadronMax = 1.5, std::vector<int> hadronPdgList = {}, std::vector<std::array<int, 2>> partPdgToReplaceList = {}, std::vector<float> freqReplaceList = {})
 {
-  auto myGen = new GeneratorPythia8GapTriggeredHF(inputTriggerRatio, std::vector<int>{4, 5}, hadronPdgList);
+  auto myGen = new GeneratorPythia8GapTriggeredHF(inputTriggerRatio, std::vector<int>{4, 5}, hadronPdgList, partPdgToReplaceList, freqReplaceList);
   auto seed = (gRandom->TRandom::GetSeed() % 900000000);
   myGen->setUsedSeed(seed);
   myGen->readString("Random:setSeed on");
@@ -270,13 +380,13 @@ FairGenerator *GeneratorPythia8GapTriggeredCharmAndBeauty(int inputTriggerRatio,
   return myGen;
 }
 
-FairGenerator *GeneratorPythia8GapHF(int inputTriggerRatio, float yQuarkMin = -1.5, float yQuarkMax = 1.5, float yHadronMin = -1.5, float yHadronMax = 1.5, std::vector<int> quarkPdgList = {}, std::vector<int> hadronPdgList = {})
+FairGenerator *GeneratorPythia8GapHF(int inputTriggerRatio, float yQuarkMin = -1.5, float yQuarkMax = 1.5, float yHadronMin = -1.5, float yHadronMax = 1.5, std::vector<int> quarkPdgList = {}, std::vector<int> hadronPdgList = {}, std::vector<std::array<int, 2>> partPdgToReplaceList = {}, std::vector<float> freqReplaceList = {})
 {
   if (hadronPdgList.size() == 0 && quarkPdgList.size() == 0)
   {
     LOG(fatal) << "GeneratorPythia8GapHF: At least one quark or hadron PDG code must be specified";
   }
-  auto myGen = new GeneratorPythia8GapTriggeredHF(inputTriggerRatio, quarkPdgList, hadronPdgList);
+  auto myGen = new GeneratorPythia8GapTriggeredHF(inputTriggerRatio, quarkPdgList, hadronPdgList, partPdgToReplaceList, freqReplaceList);
   auto seed = (gRandom->TRandom::GetSeed() % 900000000);
   myGen->setUsedSeed(seed);
   myGen->readString("Random:setSeed on");
---
+++ MC/config/PWGHF/ini/tests/GeneratorHF_D2H_ccbar_and_bbbar_gap5_DResoTrigger.C
@@ -0,0 +1,163 @@
+int External() {
+    std::string path{"o2sim_Kine.root"};
+
+    int checkPdgQuarkOne{4};
+    int checkPdgQuarkTwo{5};
+    float ratioTrigger = 1./5.; // one event triggered out of 5
+    std::array<std::array<int, 2>, 6> pdgReplParticles = {std::array{10433, 30433}, std::array{10433, 437}, std::array{435, 4325}, std::array{435, 4326}, std::array{425, 4315}, std::array{425, 4316}};
+    std::array<std::array<int, 2>, 6> pdgReplPartCounters = {std::array{0, 0}, std::array{0, 0}, std::array{0, 0}, std::array{0, 0}, std::array{0, 0}, std::array{0, 0}};
+    std::array<float, 6> freqRepl = {0.1, 0.1, 0.1, 0.1, 0.5, 0.5};
+    std::map<int, int> sumOrigReplacedParticles = {{10433, 0}, {435, 0}, {425, 0}};
+
+    std::array<int, 11> checkPdgHadron{411, 421, 10433, 30433, 435, 437, 4325, 4326, 4315, 4316, 531};
+    std::map<int, std::vector<std::vector<int>>> checkHadronDecays{ // sorted pdg of daughters
+        {411, {{-321, 211, 211}, {-313, 211}, {211, 311}, {211, 333}}}, // D+
+        {421, {{-321, 211}, {-321, 211, 111}}}, // D0
+        {435, {{311, 413}, {311, 411}}}, // Ds2*(2573)
+        {10433, {{311, 413}}}, // Ds1(2536)
+        {30433, {{311, 413}}}, // Ds1*(2700)
+        {437, {{311, 413}}}, // Ds3*(2860)
+        {4325, {{411, 3122}}}, // Xic(3055)+
+        {4326, {{411, 3122}}}, // Xic(3080)+
+        {4315, {{421, 3122}}}, // Xic(3055)+
+        {4316, {{421, 3122}}}, // Xic(3080)+
+        {531, {{-435, -11, 12}, {-10433, -11, 12}, {-435, -13, 14}, {-10433, -13, 14}, {-435, -15, 16}, {-10433, -15, 16}, {-435, 211}}}// Bs0
+    };
+
+    TFile file(path.c_str(), "READ");
+    if (file.IsZombie()) {
+        std::cerr << "Cannot open ROOT file " << path << "\n";
+        return 1;
+    }
+
+    auto tree = (TTree *)file.Get("o2sim");
+    std::vector<o2::MCTrack> *tracks{};
+    tree->SetBranchAddress("MCTrack", &tracks);
+    o2::dataformats::MCEventHeader *eventHeader = nullptr;
+    tree->SetBranchAddress("MCEventHeader.", &eventHeader);
+
+    int nEventsMB{}, nEventsInjOne{}, nEventsInjTwo{};
+    int nQuarksOne{}, nQuarksTwo{}, nSignals{}, nSignalGoodDecay{};
+    auto nEvents = tree->GetEntries();
+
+    for (int i = 0; i < nEvents; i++) {
+        tree->GetEntry(i);
+
+        // check subgenerator information
+        int subGeneratorId{-1};
+        if (eventHeader->hasInfo(o2::mcgenid::GeneratorProperty::SUBGENERATORID)) {
+            bool isValid = false;
+            subGeneratorId = eventHeader->getInfo<int>(o2::mcgenid::GeneratorProperty::SUBGENERATORID, isValid);
+            if (subGeneratorId == 0) {
+                nEventsMB++;
+            } else if (subGeneratorId == checkPdgQuarkOne) {
+                nEventsInjOne++;
+            } else if (subGeneratorId == checkPdgQuarkTwo) {
+                nEventsInjTwo++;
+            }
+        }
+
+        for (auto &track : *tracks) {
+            auto pdg = track.GetPdgCode();
+            auto absPdg = std::abs(pdg);
+            if (absPdg == checkPdgQuarkOne) {
+                nQuarksOne++;
+                continue;
+            }
+            if (absPdg == checkPdgQuarkTwo) {
+                nQuarksTwo++;
+                continue;
+            }
+            if (std::find(checkPdgHadron.begin(), checkPdgHadron.end(), absPdg) != checkPdgHadron.end()) { // found signal
+                nSignals++; // count signal PDG
+
+                if (subGeneratorId == checkPdgQuarkOne) { // replacement only for prompt
+                    for (int iRepl{0}; iRepl<6; ++iRepl) {
+                        if (absPdg == pdgReplParticles[iRepl][0]) {
+                            pdgReplPartCounters[iRepl][0]++;
+                            sumOrigReplacedParticles[pdgReplParticles[iRepl][0]]++;
+                        } else if (absPdg == pdgReplParticles[iRepl][1]) {
+                            pdgReplPartCounters[iRepl][1]++;
+                            sumOrigReplacedParticles[pdgReplParticles[iRepl][0]]++;
+                        }
+                    }
+                }
+
+                std::vector<int> pdgsDecay{};
+                std::vector<int> pdgsDecayAntiPart{};
+                if (track.getFirstDaughterTrackId() >= 0 && track.getLastDaughterTrackId() >= 0) {
+                    for (int j{track.getFirstDaughterTrackId()}; j <= track.getLastDaughterTrackId(); ++j) {
+                        auto pdgDau = tracks->at(j).GetPdgCode();
+                        pdgsDecay.push_back(pdgDau);
+                        if (pdgDau != 333) { // phi is antiparticle of itself
+                            pdgsDecayAntiPart.push_back(-pdgDau);
+                        } else {
+                            pdgsDecayAntiPart.push_back(pdgDau);
+                        }
+                    }
+                }
+
+                std::sort(pdgsDecay.begin(), pdgsDecay.end());
+                std::sort(pdgsDecayAntiPart.begin(), pdgsDecayAntiPart.end());
+
+                for (auto &decay : checkHadronDecays[std::abs(pdg)]) {
+                    if (pdgsDecay == decay || pdgsDecayAntiPart == decay) {
+                        nSignalGoodDecay++;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    std::cout << "--------------------------------\n";
+    std::cout << "# Events: " << nEvents << "\n";
+    std::cout << "# MB events: " << nEventsMB << "\n";
+    std::cout << Form("# events injected with %d quark pair: ", checkPdgQuarkOne) << nEventsInjOne << "\n";
+    std::cout << Form("# events injected with %d quark pair: ", checkPdgQuarkTwo) << nEventsInjTwo << "\n";
+    std::cout << Form("# %d (anti)quarks: ", checkPdgQuarkOne) << nQuarksOne << "\n";
+    std::cout << Form("# %d (anti)quarks: ", checkPdgQuarkTwo) << nQuarksTwo << "\n";
+    std::cout <<"# signal hadrons: " << nSignals << "\n";
+    std::cout <<"# signal hadrons decaying in the correct channel: " << nSignalGoodDecay << "\n";
+
+    if (nEventsMB < nEvents * (1 - ratioTrigger) * 0.95 || nEventsMB > nEvents * (1 - ratioTrigger) * 1.05) { // we put some tolerance since the number of generated events is small
+        std::cerr << "Number of generated MB events different than expected\n";
+        return 1;
+    }
+    if (nEventsInjOne < nEvents * ratioTrigger * 0.5 * 0.95 || nEventsInjOne > nEvents * ratioTrigger * 0.5 * 1.05) {
+        std::cerr << "Number of generated events injected with " << checkPdgQuarkOne << " different than expected\n";
+        return 1;
+    }
+    if (nEventsInjTwo < nEvents * ratioTrigger * 0.5 * 0.95 || nEventsInjTwo > nEvents * ratioTrigger * 0.5 * 1.05) {
+        std::cerr << "Number of generated events injected with " << checkPdgQuarkTwo << " different than expected\n";
+        return 1;
+    }
+
+    if (nQuarksOne < nEvents * ratioTrigger) { // we expect anyway more because the same quark is repeated several time, after each gluon radiation
+        std::cerr << "Number of generated (anti)quarks " << checkPdgQuarkOne << " lower than expected\n";
+        return 1;
+    }
+    if (nQuarksTwo < nEvents * ratioTrigger) { // we expect anyway more because the same quark is repeated several time, after each gluon radiation
+        std::cerr << "Number of generated (anti)quarks " << checkPdgQuarkTwo << " lower than expected\n";
+        return 1;
+    }
+
+    float fracForcedDecays = float(nSignalGoodDecay) / nSignals;
+    if (fracForcedDecays < 0.9) { // we put some tolerance (e.g. due to oscillations which might change the final state)
+        std::cerr << "Fraction of signals decaying into the correct channel " << fracForcedDecays << " lower than expected\n";
+        return 1;
+    }
+
+    for (int iRepl{0}; iRepl<6; ++iRepl) {
+        if (std::abs(pdgReplPartCounters[iRepl][1] - freqRepl[iRepl] * sumOrigReplacedParticles[pdgReplParticles[iRepl][0]]) > 2 * std::sqrt(freqRepl[iRepl] * sumOrigReplacedParticles[pdgReplParticles[iRepl][0]])) { // 2 sigma compatibility
+            float fracMeas = 0.;
+            if (sumOrigReplacedParticles[pdgReplParticles[iRepl][0]] > 0.) {
+                fracMeas = float(pdgReplPartCounters[iRepl][1]) / sumOrigReplacedParticles[pdgReplParticles[iRepl][0]];
+            } 
+            std::cerr << "Fraction of replaced " << pdgReplParticles[iRepl][0] << " into " << pdgReplParticles[iRepl][1] << " is " << fracMeas <<" (expected "<< freqRepl[iRepl] << ")\n";
+            return 1;    
+        }
+    }
+
+    return 0;
+}
