8edc590ee680282516dd2e6588c736e88764e3f6
alcaliva <32872606+alcaliva@users.noreply.github.com>
2025-06-03 12:43:19 +0200

    [PWGLF] Added deuteron generator using advanced coalescence (#2019)

---
+++ MC/config/PWGLF/ini/tests/GeneratorLF_deuteron_wigner.C
@@ -0,0 +1,41 @@
+
+int External() {
+  std::string path{"o2sim_Kine.root"};
+
+  TFile file(path.c_str(), "READ");
+  if (file.IsZombie()) {
+    std::cerr << "Cannot open ROOT file " << path << "\n";
+    return 1;
+  }
+
+  auto tree = (TTree *)file.Get("o2sim");
+  if (!tree) {
+    std::cerr << "Cannot find tree o2sim in file " << path << "\n";
+    return 1;
+  }
+
+  std::vector<o2::MCTrack>* tracks = nullptr;
+  tree->SetBranchAddress("MCTrack", &tracks);
+
+  Long64_t nEntries = tree->GetEntries();
+  int nSelected = 0;
+
+  for (Long64_t i = 0; i < nEntries; ++i) {
+    tree->GetEntry(i);
+    for (const auto& track : *tracks) {
+      if (std::abs(track.GetPdgCode()) == 1000010020) { // Deuteron
+        ++nSelected;
+        break; // Found in this event
+      }
+    }
+  }
+
+  if (nSelected == 0) {
+    std::cerr << "No event of interest\n";
+    return 1;
+  }
+
+  std::cout << "Found " << nSelected << " events with deuterons\n";
+  return 0;
+}
+
