9948f7d36615eb21c33fe6dbd11d1adc984dbcfd
Marco Giacalone <mgiacalo@gmail.com>
2025-06-04 12:22:35 +0200

    Update of example including EPOS4.0.3 version (#1961)
    * Update of example including EPOS4.0.3 version
    
    * Include example with EPOS4HQ
    
    * Make epos.sh compatible to /bin/sh

--- MC/config/examples/epos4/epos.sh
+++ MC/config/examples/epos4/epos.sh
@@ -5,7 +5,33 @@
 # in order not to print useless information on stdout (a z-*optns*.mtr file will be created)
 
 optns="example"
-seed=$RANDOM
+seed=$(od -An -N2 -tu2 /dev/urandom | tr -d ' ')
+EPOS4=""
+
+if [ -z "$EPO4VSN" ]; then
+    echo "Error: EPO4VSN environment variable is not set"
+    exit 1
+fi
+
+if [ "$EPO4VSN" = "4.0.0" ]; then
+    EPOS4="$EPOS4_ROOT/epos4/scripts/epos"
+    export LIBDIR=$EPOS4_ROOT/epos4/bin
+else
+    EPOS4="$EPOS4_ROOT/bin/epos"
+fi
+
+# Check if the environment variable EPO4 is set (mandatory with o2dpg-sim-tests on CI machines)
+# If not, set all the EPOS4 related variables, most likely they are unset as well.
+if [ -z "${EPO4}" ]; then
+    export OPT=./
+    export HTO=./
+    export CHK=./
+    if [ "$EPO4VSN" = "4.0.0" ]; then
+        export EPO4=$EPOS4_ROOT/epos4/
+    else
+        export EPO4=$EPOS4_ROOT/
+    fi
+fi
 
 while test $# -gt 0 ; do
     case $1 in
@@ -18,24 +44,26 @@ done
 
 if [ ! -f $optns.optns ]; then
     echo "Error: Options file $optns.optns not found"
-    exit 1
+    exit 2
+fi
+
+if grep -Fq "set ihq 1" $optns.optns; then
+    if [ -z "$EPO4HQVSN" ]; then
+        # Error: EPOS4HQ version not found
+        exit 3
+    else
+        # Running with EPOS4HQ
+        if [ -z "${EPO4HQ}" ]; then
+            export EPO4HQ=$EPOS4HQ_ROOT/
+        fi
+        EPOS4="$EPO4HQ/bin/eposhq"
+    fi
 fi
 
 if [ $seed -eq 0 ]; then
-    echo "Seed can't be 0, random number will be used"
-    seed=$RANDOM
+    # Seed can't be 0, random number will be used
+    seed=$(od -An -N2 -tu2 /dev/urandom | tr -d ' ')
 fi
 
-# Check if the environment variable EPO4 is set (mandatory with o2dpg-sim-tests on CI machines)
-# If not, set all the EPOS4 related variables, most likely they are unset as well.
-if [ -z "${EPO4}" ]; then
-    export EPO4=$EPOS4_ROOT/epos4/
-    export LIBDIR=$EPOS4_ROOT/epos4/bin
-    export EPO4VSN=4.0.0
-    export OPT=./
-    export HTO=./
-    export CHK=./
-fi
-
-# Or filters the stdout with only HepMC2 useful data
-$EPOS4_ROOT/epos4/scripts/epos -hepstd -s $seed $optns | sed -n 's/^\(HepMC::\|[EAUWVP] \)/\1/p'
+# OR filters the stdout with only HepMC useful data
+$EPOS4 -hepstd -s $seed $optns | sed -n 's/^\(HepMC::\|[EAUWVP] \)/\1/p'
