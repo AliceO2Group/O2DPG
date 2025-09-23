420dc041bbe4c2060a2bfb9b08a406c4b82bd927
Sandro Wenzel <sandro.wenzel@cern.ch>
2025-09-23 14:11:25 +0200

    improvements for runGRIDContainerized
    * allows injection of CVMFS software package via env variable
    * allow executing apptainer under sudo
      - useful for certain payloads

--- GRID/utils/runGRIDContainerized.sh
+++ GRID/utils/runGRIDContainerized.sh
@@ -44,8 +44,21 @@ cp ${ALIEN_KEYFILE} ${WORK_DIR}/userkey.pem
 
 echo "JALIEN_TOKEN_CERT=/workdir/usercert.pem" > ${WORK_DIR}/envfile
 echo "JALIEN_TOKEN_KEY=/workdir/userkey.pem" >> ${WORK_DIR}/envfile
+if [ "${GRID_CVMFS_PACKAGE}" ]; then
+  echo "GRID_CVMFS_PACKAGE=${GRID_CVMFS_PACKAGE}" >> ${WORK_DIR}/envfile
+fi
+
+# load a CVMFS package if we are asked to do so -- but do it as part of the job
+LOADER='if [ "${GRID_CVMFS_PACKAGE}" ]; then
+  /cvmfs/alice.cern.ch/bin/alienv printenv ${GRID_CVMFS_PACKAGE} > cvmfs_env
+  source cvmfs_env
+fi'
+# Inject the block after the first line (shebang)
+JOBSCRIPT=${WORK_DIR}/job.sh
+awk -v block="$LOADER" 'NR==1 {print; print block; next} 1' "$JOBSCRIPT" > tmpfile && mv tmpfile "$JOBSCRIPT"
+chmod +x "${JOBSCRIPT}"
 
 # launch job = script inside the container in the workdir
-APPTAINER_EXEC="/cvmfs/alice.cern.ch/containers/bin/apptainer/${ARCH}/current/bin/apptainer"
-${APPTAINER_EXEC} exec -C -B /cvmfs:/cvmfs,${WORK_DIR}:/workdir  \
+APPTAINER_EXEC=${APPTAINER_EXEC:-"/cvmfs/alice.cern.ch/containers/bin/apptainer/${ARCH}/current/bin/apptainer"}
+${APPTAINER_SUDO:+sudo} ${APPTAINER_EXEC} exec -C -B /cvmfs:/cvmfs,${WORK_DIR}:/workdir                 \
                   --pwd /workdir --env-file ${WORK_DIR}/envfile ${APPTAINER_CONTAINER} /workdir/job.sh
