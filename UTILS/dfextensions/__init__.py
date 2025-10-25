4290510d1786b50aead9537d05fddceccb0689fd
miranov25 <marian.ivanov@cern.cg>
2025-10-25 23:35:18 +0200

    cleanup: finalize package structure cleanup
    - Removed diff.txt (transitional artifact from import migration)
    - Updated parent __init__.py for package integration
    
    All tests passing (41/41).
    Repository clean and ready for documentation.

--- UTILS/dfextensions/__init__.py
+++ UTILS/dfextensions/__init__.py
@@ -3,7 +3,7 @@
 from .AliasDataFrame import AliasDataFrame
 from .FormulaLinearModel import FormulaLinearModel
 from .DataFrameUtils import *  # if it provides general helper functions
-from .groupby_regression import *  # or other relevant functions
+#from .groupby_regression import *  # or other relevant functions
 
 __all__ = [
     "AliasDataFrame",
