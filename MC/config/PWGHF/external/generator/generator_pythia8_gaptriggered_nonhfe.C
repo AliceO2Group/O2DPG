35ccc9a2240b12261be72dcce420ec0db0b16dfc
Rashi gupta <167059733+rashigupt@users.noreply.github.com>
2026-01-11 15:51:15 +0530

    Update generator_pythia8_gaptriggered_nonhfe.C

--- MC/config/PWGHF/external/generator/generator_pythia8_gaptriggered_nonhfe.C
+++ MC/config/PWGHF/external/generator/generator_pythia8_gaptriggered_nonhfe.C
@@ -205,7 +205,7 @@ class GeneratorPythia8GapTriggeredNonHF : public o2::eventgen::GeneratorPythia8
 // Charm-enriched
 FairGenerator* GeneratorPythia8GapTriggeredPionAndEta(int inputTriggerRatio, float yQuarkMin = -1.5, float yQuarkMax = 1.5, float yHadronMin = -1.5, float yHadronMax = 1.5, std::vector<int> hadronPdgList = {})
 {
-  auto myGen = new GeneratorPythia8GapTriggeredNonHF(inputTriggerRatio, std::vector<int>{}, hadronPdgList);
+  auto myGen = new GeneratorPythia8GapTriggeredNonHF(inputTriggerRatio, std::vector<int>{1, 2, 3}, hadronPdgList);
   auto seed = (gRandom->TRandom::GetSeed() % 900000000);
   myGen->setUsedSeed(seed);
   myGen->readString("Random:setSeed on");
