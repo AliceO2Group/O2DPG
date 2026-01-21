81594056478a4e732276956ec1ce3bf808d3d39f
Ernst Hellbar <ernst.hellbar@cern.ch>
2026-01-21 11:47:04 +0100

    TPC laser: add ccdp-populator path for laser replay run

--- DATA/production/calib/tpc-laser-aggregator.sh
+++ DATA/production/calib/tpc-laser-aggregator.sh
@@ -10,7 +10,7 @@ PROXY_INSPEC="A:TPC/LASERTRACKS;B:TPC/CEDIGITS;D:TPC/CLUSREFS"
 
 CALIB_CONFIG="TPCCalibPulser.FirstTimeBin=450;TPCCalibPulser.LastTimeBin=550;TPCCalibPulser.NbinsQtot=300;TPCCalibPulser.XminQtot=2;TPCCalibPulser.XmaxQtot=602;TPCCalibPulser.MinimumQtot=8;TPCCalibPulser.MinimumQmax=6;TPCCalibPulser.XminT0=450;TPCCalibPulser.XmaxT0=550;TPCCalibPulser.NbinsT0=400;keyval.output_dir=/dev/null"
 
-CCDB_PATH="http://o2-ccdb.internal"
+[[ $RUNTYPE == "SYNTHETIC" ]] && CCDB_PATH="http://ccdb-test.cern.ch:8080" || CCDB_PATH="http://o2-ccdb.internal"
 
 HOST=localhost
 
