aa2877a3fadda141ae1361311d7745580cf7fdbf
lgansbartl <laura.gans-bartl@cern.ch>
2026-02-06 21:11:33 +0100

    [PWGEM] Add configs for Dalitz-decay MC (#2263)
    * first comit
    
    * Fix missing new line
    
    * Add Pythia8 Forced Dalitz test config for pp 13.6 TeV
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: Laura Gansbartl <lgansbartl@pcikf5.ikf.uni-frankfurt.de>

---
+++ MC/config/PWGEM/ini/tests/pythia8_pp_13600_ForcedDalitz.C
@@ -0,0 +1,39 @@
+int External() {
+    std::string path{"o2sim_Kine.root"};
+    // Check that file exists, can be opened and has the correct tree
+    TFile file(path.c_str(), "READ");
+    if (file.IsZombie())
+    {
+      std::cerr << "Cannot open ROOT file " << path << "\n";
+      return 1;
+    }
+    auto tree = (TTree *)file.Get("o2sim");
+    if (!tree)
+    {
+      std::cerr << "Cannot find tree o2sim in file " << path << "\n";
+      return 1;
+    }
+    std::vector<o2::MCTrack> *tracks{};
+    tree->SetBranchAddress("MCTrack", &tracks);
+  
+    // Check if all events are filled
+    auto nEvents = tree->GetEntries();
+    for (Long64_t i = 0; i < nEvents; ++i)
+    {
+      tree->GetEntry(i);
+      if (tracks->empty())
+      {
+        std::cerr << "Empty entry found at event " << i << "\n";
+        return 1;
+      }
+    }
+
+  
+    return 0;
+  }
+  
+  int pythia8()
+  {
+      return External();
+  }
+  
