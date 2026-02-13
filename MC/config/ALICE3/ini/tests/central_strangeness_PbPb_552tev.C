cf115570514fa6f94a3c9aa59a322b768f5eb6c2
Jesper Karlsson Gumprecht <113693781+jesgum@users.noreply.github.com>
2026-02-13 06:07:39 +0100

    Central PbPb generator with added strangeness (#2269)
    * Central PbPb generator with strangeness
    
    * update test

---
+++ MC/config/ALICE3/ini/tests/central_strangeness_PbPb_552tev.C
@@ -0,0 +1,36 @@
+int External() 
+{
+  std::string path{"o2sim_Kine.root"};
+
+  TFile file(path.c_str(), "READ");
+  if (file.IsZombie()) {
+    std::cerr << "Cannot open ROOT file " << path << "\n";
+    return 1;
+  }
+
+  auto tree = (TTree *)file.Get("o2sim");
+  if (!tree) {
+    std::cerr << "Cannot find tree o2sim in file " << path << "\n";
+    return 1;
+  }
+
+  std::vector<o2::MCTrack> *tracks{};
+  tree->SetBranchAddress("MCTrack", &tracks);
+  auto nEvents = tree->GetEntries();
+  int nTracks = 0;
+  for (int i = 0; i < nEvents; i++) {
+    tree->GetEntry(i);
+    nTracks += tracks->size();
+  }
+
+  const int meanNTracksPerEvent = nTracks / nEvents;
+
+  // Expecting only events with a 0-10% centrality
+  // 0-100%  gives a mean of ~1350
+  // 2300 should be enough
+  if (meanNTracksPerEvent < 2300) {
+    return 1;
+  }
+
+  return 0;
+}
