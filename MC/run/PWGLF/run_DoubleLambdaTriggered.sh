dee9ab38b768ad5c8fdd3a4dc4ee0764dae73e7f
Chiara De Martin <chdemart@alicecerno2.cern.ch>
2026-02-25 17:54:09 +0100

    force the Lambda to decay into proton and pion

--- MC/run/PWGLF/run_DoubleLambdaTriggered.sh
+++ MC/run/PWGLF/run_DoubleLambdaTriggered.sh
@@ -13,7 +13,7 @@ export IGNORE_VALIDITYCHECK_OF_CCDB_LOCALCACHE=1
 
 NWORKERS=${NWORKERS:-8}
 SIMENGINE=${SIMENGINE:-TGeant4}
-NSIGEVENTS=${NSIGEVENTS:-100}
+NSIGEVENTS=${NSIGEVENTS:-1000}
 NTIMEFRAMES=${NTIMEFRAMES:-1}
 INTRATE=${INTRATE:-50000}
 SYSTEM=${SYSTEM:-pp}
@@ -26,10 +26,12 @@ echo "NWORKERS = $NWORKERS"
 
 # create workflow
 O2_SIM_WORKFLOW=${O2_SIM_WORKFLOW:-"${O2DPG_ROOT}/MC/bin/o2dpg_sim_workflow.py"}
+CFGDECAY="${O2DPG_ROOT}/MC/config/PWGLF/pythia8/decayer/force_lambda_charged_decay.cfg"
+G4CFG="${O2DPG_ROOT}/MC/config/PWGLF/pythia8/decayer/g4_ext_decayer_lambda.in"
 $O2_SIM_WORKFLOW -eCM ${ENERGY} -col ${SYSTEM} -gen external \
         -j ${NWORKERS} \
         -ns ${NSIGEVENTS} -tf ${NTIMEFRAMES} -interactionRate ${INTRATE} \
-        -confKey "Diamond.width[2]=6." \
+        -confKey "Diamond.width[0]=0.1;Diamond.width[1]=0.1;Diamond.width[2]=6.;;G4.physicsmode=kUSER;G4.userPhysicsList=FTFP_BERT_EMV+optical+biasing+hyperNuclei;DecayerPythia8.config[1]=${CFGDECAY};DecayerPythia8.showChanged=1;G4.configMacroFile=${G4CFG}" \
         ${SEED} \
         -e ${SIMENGINE} \
         -ini $CFGINIFILE
