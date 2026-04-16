556f5602478c3ffa8286368e04006a43e3aedf81
Felix Schlepper <felix.schlepper@cern.ch>
2026-04-16 16:58:27 +0200

    ITS: propagate settings to wfx (#2323)
    * ITS: propagate settings to wfx
    
    Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>
    
    * ITS: remove duplicated setting already present in dpl-workflow.sh
    
    Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>
    
    ---------
    
    Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>

--- MC/bin/o2dpg_sim_config.py
+++ MC/bin/o2dpg_sim_config.py
@@ -28,9 +28,19 @@ def create_sim_config(args):
        if 302000 <= int(args.run) and int(args.run) < 309999:
            add(config, {"ITSAlpideParam.roFrameLengthInBC" : 198})
        # ITS reco settings
-       add(config, {"ITSVertexerParam.phiCut" : 0.5,
-                    "ITSVertexerParam.clusterContributorsCut" : 3,
-                    "ITSVertexerParam.tanLambdaCut" : 0.2})
+       add(config, {"ITSVertexerParam.pairCut": 0.0317563,
+                    "ITSVertexerParam.clusterCut": 0.6640964,
+                    "ITSVertexerParam.coarseZWindow": 0.2049018,
+                    "ITSVertexerParam.seedDedupZCut": 0.0711793,
+                    "ITSVertexerParam.refitDedupZCut": 0.0680009,
+                    "ITSVertexerParam.duplicateZCut": 0.1582193,
+                    "ITSVertexerParam.finalSelectionZCut": 0.1081465,
+                    "ITSVertexerParam.duplicateDistance2Cut": 0.0117033,
+                    "ITSVertexerParam.clusterContributorsCut": 2,
+                    "ITSVertexerParam.seedMemberRadiusZ": 0,
+                    "ITSVertexerParam.vertNsigmaCut": 4.0,
+                    "ITSVertexerParam.vertRadiusSigma": 0.0452309,
+                    "ITSVertexerParam.trackletSigma": 0.0025941})
        # primary vertexing settings
        if 301000 <= int(args.run) and int(args.run) <= 301999:
           add(config, {"pvertexer.acceptableScale2" : 9,
