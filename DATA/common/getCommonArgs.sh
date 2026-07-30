fe3dca57989f9c3198419aa46a5f41e7b9725a53
shahor02 <shahor02@users.noreply.github.com>
2026-07-30 12:33:00 +0400

    Patches to allow storing ConfiParam jsons in the AOD metadata (#2421)
    * Disable ConfigParam outputs only in EPNSYNC mode
    
    * JDL option to force AODProducer to collect ConfigParam jsons

--- DATA/common/getCommonArgs.sh
+++ DATA/common/getCommonArgs.sh
@@ -10,16 +10,18 @@ if [[ -z $SEVERITY || -z $NUMAID || -z $SHMSIZE || -z $FILEWORKDIR || -z $EPNSYN
 fi
 
 ARGS_ALL="--session ${OVERRIDE_SESSION:-default} --severity $SEVERITY --shm-segment-id ${O2JOBSHMID:-$NUMAID} --shm-segment-size $SHMSIZE ${ARGS_ALL_EXTRA:-} --early-forward-policy noraw"
-ARGS_ALL_CONFIG="keyval.input_dir=$FILEWORKDIR;keyval.output_dir=/dev/null;${ALL_EXTRA_CONFIG:-}"
+ARGS_ALL_CONFIG="keyval.input_dir=$FILEWORKDIR;"
 if [[ $EPNSYNCMODE == 1 ]]; then
   ARGS_ALL+=" --infologger-severity $INFOLOGGER_SEVERITY"
   ARGS_ALL+=" --monitoring-backend influxdb-unix:///tmp/telegraf.sock --resources-monitoring ${GEN_TOPO_RESOURCE_MONITORING:-15} ${GEN_TOPO_RESOURCE_MONITORING:+--dpl-stats-min-online-publishing-interval $GEN_TOPO_RESOURCE_MONITORING}"
-  ARGS_ALL_CONFIG+="NameConf.mCCDBServer=$GEN_TOPO_EPN_CCDB_SERVER;"
+  ARGS_ALL_CONFIG+="keyval.output_dir=/dev/null;NameConf.mCCDBServer=$GEN_TOPO_EPN_CCDB_SERVER;"
   export DPL_CONDITION_BACKEND=$GEN_TOPO_EPN_CCDB_SERVER
   [[ -z ${O2_DPL_DEPLOYMENT_MODE:-} ]] && O2_DPL_DEPLOYMENT_MODE=OnlineECS
 elif [[ "${ENABLE_METRICS:-}" != "1" ]]; then
   ARGS_ALL+=" --monitoring-backend no-op://"
 fi
+ARGS_ALL_CONFIG+="${ALL_EXTRA_CONFIG:-}"
+
 [[ $SHMTHROW == 0 ]] && ARGS_ALL+=" --bad-alloc-max-attempts 60 --bad-alloc-attempt-interval 1000"
 [[ ! -z ${SHM_MANAGER_SHMID:-} && ${GEN_TOPO_CALIB_WORKFLOW:-} != 1 ]] && ARGS_ALL+=" --no-cleanup --shm-no-cleanup on --shmid $SHM_MANAGER_SHMID"
 [[ $NORATELOG == 1 ]] && ARGS_ALL+=" --fairmq-rate-logging 0"
