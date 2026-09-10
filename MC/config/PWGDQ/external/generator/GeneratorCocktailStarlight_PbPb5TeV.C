c97b94a6ed595e12a62ee0fe2ec6b8cae1a1acad
ypwangg <ypwangg@mail.ustc.edu.cn>
2026-09-10 15:20:22 +0800

    fix bug

--- MC/config/PWGDQ/external/generator/GeneratorCocktailStarlight_PbPb5TeV.C
+++ MC/config/PWGDQ/external/generator/GeneratorCocktailStarlight_PbPb5TeV.C
@@ -23,16 +23,20 @@ o2::eventgen::GeneratorStarlight_class* makeStarlightGenerator(std::string confi
   // generator->setupDpmjet(dpmjetconf);
   return generator;
 }
-o2::eventgen::GeneratorEvtGen* makeStarlightToEvtGenGenerator(std::string configuration,
-                                                               float energyCM,
-                                                               int beam1Z,
-                                                               int beam1A,
-                                                               int beam2Z,
-                                                               int beam2A,
-                                                               std::string extraParams = "",
-                                                               std::string dpmjetconf = "")
+o2::eventgen::GeneratorEvtGen<o2::eventgen::GeneratorStarlight_class>* makeStarlightToEvtGenGenerator(std::string configuration,
+                                                                                                       float energyCM,
+                                                                                                       int beam1Z,
+                                                                                                       int beam1A,
+                                                                                                       int beam2Z,
+                                                                                                       int beam2A,
+                                                                                                       std::string extraParams = "",
+                                                                                                       std::string dpmjetconf = "")
 {
-  auto generator = makeStarlightGenerator(configuration, energyCM, beam1Z, beam1A, beam2Z, beam2A, extraParams, dpmjetconf);
+  auto generator = new o2::eventgen::GeneratorEvtGen<o2::eventgen::GeneratorStarlight_class>();
+  generator->selectConfiguration(configuration);
+  generator->setCollisionSystem(energyCM, beam1Z, beam1A, beam2Z, beam2A);
+  generator->setExtraParams(extraParams);
+  // generator->setupDpmjet(dpmjetconf);
   generator->SetPolarization(1); //Transversal
   generator->SetSizePdg(2);
   generator->AddPdg(443,0);
